d1a15ce20d24a17bcec2c6a4d53e50100ccdb706
WikiBluemoose <wikibluemoose@users.sourceforge.net>
2006-07-08 18:18:28 +0000

    Added a folder remotely
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@6 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ AWB/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿//$Header: /cvsroot/autowikibrowser/src/Properties/AssemblyInfo.cs,v 1.42 2006/07/06 14:06:54 wikibluemoose Exp $
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutoWikiBrowser")]
+[assembly: AssemblyDescription("AutoWikiBrowser comes with ABSOLUTELY NO WARRANTY.\r\n\r\nThis is a development version, you are responsible for what you do with this tool and use it at your own risk.\r\n\r\nAnyone is free to copy and distribute this software, only users registered on the Wikipedia:AutoWikiBrowser/CheckPage are allowed to use it. All editing done with it must be within Wikipedia policy and guidelines.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoWikiBrowser")]
+[assembly: AssemblyCopyright("Copyright ©  2006 General Public License V2")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d6768e6e-91a8-4384-805d-350273d5011a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("3.0.0.7")]
+[assembly: AssemblyFileVersion("3.0.0.7")]
