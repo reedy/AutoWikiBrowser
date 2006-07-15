35451e718c5de22bce2da07ee19cb97843c33945
WikiBluemoose <wikibluemoose@users.sourceforge.net>
2006-07-15 10:50:13 +0000

    silly bug
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@47 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Properties/AssemblyInfo.cs
+++ AWB/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.1.1")]
-[assembly: AssemblyFileVersion("3.0.1.1")]
+[assembly: AssemblyVersion("3.0.1.2")]
+[assembly: AssemblyFileVersion("3.0.1.2")]
