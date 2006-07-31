333ca6133b44781a2a91d79d041e6a8ffce6fece
WikiBluemoose <wikibluemoose@users.sourceforge.net>
2006-07-31 12:28:52 +0000

    improve Article class for future use
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@137 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/ArticleStruct.cs
+++ WikiFunctions/ArticleStruct.cs
@@ -1,45 +1,68 @@
+/*
+Autowikibrowser
+Copyright (C) 2006 Martin Richards
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace WikiFunctions
 {
-    public struct Article
+    public class Article
     {
+        public Article()
+        { }
+
         public Article(string Name)
         {
-            strName = Name;
-            intNameSpaceKey = Tools.CalculateNS(Name);
+            this.Name = Name;
+            this.NameSpaceKey = Tools.CalculateNS(Name);
         }
 
         public Article(string Name, int NameSpaceKey)
         {
-            strName = Name;
-            intNameSpaceKey = NameSpaceKey;
+            this.Name = Name;
+            this.NameSpaceKey = NameSpaceKey;
         }
 
-        int intNameSpaceKey;
-        string strName;
-
-        public string Name
-        {
-            get { return strName; }
-            set { strName = value; }
-        }
+        public int NameSpaceKey;
+        public string Name;
+        //public string ArticleText;
+        //public string OriginalText;
+        //public string EditSummary;
 
         public string URLEncodedName
         {
             get { return System.Web.HttpUtility.UrlEncode(Name); }
         }
 
-        public int NameSpaceKey
-        {
-            get { return intNameSpaceKey; }
-        }       
+        #region Overrides
 
         public override string ToString()
         {
             return Name;
         }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        #endregion
     }
 }
