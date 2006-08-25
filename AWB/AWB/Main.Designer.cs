45930cd3c527894735e66fb689b1b4bb628d7fd4
WikiBluemoose <wikibluemoose@users.sourceforge.net>
2006-08-25 13:06:29 +0000

    wiring in the plugin feature
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@267 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Main.Designer.cs
+++ AWB/AWB/Main.Designer.cs
@@ -233,6 +233,8 @@ namespace AutoWikiBrowser
             this.btntsFalsePositive = new System.Windows.Forms.ToolStripButton();
             this.lbltsNumberofItems = new System.Windows.Forms.ToolStripLabel();
             this.webBrowserEdit = new WikiFunctions.WebControl();
+            this.pluginsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usePluginsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -1505,6 +1507,7 @@ namespace AutoWikiBrowser
             this.fileToolStripMenuItem,
             this.generalToolStripMenuItem,
             this.toolsToolStripMenuItem,
+            this.pluginsToolStripMenuItem,
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
@@ -1776,7 +1779,7 @@ namespace AutoWikiBrowser
             // dumpHTMLToolStripMenuItem
             // 
             this.dumpHTMLToolStripMenuItem.Name = "dumpHTMLToolStripMenuItem";
-            this.dumpHTMLToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.dumpHTMLToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.dumpHTMLToolStripMenuItem.Text = "Dump HTML";
             this.dumpHTMLToolStripMenuItem.Visible = false;
             this.dumpHTMLToolStripMenuItem.Click += new System.EventHandler(this.dumpHTMLToolStripMenuItem_Click);
@@ -1784,14 +1787,14 @@ namespace AutoWikiBrowser
             // helpToolStripMenuItem1
             // 
             this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
-            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(130, 22);
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
             this.helpToolStripMenuItem1.Text = "Help";
             this.helpToolStripMenuItem1.Click += new System.EventHandler(this.helpToolStripMenuItem1_Click);
             // 
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.aboutToolStripMenuItem.Text = "About...";
             this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
             // 
@@ -2040,6 +2043,23 @@ namespace AutoWikiBrowser
             this.webBrowserEdit.WebBrowserShortcutsEnabled = false;
             this.webBrowserEdit.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowserEdit_Navigating);
             this.webBrowserEdit.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowserEdit_DocumentCompleted);
+            // 
+            // pluginsToolStripMenuItem
+            // 
+            this.pluginsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usePluginsToolStripMenuItem});
+            this.pluginsToolStripMenuItem.Name = "pluginsToolStripMenuItem";
+            this.pluginsToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.pluginsToolStripMenuItem.Text = "Plugins";
+            // 
+            // usePluginsToolStripMenuItem
+            // 
+            this.usePluginsToolStripMenuItem.Checked = true;
+            this.usePluginsToolStripMenuItem.CheckOnClick = true;
+            this.usePluginsToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.usePluginsToolStripMenuItem.Name = "usePluginsToolStripMenuItem";
+            this.usePluginsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.usePluginsToolStripMenuItem.Text = "Use plugins";
             // 
             // MainForm
             // 
@@ -2290,6 +2310,8 @@ namespace AutoWikiBrowser
         private System.Windows.Forms.ToolStripButton btntsStart;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator18;
         private System.Windows.Forms.ToolStripMenuItem dumpHTMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pluginsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usePluginsToolStripMenuItem;
 
 
     }
