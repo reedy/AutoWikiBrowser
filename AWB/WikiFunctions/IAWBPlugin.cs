bab6145c1978538d00a7fcb7322eea7a8f5c6bb2
WikiBluemoose <wikibluemoose@users.sourceforge.net>
2006-08-26 13:23:08 +0000

    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@273 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/IAWBPlugin.cs
+++ AWB/WikiFunctions/IAWBPlugin.cs
@@ -7,7 +7,7 @@ namespace WikiFunctions.Plugin
 {
     public interface IAWBPlugin
     {
-        void Initialise(Form f, ToolStripMenuItem tsmi, ContextMenuStrip cms);
+        void Initialise(WikiFunctions.Lists.ListMaker list, WikiFunctions.Browser.WebControl web, ToolStripMenuItem tsmi, ContextMenuStrip cms);
         string Name { get; }
         string EditSummary { get;set; }
         string ProcessArticle(string ArticleText, string ArticleTitle, int Namespace, ref bool Skip);
