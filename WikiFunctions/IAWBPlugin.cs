69cd30354227dc4220df1736985937cb0536b3cd
WikiBluemoose <wikibluemoose@users.sourceforge.net>
2006-08-29 18:19:54 +0000

    added load/save/reset to plugins.
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@295 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/IAWBPlugin.cs
+++ WikiFunctions/IAWBPlugin.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace WikiFunctions.Plugin
 {
@@ -10,5 +11,9 @@ namespace WikiFunctions.Plugin
         void Initialise(WikiFunctions.Lists.ListMaker list, WikiFunctions.Browser.WebControl web, ToolStripMenuItem tsmi, ContextMenuStrip cms);
         string Name { get; }
         string ProcessArticle(string ArticleText, string ArticleTitle, int Namespace, ref string Summary, ref bool Skip);
+
+        void ReadXML(XmlTextReader Reader);
+        void WriteXML(XmlTextWriter Writer);
+        void Reset();
     }
 }
