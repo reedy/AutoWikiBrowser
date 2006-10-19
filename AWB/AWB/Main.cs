c0389ad9f5ab4fff4179788946590fb42897443e
WikiBluemoose <wikibluemoose@users.sourceforge.net>
2006-10-19 19:00:51 +0000

    fixed couple of bugs
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@436 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Main.cs
+++ AWB/AWB/Main.cs
@@ -1869,12 +1869,6 @@ namespace AutoWikiBrowser
 
         private void btnMoreFindAndReplce_Click(object sender, EventArgs e)
         {
-            Replacement r = new Replacement();
-            r.RegularExpressinonOptions = RegexOptions.IgnoreCase | RegexOptions.Multiline;
-
-            MessageBox.Show(r.RegularExpressinonOptions.ToString());
-            findAndReplace.AddNew(r);
-
             if (!findAndReplace.Visible)
                 findAndReplace.ShowDialog();
             else
