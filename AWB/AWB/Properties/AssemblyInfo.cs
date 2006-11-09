8e0b7b96a9991179c2149e764b52d876f85843b1
WikiBluemoose <wikibluemoose@users.sourceforge.net>
2006-11-09 17:51:16 +0000

    bump version and disable admin features for now
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@501 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Properties/AssemblyInfo.cs
+++ AWB/AWB/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.5.1")]
-[assembly: AssemblyFileVersion("3.0.5.1")]
+[assembly: AssemblyVersion("3.0.5.2")]
+[assembly: AssemblyFileVersion("3.0.5.2")]
