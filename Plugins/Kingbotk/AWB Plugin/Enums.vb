0656af3acd4ef9bf2e73934564a5c96045b87d94
Steven Kennedy <kingboyk@users.sourceforge.net>
2006-11-11 13:20:35 +0000

    Last version before change to single plugin model
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2561 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ Plugins/Kingbotk/AWB Plugin/Enums.vb
@@ -0,0 +1,24 @@
+Namespace AWB
+    Public Enum Namespaces As Integer
+        Media = -2
+        Special = -1
+        Main = 0
+        Talk
+        User
+        UserTalk
+        Project
+        ProjectTalk
+        Image
+        ImageTalk
+        Mediawiki
+        MediawikiTalk
+        Template
+        TemplateTalk
+        Help
+        HelpTalk
+        Category
+        CategoryTalk
+        Portal = 100
+        PortalTalk
+    End Enum
+End Namespace
---
+++ Plugins/Kingbotk/AWB Plugin/PluginSettings.vb
@@ -0,0 +1,13 @@
+'Namespace AWB.Plugins.SDKSoftware.Kingbotk.Components
+Friend Class PluginSettings
+    '* General options/display:
+    ' open skipped list (skipped for tagging errors list; build as HTML with edit links)
+    ' tag categories option (where a suitable class= exists; maybe should be in template-specific options)
+    ' hide AWB tabs (may be a menu item)
+    ' do AWB settings (basically turn everything off)
+    ' reset #s when get a reset event
+    ' duration, edits, rate
+    ' load/save settings buttons
+    ' stop/start
+End Class
+'end namespace
