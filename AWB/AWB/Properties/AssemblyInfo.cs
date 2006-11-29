8762b150911a040c8be941bcde75be0b81d1c885
WikiBluemoose <wikibluemoose@users.sourceforge.net>
2006-11-29 10:17:43 +0000

    bump version to 3.0.6.0
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@528 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Properties/AssemblyInfo.cs
+++ AWB/AWB/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.5.2")]
-[assembly: AssemblyFileVersion("3.0.5.2")]
+[assembly: AssemblyVersion("3.0.6.0")]
+[assembly: AssemblyFileVersion("3.0.6.0")]
