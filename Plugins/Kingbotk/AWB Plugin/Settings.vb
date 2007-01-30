8495b803dd4c8dbe32eb40b9a271380ec60c8c1c
Sam Reed <sam@reedyboy.net>
2007-01-30 21:54:01 +0000

    Added LoadSettings and SaveSettings Code
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2602 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/Kingbotk/AWB Plugin/Settings.vb
+++ Plugins/Kingbotk/AWB Plugin/Settings.vb
@@ -1,7 +1,12 @@
 Imports System.Xml.Serialization
 
 Namespace AutoWikiBrowser.Plugins.SDKSoftware.Kingbotk
+
+
     <System.Serializable()> Public Class PluginSettings
+        'Public conNudgeParm As String = NudgeMenuItem.Checked.ToString
+        'Public conShowHideTabsParm As String = ShowHideTabs.ToString
+        'Public conShowManualAssessmentsInstructions As String = blnShowManualAssessmentsInstructions.ToString
 
     End Class
 End Namespace
