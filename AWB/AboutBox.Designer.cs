d444a1f5707e0b16f35b844f4d7f90c230745d42
Sam Reed <sam@reedyboy.net>
2007-02-23 15:13:33 +0000

    Updated AboutBox
    Added links to Bugs and Feature Request pages
    
    Added Reedy Boy to maintainers
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@628 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AboutBox.Designer.cs
+++ AWB/AboutBox.Designer.cs
@@ -48,26 +48,30 @@ namespace AutoWikiBrowser
         {
             this.textBoxDescription = new System.Windows.Forms.TextBox();
             this.okButton = new System.Windows.Forms.Button();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkBluemoose = new System.Windows.Forms.LinkLabel();
+            this.linkAWBPage = new System.Windows.Forms.LinkLabel();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.lblAWBVersion = new System.Windows.Forms.Label();
             this.lblOSVersion = new System.Windows.Forms.Label();
             this.lblIEVersion = new System.Windows.Forms.Label();
             this.lblNETVersion = new System.Windows.Forms.Label();
-            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLigulem = new System.Windows.Forms.LinkLabel();
             this.lblTimeAndEdits = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
-            this.linkLabel5 = new System.Windows.Forms.LinkLabel();
+            this.linkMets501 = new System.Windows.Forms.LinkLabel();
+            this.linkMaxSem = new System.Windows.Forms.LinkLabel();
+            this.linkBugs = new System.Windows.Forms.LinkLabel();
+            this.linkFeatureRequests = new System.Windows.Forms.LinkLabel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.linkReedy = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // textBoxDescription
             // 
-            this.textBoxDescription.Location = new System.Drawing.Point(15, 209);
+            this.textBoxDescription.Location = new System.Drawing.Point(15, 267);
             this.textBoxDescription.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
             this.textBoxDescription.Multiline = true;
             this.textBoxDescription.Name = "textBoxDescription";
@@ -82,34 +86,34 @@ namespace AutoWikiBrowser
             // 
             this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.okButton.Location = new System.Drawing.Point(188, 292);
+            this.okButton.Location = new System.Drawing.Point(188, 350);
             this.okButton.Name = "okButton";
             this.okButton.Size = new System.Drawing.Size(75, 22);
             this.okButton.TabIndex = 24;
             this.okButton.Text = "&OK";
             this.okButton.Click += new System.EventHandler(this.okButton_Click);
             // 
-            // linkLabel1
+            // linkBluemoose
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(34, 24);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(84, 13);
-            this.linkLabel1.TabIndex = 25;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "User:Bluemoose";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            this.linkBluemoose.AutoSize = true;
+            this.linkBluemoose.Location = new System.Drawing.Point(34, 24);
+            this.linkBluemoose.Name = "linkBluemoose";
+            this.linkBluemoose.Size = new System.Drawing.Size(84, 13);
+            this.linkBluemoose.TabIndex = 25;
+            this.linkBluemoose.TabStop = true;
+            this.linkBluemoose.Text = "User:Bluemoose";
+            this.linkBluemoose.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkBluemoose_LinkClicked);
             // 
-            // linkLabel2
+            // linkAWBPage
             // 
-            this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Location = new System.Drawing.Point(63, 94);
-            this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(115, 13);
-            this.linkLabel2.TabIndex = 26;
-            this.linkLabel2.TabStop = true;
-            this.linkLabel2.Text = "AutoWikiBrowser page";
-            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            this.linkAWBPage.AutoSize = true;
+            this.linkAWBPage.Location = new System.Drawing.Point(63, 114);
+            this.linkAWBPage.Name = "linkAWBPage";
+            this.linkAWBPage.Size = new System.Drawing.Size(115, 13);
+            this.linkAWBPage.TabIndex = 26;
+            this.linkAWBPage.TabStop = true;
+            this.linkAWBPage.Text = "AutoWikiBrowser page";
+            this.linkAWBPage.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
             // 
             // label1
             // 
@@ -123,7 +127,7 @@ namespace AutoWikiBrowser
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 94);
+            this.label2.Location = new System.Drawing.Point(15, 124);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(42, 13);
             this.label2.TabIndex = 28;
@@ -131,7 +135,7 @@ namespace AutoWikiBrowser
             // 
             // lblAWBVersion
             // 
-            this.lblAWBVersion.Location = new System.Drawing.Point(15, 112);
+            this.lblAWBVersion.Location = new System.Drawing.Point(15, 190);
             this.lblAWBVersion.Name = "lblAWBVersion";
             this.lblAWBVersion.Size = new System.Drawing.Size(163, 13);
             this.lblAWBVersion.TabIndex = 29;
@@ -140,7 +144,7 @@ namespace AutoWikiBrowser
             // lblOSVersion
             // 
             this.lblOSVersion.AutoSize = true;
-            this.lblOSVersion.Location = new System.Drawing.Point(15, 148);
+            this.lblOSVersion.Location = new System.Drawing.Point(15, 226);
             this.lblOSVersion.Name = "lblOSVersion";
             this.lblOSVersion.Size = new System.Drawing.Size(59, 13);
             this.lblOSVersion.TabIndex = 30;
@@ -149,7 +153,7 @@ namespace AutoWikiBrowser
             // lblIEVersion
             // 
             this.lblIEVersion.AutoSize = true;
-            this.lblIEVersion.Location = new System.Drawing.Point(15, 130);
+            this.lblIEVersion.Location = new System.Drawing.Point(15, 208);
             this.lblIEVersion.Name = "lblIEVersion";
             this.lblIEVersion.Size = new System.Drawing.Size(54, 13);
             this.lblIEVersion.TabIndex = 31;
@@ -158,27 +162,27 @@ namespace AutoWikiBrowser
             // lblNETVersion
             // 
             this.lblNETVersion.AutoSize = true;
-            this.lblNETVersion.Location = new System.Drawing.Point(15, 166);
+            this.lblNETVersion.Location = new System.Drawing.Point(15, 244);
             this.lblNETVersion.Name = "lblNETVersion";
             this.lblNETVersion.Size = new System.Drawing.Size(69, 13);
             this.lblNETVersion.TabIndex = 32;
             this.lblNETVersion.Text = ".NET version";
             // 
-            // linkLabel3
+            // linkLigulem
             // 
-            this.linkLabel3.AutoSize = true;
-            this.linkLabel3.Location = new System.Drawing.Point(139, 24);
-            this.linkLabel3.Name = "linkLabel3";
-            this.linkLabel3.Size = new System.Drawing.Size(68, 13);
-            this.linkLabel3.TabIndex = 33;
-            this.linkLabel3.TabStop = true;
-            this.linkLabel3.Text = "User:Ligulem";
-            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            this.linkLigulem.AutoSize = true;
+            this.linkLigulem.Location = new System.Drawing.Point(139, 24);
+            this.linkLigulem.Name = "linkLigulem";
+            this.linkLigulem.Size = new System.Drawing.Size(68, 13);
+            this.linkLigulem.TabIndex = 33;
+            this.linkLigulem.TabStop = true;
+            this.linkLigulem.Text = "User:Ligulem";
+            this.linkLigulem.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLigulem_LinkClicked);
             // 
             // lblTimeAndEdits
             // 
             this.lblTimeAndEdits.AutoSize = true;
-            this.lblTimeAndEdits.Location = new System.Drawing.Point(15, 184);
+            this.lblTimeAndEdits.Location = new System.Drawing.Point(15, 160);
             this.lblTimeAndEdits.Name = "lblTimeAndEdits";
             this.lblTimeAndEdits.Size = new System.Drawing.Size(0, 13);
             this.lblTimeAndEdits.TabIndex = 34;
@@ -206,53 +210,99 @@ namespace AutoWikiBrowser
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(104, 67);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.Size = new System.Drawing.Size(10, 13);
             this.label5.TabIndex = 39;
-            this.label5.Text = "and";
+            this.label5.Text = ",";
             // 
-            // linkLabel4
+            // linkMets501
             // 
-            this.linkLabel4.AutoSize = true;
-            this.linkLabel4.Location = new System.Drawing.Point(126, 67);
-            this.linkLabel4.Name = "linkLabel4";
-            this.linkLabel4.Size = new System.Drawing.Size(73, 13);
-            this.linkLabel4.TabIndex = 38;
-            this.linkLabel4.TabStop = true;
-            this.linkLabel4.Text = "User:Mets501";
-            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
+            this.linkMets501.AutoSize = true;
+            this.linkMets501.Location = new System.Drawing.Point(113, 67);
+            this.linkMets501.Name = "linkMets501";
+            this.linkMets501.Size = new System.Drawing.Size(73, 13);
+            this.linkMets501.TabIndex = 38;
+            this.linkMets501.TabStop = true;
+            this.linkMets501.Text = "User:Mets501";
+            this.linkMets501.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkMets501_LinkClicked);
             // 
-            // linkLabel5
+            // linkMaxSem
             // 
-            this.linkLabel5.AutoSize = true;
-            this.linkLabel5.Location = new System.Drawing.Point(34, 67);
-            this.linkLabel5.Name = "linkLabel5";
-            this.linkLabel5.Size = new System.Drawing.Size(73, 13);
-            this.linkLabel5.TabIndex = 37;
-            this.linkLabel5.TabStop = true;
-            this.linkLabel5.Text = "User:MaxSem";
-            this.linkLabel5.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel5_LinkClicked);
+            this.linkMaxSem.AutoSize = true;
+            this.linkMaxSem.Location = new System.Drawing.Point(34, 67);
+            this.linkMaxSem.Name = "linkMaxSem";
+            this.linkMaxSem.Size = new System.Drawing.Size(73, 13);
+            this.linkMaxSem.TabIndex = 37;
+            this.linkMaxSem.TabStop = true;
+            this.linkMaxSem.Text = "User:MaxSem";
+            this.linkMaxSem.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkMaxSem_LinkClicked);
+            // 
+            // linkBugs
+            // 
+            this.linkBugs.AutoSize = true;
+            this.linkBugs.Location = new System.Drawing.Point(63, 137);
+            this.linkBugs.Name = "linkBugs";
+            this.linkBugs.Size = new System.Drawing.Size(31, 13);
+            this.linkBugs.TabIndex = 40;
+            this.linkBugs.TabStop = true;
+            this.linkBugs.Text = "Bugs";
+            this.linkBugs.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel6_LinkClicked);
+            // 
+            // linkFeatureRequests
+            // 
+            this.linkFeatureRequests.AutoSize = true;
+            this.linkFeatureRequests.Location = new System.Drawing.Point(100, 137);
+            this.linkFeatureRequests.Name = "linkFeatureRequests";
+            this.linkFeatureRequests.Size = new System.Drawing.Size(91, 13);
+            this.linkFeatureRequests.TabIndex = 42;
+            this.linkFeatureRequests.TabStop = true;
+            this.linkFeatureRequests.Text = "Feature Requests";
+            this.linkFeatureRequests.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel7_LinkClicked);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(71, 89);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 43;
+            this.label6.Text = "and";
+            // 
+            // linkReedy
+            // 
+            this.linkReedy.AutoSize = true;
+            this.linkReedy.Location = new System.Drawing.Point(102, 89);
+            this.linkReedy.Name = "linkReedy";
+            this.linkReedy.Size = new System.Drawing.Size(84, 13);
+            this.linkReedy.TabIndex = 44;
+            this.linkReedy.TabStop = true;
+            this.linkReedy.Text = "User:Reedy Boy";
+            this.linkReedy.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkReedy_LinkClicked);
             // 
             // AboutBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(281, 326);
+            this.ClientSize = new System.Drawing.Size(281, 384);
+            this.Controls.Add(this.linkReedy);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.linkFeatureRequests);
+            this.Controls.Add(this.linkBugs);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.linkLabel4);
-            this.Controls.Add(this.linkLabel5);
+            this.Controls.Add(this.linkMets501);
+            this.Controls.Add(this.linkMaxSem);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lblTimeAndEdits);
-            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLigulem);
             this.Controls.Add(this.lblNETVersion);
             this.Controls.Add(this.lblIEVersion);
             this.Controls.Add(this.lblOSVersion);
             this.Controls.Add(this.lblAWBVersion);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.linkLabel2);
-            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.linkAWBPage);
+            this.Controls.Add(this.linkBluemoose);
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.textBoxDescription);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -275,20 +325,24 @@ namespace AutoWikiBrowser
 
         private System.Windows.Forms.TextBox textBoxDescription;
         private System.Windows.Forms.Button okButton;
-        private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkBluemoose;
+        private System.Windows.Forms.LinkLabel linkAWBPage;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblAWBVersion;
         private System.Windows.Forms.Label lblOSVersion;
         private System.Windows.Forms.Label lblIEVersion;
         private System.Windows.Forms.Label lblNETVersion;
-        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLigulem;
         private System.Windows.Forms.Label lblTimeAndEdits;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.LinkLabel linkLabel4;
-        private System.Windows.Forms.LinkLabel linkLabel5;
+        private System.Windows.Forms.LinkLabel linkMets501;
+        private System.Windows.Forms.LinkLabel linkMaxSem;
+        private System.Windows.Forms.LinkLabel linkBugs;
+        private System.Windows.Forms.LinkLabel linkFeatureRequests;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.LinkLabel linkReedy;
     }
 }
