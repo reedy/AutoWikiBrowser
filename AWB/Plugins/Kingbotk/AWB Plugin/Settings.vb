4191022fe65220966924cd2a7c35b3324a6eee90
Steven Kennedy <kingboyk@users.sourceforge.net>
2007-03-01 19:56:43 +0000

    Read/write settings (testing - we can write a plugin-defined object but I haven't been able to deserialize same yet)
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@2606 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Plugins/Kingbotk/AWB Plugin/Settings.vb
+++
@@ -1,8 +0,0 @@
-Imports System.Xml.Serialization
-
-Namespace AutoWikiBrowser.Plugins.SDKSoftware.Kingbotk
-
-    <System.Serializable()> Public Class AWBSettings ' Must be public
-        Public test As String = "MaxSem dies"
-    End Class
-End Namespace
