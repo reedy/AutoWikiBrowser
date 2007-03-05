46ef2584ac686b85abe33a81c1d0c7e8cecb575c
Steven Kennedy <kingboyk@users.sourceforge.net>
2007-03-05 22:24:45 +0000

    Added some commented-out code to test if WPBio's listas= param needs to be migrated to {{DEFAULTSORT}}
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@2624 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Plugins/Kingbotk/ConsoleApplication1/Module1.vb
+++ AWB/Plugins/Kingbotk/ConsoleApplication1/Module1.vb
@@ -6,27 +6,9 @@ Imports system.xml
 Module Module1
 
     Sub Main()
-        'Dim st As New IO.StringWriter
-        'Dim Writer As Xml.XmlWriter = XmlTextWriter.Create(st)
-        'Writer.WriteStartElement("root")
-        'Writer.WriteAttributeString("test123", "456")
-        'Writer.WriteEndElement()
-        'Writer.Flush()
-        'Return New Object() {stream}
+        Dim DefaultSortRegex As New Regex("\{{2,3}\s*(template\s*:\s*|)\s*DEFAULTSORT[\s\n\r]*(\||:)")
 
-        Dim st As New IO.StringReader("<?xml version=""1.0"" encoding=""utf-16""?>" & vbCrLf & _
-           "<root test123=""456"" fuck=""off"" />")
-        'st.ReadToEnd()
-        Dim Reader As XmlTextReader = New XmlTextReader(st)
-        'Reader.ReadStartElement("root")
-        While Reader.Read()
-            If Reader.NodeType = XmlNodeType.Element Then
-                Debug.Print(Reader.MoveToAttribute("fuck").ToString)
-                Debug.Print(Reader.Value)
-            End If
-        End While
-
-        'Debug.Print(Reader.MoveToAttribute("test123").ToString)
+        Debug.Print(DefaultSortRegex.IsMatch("{{{DEFAULTSORT:Bloggs, Fred}}}").ToString)
     End Sub
 
 End Module
