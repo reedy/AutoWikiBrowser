83823d394d85920bddaa081c639ba4261c46fa83
Steven Kennedy <kingboyk@users.sourceforge.net>
2007-03-17 18:25:09 +0000

    ConsoleApplication1
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2639 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/Kingbotk/ConsoleApplication1/Module1.vb
+++ Plugins/Kingbotk/ConsoleApplication1/Module1.vb
@@ -4,29 +4,8 @@ Imports system.xml
 ''' </summary>
 ''' <remarks></remarks>
 Module Module1
-    Dim MainRegex As Regex
-    Dim SecondChanceRegex As Regex
-    Const conRegexpLeft As String = "\{\{\s*(?<tl>template\s*:)?\s*(?<tlname>"
-    'dim Const conRegexpRight As String = _
-    '   ")[\s\n\r]*(([\s\n\r]*\|[\s\n\r]*(?<parm>[-a-z0-9&]*)[\s\n\r]*)+(=[\s\n\r]*(?<val>[-a-z0-9]*)[\s\n\r]*)?)*\}\}[\s\n\r]*"
-    Const conRegexpRight As String = _
-       ")[\s\n\r]*(([\s\n\r]*\|[\s\n\r]*(?<parm>[^}{|\s\n\r=]*)[\s\n\r]*)+(=[\s\n\r]*" & _
-       "(?<val>[^}{|\s\n\r]*)[\s\n\r]*)?)*\}\}[\s\n\r]*"
-    Const conRegexpRightNotStrict As String = ")[^}]*"
-    Const conRegexpOptions As RegexOptions = RegexOptions.Compiled Or RegexOptions.Multiline Or _
-       RegexOptions.IgnoreCase Or RegexOptions.ExplicitCapture
-    Dim PreferredTemplateNameRegex As Regex
-
     Sub Main()
-        Const RegexpMiddle As String = "WPBiography|BioWikiProject|Musician|WikiProject Biography|broy"
-        MainRegex = New Regex(conRegexpLeft & RegexpMiddle & conRegexpRight, conRegexpOptions)
-
-        Dim ArticleText As String = "{{talkheader}}{{WPBiography|living=yes|class=B|importance=|musician-work-group=yes|listas=Levitin, Daniel}}" & _
-           Microsoft.VisualBasic.vbCrLf & Microsoft.VisualBasic.vbCrLf & _
-           "== Too many red links hurt my brain ==" & Microsoft.VisualBasic.vbCrLf & _
-           "I'm going to remove a few of them.  I hope you don't mind. -- [[User:Craigtalbert|Craigtalbert]] 13:30, 6 November 2006 (UTC)"
-
-        Debug.Print(MainRegex.IsMatch(ArticleText).ToString)
+        Debug.Print(WikiFunctions.Tools.MakeHumanCatKey("Talk:Dionysus (power metal band)"))
     End Sub
 
 End Module
