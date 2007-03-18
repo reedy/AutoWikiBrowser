1b097cd7d77e0b157fff7d62aeb3957b6bfeef2d
Mets501 <mets501@users.sourceforge.net>
2007-03-18 05:27:35 +0000

    complete rewrite of unzip code
    AutoUpdater fully integrated into AWB
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@739 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWBUpdater/Main.cs
+++ AWBUpdater/Main.cs
@@ -5,8 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
 using System.IO;
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip.Compression;
+using System.Text.RegularExpressions;
+using System.Net;
 
 namespace AWBUpdater
 {
@@ -14,8 +18,8 @@ namespace AWBUpdater
     {
         string AWBdirectory;
         string tempDirectory;
-        string AWBZipName = "AutoWikiBrowser3140.zip";
-        string WebAddress = "http://192.168.0.10/AutoWikiBrowser3140.zip";
+        string AWBZipName;
+        string WebAddress;
 
         public Main()
         {
@@ -25,14 +29,59 @@ namespace AWBUpdater
             tempDirectory = AWBdirectory + "temp\\";
         }
 
+        public void AWBversion()
+        {
+            string text = "";
+            try
+            {
+                HttpWebRequest rq = (HttpWebRequest)WebRequest.Create("http://en.wikipedia.org/w/index.php?title=Wikipedia:AutoWikiBrowser/CheckPage/Version&action=edit");
+
+                rq.Proxy.Credentials = CredentialCache.DefaultCredentials;
+                rq.UserAgent = "AWBUpdater " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+                HttpWebResponse response = (HttpWebResponse)rq.GetResponse();
+
+                Stream stream = response.GetResponseStream();
+                StreamReader sr = new StreamReader(stream, Encoding.UTF8);
+
+                text = sr.ReadToEnd();
+
+                sr.Close();
+                stream.Close();
+                response.Close();
+            }
+            catch
+            {
+                throw;
+            }
+
+            Match m_version = Regex.Match(text, @"&lt;!-- Current version: (.*?) --&gt;");
+            try
+            {
+                if (m_version.Success && m_version.Groups[1].Value.Length == 4)
+                {
+                    AWBZipName = "AutoWikiBrowser" + m_version.Groups[1].Value.Replace(".", "") + ".zip";
+                    WebAddress = "http://umn.dl.sourceforge.net/sourceforge/autowikibrowser/AutoWikiBrowser" + m_version.Groups[1].Value.Replace(".", "") + ".zip";
+                }
+                else
+                    throw new Exception();
+            }
+            catch
+            {
+                MessageBox.Show("Error fetching current version number.");
+            }
+        }
+
         //TODO: Functionality
         //Pass Where file to be downloaded from, and file name
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            AWBversion();
             createTempDir();
             getAWBFromInternet();
             unzipAWB();
+            MessageBox.Show("Please save your settings (if you wish) and close AutoWikiBrowser completely before pressing OK.");
             deleteOldFiles();
             copyFiles();
             MessageBox.Show("AWB Update Successful", "Update Sucessful");
@@ -60,10 +109,60 @@ namespace AWBUpdater
         }
 
         private void unzipAWB()
-        {
-            zipHelper.ExtractZipFile(tempDirectory + AWBZipName, tempDirectory);
-
+        {            
+            //zipHelper.ExtractZipFile(tempDirectory + AWBZipName, tempDirectory);
+            
+            using (ZipFile zf = new ZipFile(tempDirectory + AWBZipName))
+			{
+				foreach ( ZipEntry entry in zf )
+				{
+                    if (entry.IsFile)
+                    	ExtractFile(zf.GetInputStream(entry), entry, tempDirectory);
+				}
+			}
+            
             progressUpdate.Value = 70;
+        }
+
+        private void ExtractFile(Stream inputStream, ZipEntry theEntry, string targetDir)
+        {
+            // try and sort out the correct place to save this entry
+            string entryFileName;
+
+            if (Path.IsPathRooted(theEntry.Name))
+            {
+                string workName = Path.GetPathRoot(theEntry.Name);
+                workName = theEntry.Name.Substring(workName.Length);
+                entryFileName = Path.Combine(Path.GetDirectoryName(workName), Path.GetFileName(theEntry.Name));
+            }
+            else
+            {
+                entryFileName = theEntry.Name;
+            }
+
+            string targetName = Path.Combine(targetDir, entryFileName);
+            string fullPath = Path.GetDirectoryName(Path.GetFullPath(targetName));
+
+            // Could be an option or parameter to allow failure or try creation
+            if (Directory.Exists(fullPath) == false)
+            {
+                try
+                {
+                    Directory.CreateDirectory(fullPath);
+                }
+                catch
+                {
+                    MessageBox.Show("Error in updating AWB.");
+                }
+            }
+
+            if (entryFileName.Length > 0)
+            {
+                using (FileStream outputStream = File.Create(targetName))
+                {
+                    StreamUtils.Copy(inputStream, outputStream, new byte[4096]);
+                }
+            }
         }
 
         private void deleteOldFiles()
