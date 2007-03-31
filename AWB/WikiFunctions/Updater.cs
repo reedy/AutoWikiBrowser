69c2f9e4cd6023afc7a29a40f56617226ea76114
Sam Reed <sam@reedyboy.net>
2007-03-31 10:17:20 +0000

    Moved update Updater code into WF.dll
    Added check to see if AWBUpdater is out of date
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@861 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ AWB/WikiFunctions/Updater.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+
+namespace WikiFunctions
+{
+    public class Updater
+    {
+        /// <summary>
+        /// Checks to see if AWBUpdater.exe.new exists, if it does, replace it.
+        /// If not, see if the version of AWB Updater is older than the version on the checkpage, and run AWBUpdater if so
+        /// </summary>
+        public void Update()
+        {
+            String tempPath = ".\\";
+            if (File.Exists(tempPath + "AWBUpdater.exe.new"))
+            {
+                File.Delete(tempPath + "AWBUpdater.exe");
+                File.Copy(tempPath + "AWBUpdater.exe.new", tempPath + "AWBUpdater.exe");
+                File.Delete(tempPath + "AWBUpdater.exe.new");
+            }
+            else
+            {
+                string text = "";
+                try
+                {
+                    HttpWebRequest rq = (HttpWebRequest)WebRequest.Create("http://en.wikipedia.org/w/index.php?title=Wikipedia:AutoWikiBrowser/CheckPage/Version&action=edit");
+
+                    rq.Proxy.Credentials = CredentialCache.DefaultCredentials;
+                    rq.UserAgent = "AutoWikiBrowser " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+                    HttpWebResponse response = (HttpWebResponse)rq.GetResponse();
+
+                    Stream stream = response.GetResponseStream();
+                    StreamReader sr = new StreamReader(stream, Encoding.UTF8);
+
+                    text = sr.ReadToEnd();
+
+                    sr.Close();
+                    stream.Close();
+                    response.Close();
+                }
+                catch
+                {
+                }
+
+                Match m_updversion = Regex.Match(text, @"&lt;!-- Updater version: (.*?) --&gt;");
+
+                if (m_updversion.Success && m_updversion.Groups[1].Value.Length == 4)
+                {
+                    try
+                    {
+                        FileVersionInfo versionUpdater = FileVersionInfo.GetVersionInfo(".\\AWBUpdater.exe");
+
+                        if (Convert.ToInt32(m_updversion.Groups[1].Value) > Convert.ToInt32(versionUpdater.FileVersion.Replace(".", "")))
+                        {
+                            System.Diagnostics.Process.Start(".\\AWBUpdater.exe");
+                        }
+                    }
+                    catch
+                    { }
+                }
+            }
+        }
+    }
+}
