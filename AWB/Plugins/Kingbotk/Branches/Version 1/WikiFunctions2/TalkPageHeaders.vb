35c2085fc83d45f21f04360775e322c4e382cf81
Steven Kennedy <kingboyk@users.sourceforge.net>
2007-04-02 16:19:39 +0000

    Changes for DEFAULTSORT/talk page layout
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@2701 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Plugins/Kingbotk/Branches/Version 1/WikiFunctions2/TalkPageHeaders.vb
+++ AWB/Plugins/Kingbotk/Branches/Version 1/WikiFunctions2/TalkPageHeaders.vb
@@ -1,4 +1,9 @@
 Namespace TalkPages
+    Public Enum DEFAULTSORT
+        NoChange
+        MoveToTop
+        MoveToBottom
+    End Enum
     Public Module TalkPageHeaders
         ''' <summary>
         ''' Parses for {{talkheader}} and {{skiptotoctalk}} and moves them to the top
@@ -9,32 +14,83 @@ Namespace TalkPages
         Private SkipTOCTemplateRegex As New Regex( _
            "\{\{\s*(template:)?(skiptotoctalk|Skiptotoc|Skiptotoc-talk)\s*\}\}[\s\n\r]*", _
            RegexOptions.ExplicitCapture Or RegexOptions.Compiled Or RegexOptions.IgnoreCase)
-        Private FoundTalkheader As Boolean, FoundSkipTOC As Boolean
+        Private DefaultSortRegex As New Regex("\{\{\s*(template\s*:\s*)*\s*defaultsort\s*(:|\|)(?<key>[^\}]*)\}\}", _
+           RegexOptions.IgnoreCase Or RegexOptions.Compiled Or RegexOptions.ExplicitCapture)
+        Private FoundTalkheader As Boolean, FoundSkipTOC As Boolean, FoundDefaultSort As Boolean
+        Private DefaultSortKey As String
 
-        Public Sub ProcessTalkPage(ByRef ArticleText As String, ByVal Trace As TraceManager)
-            FoundTalkheader = False : FoundSkipTOC = False
+        Public Function ProcessTalkPage(ByRef ArticleText As String, ByVal Trace As IMyTraceListener, _
+        ByVal MoveDEFAULTSORT As DEFAULTSORT, ByVal PluginName As String) As Boolean
+            FoundTalkheader = False : FoundSkipTOC = False : FoundDefaultSort = False
 
-            ArticleText = TalkheaderTemplateRegex.Replace(ArticleText, AddressOf MatchEvaluator, 1)
-            ArticleText = SkipTOCTemplateRegex.Replace(ArticleText, AddressOf MatchEvaluator2, 1)
+            ArticleText = TalkheaderTemplateRegex.Replace(ArticleText, AddressOf TalkheaderMatchEvaluator, 1)
+            ArticleText = SkipTOCTemplateRegex.Replace(ArticleText, AddressOf SkipTOCMatchEvaluator, 1)
 
-            If FoundTalkheader Then WriteHeaderTemplate("talkheader", ArticleText, Trace)
-            If FoundSkipTOC Then WriteHeaderTemplate("skiptotoctalk", ArticleText, Trace)
+            If FoundTalkheader Then WriteHeaderTemplate("talkheader", ArticleText, Trace, PluginName)
+            If FoundSkipTOC Then WriteHeaderTemplate("skiptotoctalk", ArticleText, Trace, PluginName)
+
+            If Not MoveDEFAULTSORT = DEFAULTSORT.NoChange Then
+                ArticleText = DefaultSortRegex.Replace(ArticleText, AddressOf DefaultSortMatchEvaluator, 1)
+                If FoundDefaultSort Then
+                    If DefaultSortKey = "" Then
+                        If Not Trace Is Nothing Then _
+                           Trace.WriteArticleActionLine("DEFAULTSORT has no key; removed", PluginName)
+                    Else
+                        WriteDefaultSortMagicWord(MoveDEFAULTSORT, Trace, PluginName, ArticleText)
+                    End If
+                End If
+
+                DefaultSortKey = ""
+            End If
+
+            Return FoundTalkheader OrElse FoundSkipTOC OrElse FoundDefaultSort
+        End Function
+        Public Sub ProcessTalkPage(ByRef ArticleText As String, ByVal PluginName As String)
+            ProcessTalkPage(ArticleText, Nothing, DEFAULTSORT.NoChange, PluginName)
         End Sub
-        Public Sub ProcessTalkPage(ByRef ArticleText As String)
-            ProcessTalkPage(ArticleText, Nothing)
+        Public Sub ProcessTalkPage(ByRef ArticleText As String, ByVal MoveDEFAULTSORT As DEFAULTSORT, _
+        ByVal PluginName As String)
+            ProcessTalkPage(ArticleText, Nothing, MoveDEFAULTSORT, PluginName)
         End Sub
+        Public Function ContainsDefaultSortKeywordOrTemplate(ByVal ArticleText As String) As Boolean
+            Return DefaultSortRegex.IsMatch(ArticleText)
+        End Function
 
-        Private Sub WriteHeaderTemplate(ByVal Name As String, ByRef ArticleText As String, ByVal Trace As TraceManager)
+        Private Sub WriteDefaultSortMagicWord(ByVal Location As DEFAULTSORT, ByVal Trace As IMyTraceListener, _
+        ByVal PluginName As String, ByRef ArticleText As String)
+            Const conDefSort As String = "{{DEFAULTSORT:"
+            Dim strMovedTo As String
+
+            If Location = DEFAULTSORT.MoveToTop Then
+                ArticleText = conDefSort & DefaultSortKey & "}}" & Microsoft.VisualBasic.vbCrLf & ArticleText
+                strMovedTo = " given top billing"
+            Else
+                ArticleText += Microsoft.VisualBasic.vbCrLf & conDefSort & DefaultSortKey & "}}"
+                strMovedTo = " sent to the bottom"
+            End If
+            If Not Trace Is Nothing Then Trace.WriteArticleActionLine("{{DEFAULTSORT}}" & strMovedTo, _
+                PluginName, False)
+        End Sub
+        Private Sub WriteHeaderTemplate(ByVal Name As String, ByRef ArticleText As String, _
+        ByVal Trace As IMyTraceListener, ByVal PluginName As String)
             ArticleText = "{{" & Name & "}}" & Microsoft.VisualBasic.vbCrLf & ArticleText
-            If Not Trace Is Nothing Then Trace.WriteArticleActionLine1("{{tl|" & Name & " }} given top billing", _
-               "Plugin manager", True)
+            If Not Trace Is Nothing Then Trace.WriteArticleActionLine("{{tl|" & Name & " }} given top billing", _
+               PluginName, False)
         End Sub
-        Private Function MatchEvaluator(ByVal match As Match) As String
+        Private Function TalkheaderMatchEvaluator(ByVal match As Match) As String
             FoundTalkheader = True
             Return ""
         End Function
-        Private Function MatchEvaluator2(ByVal match As Match) As String
+        Private Function SkipTOCMatchEvaluator(ByVal match As Match) As String
             FoundSkipTOC = True
+            Return ""
+        End Function
+        Private Function DefaultSortMatchEvaluator(ByVal match As Match) As String
+            FoundDefaultSort = True
+
+            If match.Groups("key").Captures.Count > 0 Then _
+               DefaultSortKey = match.Groups("key").Captures(0).Value.Trim
+
             Return ""
         End Function
     End Module
