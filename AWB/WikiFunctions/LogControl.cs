881038ef9f03666e7618ed5be03f94d0cdb1f258
Sam Reed <sam@reedyboy.net>
2007-04-06 18:39:08 +0000

    Added extra try catch to LogControl Column Sorter
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@962 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/LogControl.cs
+++ AWB/WikiFunctions/LogControl.cs
@@ -234,29 +234,33 @@ namespace WikiFunctions.Logging
 
         private void lvColumnSort(ListView listView, System.Windows.Forms.ColumnClickEventArgs e)
         {
-            // Determine whether the column is the same as the last column clicked.
-            if (e.Column != sortColumn)
+            try
             {
-                // Set the sort column to the new column.
-                sortColumn = e.Column;
-                // Set the sort order to ascending by default.
-                listView.Sorting = SortOrder.Ascending;
-            }
-            else
-            {
-                // Determine what the last sort order was and change it.
-                if (listView.Sorting == SortOrder.Ascending)
-                    listView.Sorting = SortOrder.Descending;
-                else
+                // Determine whether the column is the same as the last column clicked.
+                if (e.Column != sortColumn)
+                {
+                    // Set the sort column to the new column.
+                    sortColumn = e.Column;
+                    // Set the sort order to ascending by default.
                     listView.Sorting = SortOrder.Ascending;
-            }
+                }
+                else
+                {
+                    // Determine what the last sort order was and change it.
+                    if (listView.Sorting == SortOrder.Ascending)
+                        listView.Sorting = SortOrder.Descending;
+                    else
+                        listView.Sorting = SortOrder.Ascending;
+                }
 
-            // Call the sort method to manually sort.
-            listView.Sort();
-            // Set the ListViewItemSorter property to a new ListViewItemComparer
-            // object.
-            listView.ListViewItemSorter = new ListViewItemComparer(e.Column,
-                                                              listView.Sorting);
+                // Call the sort method to manually sort.
+                listView.Sort();
+                // Set the ListViewItemSorter property to a new ListViewItemComparer
+                // object.
+                listView.ListViewItemSorter = new ListViewItemComparer(e.Column,
+                                                                  listView.Sorting);
+            }
+            catch { }
         }
         #endregion
 
