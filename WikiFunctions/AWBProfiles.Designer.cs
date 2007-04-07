7f7e5460e7c1430f7101c6115e46b770a9f58fc7
Sam Reed <sam@reedyboy.net>
2007-04-07 12:32:22 +0000

    Added basis of AWBProfiles
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@972 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ WikiFunctions/AWBProfiles.Designer.cs
@@ -0,0 +1,192 @@
+namespace WikiFunctions
+{
+    partial class AWBProfiles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lvAccounts = new System.Windows.Forms.ListView();
+            this.colAccountName = new System.Windows.Forms.ColumnHeader();
+            this.colPasswordSaved = new System.Windows.Forms.ColumnHeader();
+            this.colNotes = new System.Windows.Forms.ColumnHeader();
+            this.colProfileSettings = new System.Windows.Forms.ColumnHeader();
+            this.mnuAccounts = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.loginAsThisAccountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.deleteThisSavedAccountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewAccountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.changePasswordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAccounts.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvAccounts
+            // 
+            this.lvAccounts.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colAccountName,
+            this.colPasswordSaved,
+            this.colProfileSettings,
+            this.colNotes});
+            this.lvAccounts.ContextMenuStrip = this.mnuAccounts;
+            this.lvAccounts.Location = new System.Drawing.Point(12, 12);
+            this.lvAccounts.Name = "lvAccounts";
+            this.lvAccounts.Size = new System.Drawing.Size(447, 179);
+            this.lvAccounts.TabIndex = 0;
+            this.lvAccounts.UseCompatibleStateImageBehavior = false;
+            this.lvAccounts.View = System.Windows.Forms.View.Details;
+            // 
+            // colAccountName
+            // 
+            this.colAccountName.Text = "Account Name";
+            this.colAccountName.Width = 99;
+            // 
+            // colPasswordSaved
+            // 
+            this.colPasswordSaved.Text = "Password Saved?";
+            this.colPasswordSaved.Width = 107;
+            // 
+            // colNotes
+            // 
+            this.colNotes.Text = "Notes";
+            this.colNotes.Width = 93;
+            // 
+            // colProfileSettings
+            // 
+            this.colProfileSettings.Text = "Profile Default Settings";
+            this.colProfileSettings.Width = 141;
+            // 
+            // mnuAccounts
+            // 
+            this.mnuAccounts.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loginAsThisAccountToolStripMenuItem,
+            this.changePasswordToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.addNewAccountToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.deleteThisSavedAccountToolStripMenuItem});
+            this.mnuAccounts.Name = "mnuAccounts";
+            this.mnuAccounts.Size = new System.Drawing.Size(211, 104);
+            // 
+            // loginAsThisAccountToolStripMenuItem
+            // 
+            this.loginAsThisAccountToolStripMenuItem.Name = "loginAsThisAccountToolStripMenuItem";
+            this.loginAsThisAccountToolStripMenuItem.Size = new System.Drawing.Size(210, 22);
+            this.loginAsThisAccountToolStripMenuItem.Text = "Log-in as this Account";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(207, 6);
+            // 
+            // deleteThisSavedAccountToolStripMenuItem
+            // 
+            this.deleteThisSavedAccountToolStripMenuItem.Name = "deleteThisSavedAccountToolStripMenuItem";
+            this.deleteThisSavedAccountToolStripMenuItem.Size = new System.Drawing.Size(210, 22);
+            this.deleteThisSavedAccountToolStripMenuItem.Text = "Delete this saved Account";
+            // 
+            // addNewAccountToolStripMenuItem
+            // 
+            this.addNewAccountToolStripMenuItem.Name = "addNewAccountToolStripMenuItem";
+            this.addNewAccountToolStripMenuItem.Size = new System.Drawing.Size(210, 22);
+            this.addNewAccountToolStripMenuItem.Text = "Add new Account";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(207, 6);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 197);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(91, 36);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add new Account";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(206, 197);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(91, 36);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete selected";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(109, 197);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(91, 36);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login as selected";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // changePasswordToolStripMenuItem
+            // 
+            this.changePasswordToolStripMenuItem.Name = "changePasswordToolStripMenuItem";
+            this.changePasswordToolStripMenuItem.Size = new System.Drawing.Size(210, 22);
+            this.changePasswordToolStripMenuItem.Text = "Change Saved Password";
+            // 
+            // AWBProfiles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 248);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lvAccounts);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "AWBProfiles";
+            this.Text = "Profiles";
+            this.mnuAccounts.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvAccounts;
+        private System.Windows.Forms.ColumnHeader colAccountName;
+        private System.Windows.Forms.ColumnHeader colPasswordSaved;
+        private System.Windows.Forms.ColumnHeader colProfileSettings;
+        private System.Windows.Forms.ColumnHeader colNotes;
+        private System.Windows.Forms.ContextMenuStrip mnuAccounts;
+        private System.Windows.Forms.ToolStripMenuItem loginAsThisAccountToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem addNewAccountToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem deleteThisSavedAccountToolStripMenuItem;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.ToolStripMenuItem changePasswordToolStripMenuItem;
+    }
+}
