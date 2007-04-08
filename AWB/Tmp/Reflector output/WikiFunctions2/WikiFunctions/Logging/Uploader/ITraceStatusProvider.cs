1264b9609095022402f9ca2fdd806b4159b579b7
Steven Kennedy <kingboyk@users.sourceforge.net>
2007-04-08 23:30:49 +0000

    c# output from reflector for WikiFunctions2.dll
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@989 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ AWB/Tmp/Reflector output/WikiFunctions2/WikiFunctions/Logging/Uploader/ITraceStatusProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace WikiFunctions.Logging.Uploader
+{
+    public interface ITraceStatusProvider
+    {
+        WikiFunctions.Logging.Uploader.TraceStatus TraceStatus { get; }
+    }
+}
+
---
+++ AWB/Tmp/Reflector output/WikiFunctions2/WikiFunctions/WikiFunctions2.cs
@@ -0,0 +1,41 @@
+﻿namespace WikiFunctions
+{
+    using Microsoft.VisualBasic.CompilerServices;
+    using System;
+    using System.Reflection;
+    using System.Threading;
+
+    [StandardModule]
+    public sealed class WikiFunctions2
+    {
+        private static System.Version $STATIC$get_Version$001279$rtn;
+        private static StaticLocalInitFlag $STATIC$get_Version$001279$rtn$Init = new StaticLocalInitFlag();
+
+        public static System.Version Version
+        {
+            get
+            {
+                Monitor.Enter($STATIC$get_Version$001279$rtn$Init);
+                try
+                {
+                    if ($STATIC$get_Version$001279$rtn$Init.State == 0)
+                    {
+                        $STATIC$get_Version$001279$rtn$Init.State = 2;
+                        $STATIC$get_Version$001279$rtn = Assembly.GetExecutingAssembly().GetName().Version;
+                    }
+                    else if ($STATIC$get_Version$001279$rtn$Init.State == 2)
+                    {
+                        throw new IncompleteInitialization();
+                    }
+                }
+                finally
+                {
+                    $STATIC$get_Version$001279$rtn$Init.State = 1;
+                    Monitor.Exit($STATIC$get_Version$001279$rtn$Init);
+                }
+                return $STATIC$get_Version$001279$rtn;
+            }
+        }
+    }
+}
+
