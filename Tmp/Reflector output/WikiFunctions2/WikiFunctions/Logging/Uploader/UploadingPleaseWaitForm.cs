ac52e676014f8583f37113721d638d16146726ad
Steven Kennedy <kingboyk@users.sourceforge.net>
2007-04-08 23:30:49 +0000

    c# output from reflector for WikiFunctions2.dll
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@989 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ Tmp/Reflector output/WikiFunctions2/WikiFunctions/Logging/Uploader/UploadingPleaseWaitForm.cs
@@ -0,0 +1,123 @@
+﻿namespace WikiFunctions.Logging.Uploader
+{
+    using Microsoft.VisualBasic.CompilerServices;
+    using System;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.Runtime.CompilerServices;
+    using System.Windows.Forms;
+
+    [DesignerGenerated]
+    public class UploadingPleaseWaitForm : Form
+    {
+        [AccessedThroughProperty("Label1")]
+        private Label _Label1;
+        [AccessedThroughProperty("PictureBox1")]
+        private PictureBox _PictureBox1;
+        private IContainer components;
+        private Cursor oldCursor;
+
+        [DebuggerNonUserCode]
+        public UploadingPleaseWaitForm()
+        {
+            base.Shown += new EventHandler(this.Form_Shown);
+            base.FormClosing += new FormClosingEventHandler(this.Form_Closing);
+            this.InitializeComponent();
+        }
+
+        [DebuggerNonUserCode]
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (this.components != null))
+            {
+                this.components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            this.Cursor = this.oldCursor;
+        }
+
+        private void Form_Shown(object sender, EventArgs e)
+        {
+            this.oldCursor = this.Cursor;
+            this.Cursor = Cursors.WaitCursor;
+        }
+
+        [DebuggerStepThrough]
+        private void InitializeComponent()
+        {
+            ComponentResourceManager resources = new ComponentResourceManager(typeof(UploadingPleaseWaitForm));
+            this.PictureBox1 = new PictureBox();
+            this.Label1 = new Label();
+            ((ISupportInitialize) this.PictureBox1).BeginInit();
+            this.SuspendLayout();
+            this.PictureBox1.Image = (Image) resources.GetObject("PictureBox1.Image");
+            Point VB$t_struct$S0 = new Point(12, 12);
+            this.PictureBox1.Location = VB$t_struct$S0;
+            this.PictureBox1.Name = "PictureBox1";
+            Size VB$t_struct$S1 = new Size(70, 70);
+            this.PictureBox1.Size = VB$t_struct$S1;
+            this.PictureBox1.TabIndex = 0;
+            this.PictureBox1.TabStop = false;
+            this.Label1.AutoSize = true;
+            VB$t_struct$S0 = new Point(0x59, 0x17);
+            this.Label1.Location = VB$t_struct$S0;
+            this.Label1.Name = "Label1";
+            VB$t_struct$S1 = new Size(0xbf, 13);
+            this.Label1.Size = VB$t_struct$S1;
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "Your log is being uploaded, please wait";
+            SizeF VB$t_struct$S2 = new SizeF(6f, 13f);
+            this.AutoScaleDimensions = VB$t_struct$S2;
+            this.AutoScaleMode = AutoScaleMode.Font;
+            VB$t_struct$S1 = new Size(0x124, 0x5f);
+            this.ClientSize = VB$t_struct$S1;
+            this.ControlBox = false;
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.PictureBox1);
+            this.Icon = (Icon) resources.GetObject("$this.Icon");
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UploadingPleaseWaitForm";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "Uploading...";
+            this.TopMost = true;
+            ((ISupportInitialize) this.PictureBox1).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        protected internal virtual Label Label1
+        {
+            [DebuggerNonUserCode]
+            get
+            {
+                return this._Label1;
+            }
+            [MethodImpl(MethodImplOptions.Synchronized), DebuggerNonUserCode]
+            set
+            {
+                this._Label1 = value;
+            }
+        }
+
+        protected internal virtual PictureBox PictureBox1
+        {
+            [DebuggerNonUserCode]
+            get
+            {
+                return this._PictureBox1;
+            }
+            [MethodImpl(MethodImplOptions.Synchronized), DebuggerNonUserCode]
+            set
+            {
+                this._PictureBox1 = value;
+            }
+        }
+    }
+}
+
---
+++ Tmp/Reflector output/WikiFunctions2/WikiFunctions/Logging/XHTMLTraceListener.cs
@@ -0,0 +1,112 @@
+﻿namespace WikiFunctions.Logging
+{
+    using Microsoft.VisualBasic.CompilerServices;
+    using System;
+    using WikiFunctions;
+
+    public class XHTMLTraceListener : TraceListenerBase
+    {
+        protected static int mArticleCount = 1;
+        protected static bool mVerbose;
+
+        public XHTMLTraceListener(string filename, bool LogVerbose) : base(filename)
+        {
+            mVerbose = LogVerbose;
+            base.WriteLine("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">");
+            base.WriteLine("<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\" dir=\"ltr\">");
+            base.WriteLine("<head>");
+            base.WriteLine("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />");
+            base.WriteLine("<meta name=\"generator\" content=\"SDK Software WikiFunctions2" + WikiFunctions2.Version.ToString() + "\" />");
+            base.WriteLine("<title>AWB log</title>");
+            base.WriteLine("</head><body>");
+        }
+
+        public override void Close()
+        {
+            base.WriteLine("</body>");
+            base.WriteLine("</html>");
+            base.Close();
+        }
+
+        public override void ProcessingArticle(string ArticleFullTitle, Namespaces NS)
+        {
+            base.WriteLine("<br/>" + mArticleCount.ToString() + ". <a href=\"" + TraceListenerBase.GetURL(ArticleFullTitle) + "\">[[" + ArticleFullTitle + "]]</a>");
+            mArticleCount++;
+        }
+
+        public override void SkippedArticle(string SkippedBy, string Reason)
+        {
+            if (Operators.CompareString(Reason, "", false) != 0)
+            {
+                Reason = ": " + Reason;
+            }
+            base.WriteLine("<li><i>" + SkippedBy + ": Skipped" + Reason + "</i></li>");
+        }
+
+        public override void SkippedArticleBadTag(string SkippedBy, string FullArticleTitle, Namespaces NS)
+        {
+            this.SkippedArticle(SkippedBy, "Bad tag");
+        }
+
+        public override void WriteArticleActionLine(string Line, string PluginName)
+        {
+            base.WriteLine("<li><i>" + PluginName + ": " + Line + "</i></li>");
+        }
+
+        public override void WriteBulletedLine(string Line, bool Bold, bool VerboseOnly, bool DateStamp)
+        {
+            if (!VerboseOnly || mVerbose)
+            {
+                if (DateStamp)
+                {
+                    Line = string.Format("{0:g}: {1}", DateTime.Now, Line);
+                }
+                if (Bold)
+                {
+                    base.WriteLine("<br/><li><b>" + Line + "</b></li>");
+                }
+                else
+                {
+                    base.WriteLine("<li>" + Line + "</li>");
+                }
+            }
+        }
+
+        public override void WriteComment(string Line)
+        {
+            base.Write("<!-- " + Line + " -->");
+        }
+
+        public override void WriteCommentAndNewLine(string Line)
+        {
+            base.WriteLine("<!-- " + Line + " -->");
+        }
+
+        public override void WriteLine(string value)
+        {
+            base.WriteLine(value);
+        }
+
+        public override void WriteTemplateAdded(string Template, string PluginName)
+        {
+            base.WriteLine("<br/><li><i>" + PluginName + ": " + Template + "</i></li>");
+        }
+
+        public override bool Uploadable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override bool Verbose
+        {
+            get
+            {
+                return mVerbose;
+            }
+        }
+    }
+}
+
