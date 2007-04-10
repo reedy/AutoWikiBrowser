cfe6bd432add6b35bd60e04e6f1a80f086f5c5d0
Steven Kennedy <kingboyk@users.sourceforge.net>
2007-04-10 21:51:55 +0000

    Add Estimated Time of Completion label
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2727 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/Kingbotk/Branches/Version 1/AWB Plugin/TimerStats.vb
+++ Plugins/Kingbotk/Branches/Version 1/AWB Plugin/TimerStats.vb
@@ -4,6 +4,7 @@ Namespace AutoWikiBrowser.Plugins.SDKSoftware.Kingbotk.Components
         Private TimeSpan As TimeSpan
         Private Start As Date
         Private mNumberOfEdits As Integer
+        Private mETALabel As Label
 
         Private Property NumberOfEdits() As Integer
             Get
@@ -14,10 +15,11 @@ Namespace AutoWikiBrowser.Plugins.SDKSoftware.Kingbotk.Components
                 EditsLabel.Text = mNumberOfEdits.ToString
             End Set
         End Property
-        Friend Sub Init(ByVal w As WikiFunctions.Browser.WebControl)
+        Friend Sub Init(ByVal w As WikiFunctions.Browser.WebControl, ByVal ETALabel As Label)
             webcontrol = w
             ResetVars()
-            Timer1.Enabled = True
+            mETALabel = ETALabel
+            TimerEnabled = True
         End Sub
         Friend Sub Reset()
             ResetVars()
@@ -29,23 +31,63 @@ Namespace AutoWikiBrowser.Plugins.SDKSoftware.Kingbotk.Components
             NumberOfEdits = 0
             Start = Date.Now
         End Sub
-        Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles Timer1.Tick
-            Static regexp As New Regex("\..*")
-            Dim result As Double
-
-            TimeSpan = Date.Now - Start
-            TimerLabel.Text = regexp.Replace(TimeSpan.ToString, "")
-            result = Math.Round(TimeSpan.TotalSeconds / NumberOfEdits, 2)
-            If Double.IsInfinity(result) Then
-                SpeedLabel.Text = "0"
+        Private Property ETA() As String
+            Get
+                Return mETALabel.Text.Replace("ETC: ", "")
+            End Get
+            Set(ByVal value As String)
+                mETALabel.Text = "ETC: " & value
+            End Set
+        End Property
+        Private Sub CalculateETA(ByVal SecondsPerPage As Double)
+            Dim Count As Integer = PluginManager.listmaker.Count
+            If Count < 1 Then
+                ETA = "Now"
             Else
-                SpeedLabel.Text = result.ToString & " s/p"
+                Dim ETADateTime As Date = Date.Now.AddSeconds(SecondsPerPage * Count)
+
+                If ETADateTime.Date = Date.Now.Date Then
+                    ETA = ETADateTime.ToString("HH:mm") + " today"
+                ElseIf Date.Now.AddDays(1).Date = ETADateTime.Date Then
+                    ETA = ETADateTime.ToString("HH:mm") + " tomorrow"
+                Else
+                    ETA = ETADateTime.ToString("HH:mm, ddd d MMM")
+                End If
             End If
         End Sub
+        Private WriteOnly Property TimerEnabled() As Boolean
+            Set(ByVal value As Boolean)
+                mETALabel.Visible = value
+                Timer1.Enabled = value
+            End Set
+        End Property
+        Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles Timer1.Tick
+            Static regexp As New Regex("\..*")
+            Static UpdateETACount As Integer
+            Dim SecondsPerPage As Double
 
+            UpdateETACount += 1
+            TimeSpan = Date.Now - Start
+            TimerLabel.Text = regexp.Replace(TimeSpan.ToString, "")
+            SecondsPerPage = Math.Round(TimeSpan.TotalSeconds / NumberOfEdits, 2)
+
+            If Double.IsInfinity(SecondsPerPage) Then
+                SpeedLabel.Text = "0"
+                ETA = "-"
+                If UpdateETACount > 9 Then UpdateETACount = 0
+            Else
+                SpeedLabel.Text = SecondsPerPage.ToString & " s/p"
+                If UpdateETACount > 9 OrElse ETA = "-" Then
+                    UpdateETACount = 0
+                    CalculateETA(SecondsPerPage)
+                End If
+            End If
+        End Sub
         Private Sub webcontrol_BusyChanged() Handles webcontrol.BusyChanged
-            If Not webcontrol.Busy Then
-                Timer1.Enabled = False
+            If webcontrol.Busy Then
+                mETALabel.Visible = True
+            Else
+                TimerEnabled = False
                 TimeSpan = Nothing
                 webcontrol = Nothing
             End If
