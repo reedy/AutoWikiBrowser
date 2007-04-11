8fd1ca2d7680b1e4474f6c0981d92115ecdccb26
Steven Kennedy <kingboyk@users.sourceforge.net>
2007-04-11 20:45:18 +0000

    Add AWBProfile.cs (should have been in last commit)
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@1000 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ WikiFunctions/AWBProfile.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WikiFunctions.AWBProfiles
+{
+    class AWBProfile
+    {
+        protected string mUsername;
+        private string mPassword; // or store in RAM encrypted
+
+        public string Username
+        {
+            //get { }
+            set { }
+        }
+
+        public string Password
+        {
+            set { }
+        }
+    }
+}
