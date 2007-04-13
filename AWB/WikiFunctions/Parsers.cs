e82edc16ea1b8110057b30deed73ceaa30f49762
Max Semenik <maxsem.wiki@gmail.com>
2007-04-13 14:26:38 +0000

    de-bold headings
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@1007 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Parsers.cs
+++ AWB/WikiFunctions/Parsers.cs
@@ -155,6 +155,7 @@ namespace WikiFunctions.Parse
         readonly Regex regexHeadings5 = new Regex("(== ?)(further readings?:?)( ?==)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
         readonly Regex regexHeadings6 = new Regex("(== ?)(Early|Personal|Adult|Later) Life( ?==)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
         readonly Regex regexHeadings7 = new Regex("(== ?)(Current|Past|Prior) Members( ?==)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        readonly Regex regexHeadings8 = new Regex(@"^(=+)'''(.*?)'''(=+)\s*$", RegexOptions.Multiline | RegexOptions.Compiled);
         readonly Regex regexHeadingsCareer = new Regex("(== ?)([a-zA-Z]+) Career( ?==)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
         readonly Regex RegexBadHeader = new Regex("^(={1,4} ?(about|description|overview|definition|profile|(?:general )?information|background|intro(?:duction)?|summary|bio(?:graphy)?) ?={1,4})", RegexOptions.IgnoreCase | RegexOptions.Compiled);
@@ -198,6 +199,7 @@ namespace WikiFunctions.Parse
             ArticleText = regexHeadings5.Replace(ArticleText, "$1Further reading$3");
             ArticleText = regexHeadings6.Replace(ArticleText, "$1$2 life$3");
             ArticleText = regexHeadings7.Replace(ArticleText, "$1$2 members$3");
+            ArticleText = regexHeadings8.Replace(ArticleText, "$1$2$3");
             ArticleText = regexHeadingsCareer.Replace(ArticleText, "$1$2 career$3");
 
             return ArticleText;
