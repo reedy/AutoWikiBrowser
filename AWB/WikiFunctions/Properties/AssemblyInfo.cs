0e109becc6667e4df347e4b010589678260a8993
Steven Kennedy <kingboyk@users.sourceforge.net>
2007-04-21 12:17:24 +0000

    Up versions; modify Build profile to restore proper Debug build functionality (Max!)
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@1066 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Properties/AssemblyInfo.cs
+++ AWB/WikiFunctions/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WikiFunctions")]
-[assembly: AssemblyCopyright("Copyright ©  2006")]
+[assembly: AssemblyCopyright("Copyright ©  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.9.1.0")]
-[assembly: AssemblyFileVersion("3.9.1.0")]
+[assembly: AssemblyVersion("3.9.2.0")]
+[assembly: AssemblyFileVersion("3.9.2.0")]
