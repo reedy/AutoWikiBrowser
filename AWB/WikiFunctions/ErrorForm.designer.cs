d41030587f1d876304a52618bb5ed1e1be89d3b6
Sam Reed <sam@reedyboy.net>
2007-05-07 13:41:18 +0000

    Moved Rest of WikiFunctions2 to WikiFunctions
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@1140 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ AWB/WikiFunctions/ErrorForm.designer.cs
@@ -0,0 +1,64 @@
+namespace WikiFunctions.Logging.Uploader
+{
+    partial class ErrorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ErrorForm));
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(0, 0);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(35, 13);
+            this.lblError.TabIndex = 0;
+            this.lblError.Text = "label1";
+            // 
+            // ErrorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(407, 133);
+            this.Controls.Add(this.lblError);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ErrorForm";
+            this.Text = "ErrorForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblError;
+    }
+}
---
+++ AWB/WikiFunctions/TraceListeners.cs
@@ -0,0 +1,206 @@
+/*
+(C) 2007 Stephen Kennedy (Kingboyk) http://www.sdk-software.com/
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// From WikiFunctions2.dll. Converted from VB to C#
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WikiFunctions;
+using WikiFunctions.Logging;
+using WikiFunctions.Logging.Uploader;
+
+namespace WikiFunctions.Logging
+{
+	/// <summary>
+	/// This class logs in wiki format
+	/// </summary>
+	public class WikiTraceListener : TraceListenerUploadableBase
+	{
+
+		protected const string mDateFormat = "[[d MMMM]] [[yyyy]] HH:mm ";
+
+        public WikiTraceListener(UploadableLogSettings2 UploadSettings, TraceStatus TraceStatus)
+            : base(UploadSettings, TraceStatus)
+		{
+			WriteBulletedLine("Logging: [[User:Kingbotk/Plugin/WikiFunctions2|WikiFunctions2]].dll v" + Tools.Version.ToString(), false, false);
+		}
+
+		// Overrides:
+		public override void WriteBulletedLine(string Line, bool Bold, bool VerboseOnly, bool DateStamp)
+		{
+			if (VerboseOnly && ! Verbose)
+			{
+				return;
+			}
+			if (DateStamp)
+			{
+				Line = System.DateTime.Now.ToString(mDateFormat) + Line;
+			}
+			if (Bold)
+			{
+                base.WriteLine("*'''" + Line + "'''", true);
+            }
+                else base.WriteLine("*" + Line, true) ;
+		}
+		public override void ProcessingArticle(string ArticleFullTitle, Namespaces NS)
+		{
+			CheckCounterForUpload(); // Check counter *before* starting a new article section
+			base.WriteLine(GetArticleTemplate(ArticleFullTitle, NS), false);
+		}
+		public override void SkippedArticle(string SkippedBy, string Reason)
+		{
+			if (! (Reason == ""))
+			{
+				Reason = ": " + Reason;
+			}
+			base.WriteLine("#*''" + SkippedBy + ": Skipped" + Reason + "''", false);
+		}
+		public override void SkippedArticleBadTag(string SkippedBy, string FullArticleTitle, Namespaces NS)
+		{
+			SkippedArticle(SkippedBy, "Bad tag");
+		}
+		public override void WriteArticleActionLine(string Line, string PluginName)
+		{
+			base.WriteLine("#*" + PluginName + ": " + Line.Replace("[[Category:", "[[:Category:"), false);
+		}
+		public override void WriteTemplateAdded(string Template, string PluginName)
+		{
+			base.WriteLine(string.Format("#*{1}: [[Template:{0}|{0}]] added", Template, PluginName), false);
+		}
+
+		public override void WriteLine(string value)
+		{
+			WriteLine(value, true);
+		}
+
+		public override void WriteLine(string value, bool CheckCounter)
+		{
+			base.WriteLine(value, CheckCounter);
+		}
+		public override void WriteComment(string Line)
+		{
+			base.Write("<!-- " + Line + " -->");
+		}
+		public override void WriteCommentAndNewLine(string Line)
+		{
+			base.WriteLine("<!-- " + Line + " -->", false);
+		}
+	}
+
+    /// <summary>
+	/// This class logs in XHTML format
+	/// </summary>
+	public class XHTMLTraceListener : TraceListenerBase
+	{
+
+		protected static int mArticleCount = 1;
+		protected static bool mVerbose;
+
+		public XHTMLTraceListener(string filename, bool LogVerbose) : base(filename)
+		{
+			mVerbose = LogVerbose;
+
+			base.WriteLine("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" " + "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">");
+			base.WriteLine("<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" " + "lang=\"en\" dir=\"ltr\">");
+			base.WriteLine("<head>");
+			base.WriteLine("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />");
+			base.WriteLine("<meta name=\"generator\" content=\"SDK Software WikiFunctions2" + Tools.Version.ToString() + "\" />");
+			base.WriteLine("<title>AWB log</title>");
+			base.WriteLine("</head><body>");
+		}
+
+		// Overrides:
+		public override void Close()
+		{
+			base.WriteLine("</body>");
+			base.WriteLine("</html>");
+			base.Close();
+		}
+		public override bool Verbose
+		{
+			get
+			{
+				return mVerbose;
+			}
+		}
+		public override void WriteBulletedLine(string Line, bool Bold, bool VerboseOnly, bool DateStamp)
+		{
+			if (VerboseOnly && ! mVerbose)
+			{
+				return;
+			}
+			if (DateStamp)
+			{
+				Line = string.Format("{0:g}: {1}", System.DateTime.Now, Line);
+			}
+			if (Bold)
+			{
+				base.WriteLine("<br/><li><b>" + Line + "</b></li>");
+			}
+			else
+			{
+				base.WriteLine("<li>" + Line + "</li>");
+			}
+		}
+		public override void ProcessingArticle(string ArticleFullTitle, Namespaces NS)
+		{
+			base.WriteLine("<br/>" + mArticleCount.ToString() + ". <a href=\"" + WikiFunctions.Variables.GetPageURL(ArticleFullTitle) + "\">[[" + ArticleFullTitle + "]]</a>");
+			mArticleCount += 1;
+		}
+		public override void SkippedArticle(string SkippedBy, string Reason)
+		{
+			if (! (Reason == ""))
+			{
+				Reason = ": " + Reason;
+			}
+			base.WriteLine("<li><i>" + SkippedBy + ": Skipped" + Reason + "</i></li>");
+		}
+		public override void SkippedArticleBadTag(string SkippedBy, string FullArticleTitle, Namespaces NS)
+		{
+			SkippedArticle(SkippedBy, "Bad tag");
+		}
+		public override void WriteArticleActionLine(string Line, string PluginName)
+		{
+			base.WriteLine("<li><i>" + PluginName + ": " + Line + "</i></li>");
+		}
+		public override void WriteTemplateAdded(string Template, string PluginName)
+		{
+			base.WriteLine("<br/><li><i>" + PluginName + ": " + Template + "</i></li>");
+		}
+		public override void WriteLine(string value)
+		{
+			base.WriteLine(value);
+		}
+		public override void WriteComment(string Line)
+		{
+			base.Write("<!-- " + Line + " -->");
+		}
+		public override void WriteCommentAndNewLine(string Line)
+		{
+			base.WriteLine("<!-- " + Line + " -->");
+		}
+		public override bool Uploadable
+		{
+			get
+			{
+				return false;
+			}
+		}
+	}
+}
