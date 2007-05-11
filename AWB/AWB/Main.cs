6b1e664e516b0e7153c5d90037f5a73e078d09fc
Steven Kennedy <kingboyk@users.sourceforge.net>
2007-05-11 21:34:18 +0000

    add IAutoWikiBrowser.AddLogItem
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@1173 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Main.cs
+++ AWB/AWB/Main.cs
@@ -3173,6 +3173,7 @@ font-size: 150%;'>No changes</h2><p>Press the ""Ignore"" button below to skip to
             string IAutoWikiBrowser.CustomModule { get { if (cModule.ModuleEnabled && cModule.Module != null) return cModule.Code; else return null; } }
             System.Version IAutoWikiBrowser.AWBVersion { get { return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version; } }
             System.Version IAutoWikiBrowser.WikiFunctionsVersion { get { return WikiFunctions.Tools.Version; } }
+            void IAutoWikiBrowser.AddLogItem(bool Skipped, AWBLogListener LogListener) { LogControl1.AddLog(Skipped, LogListener); }
 
         // "Events":
             void IAutoWikiBrowser.SkipPage(IAWBPlugin sender, string reason) { SkipPage(sender.Name, reason); }
