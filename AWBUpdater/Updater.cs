3feba46e63e00a9d262851a1c001bed819d3317f
Sam Reed <sam@reedyboy.net>
2007-05-17 15:35:45 +0000

    (m) Altered AWBUpdater GPL Header
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@1235 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWBUpdater/Updater.cs
+++ AWBUpdater/Updater.cs
@@ -1,4 +1,6 @@
 /*
+AWBUpdater
+Copyright (C) 2007 Sam Reed
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
