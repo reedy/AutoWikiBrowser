fb3d2d716f48535e1de3cd4ae75ebf63c9de13f7
Steven Kennedy <kingboyk@users.sourceforge.net>
2007-05-17 17:09:04 +0000

    (m) props
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@1238 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/LoggingSettings.cs
+++ AWB/AWB/LoggingSettings.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using WikiFunctions.Logging;
+using WikiFunctions.Logging.Uploader;
 
 // temp: this should be built into the Logging tab control
 namespace AutoWikiBrowser.Logging
@@ -14,5 +15,29 @@ namespace AutoWikiBrowser.Logging
 
         internal WikiFunctions.Controls.Colour LEDColour
         { get { return WikiFunctions.Controls.Colour.Blue; } set { } }
+
+        internal sealed class Props : UploadableLogSettings2
+        {
+            internal Props()
+            {
+                base.mUploadLocation = "$USER/Logs";
+                base.mUploadJobName = "$CATEGORY";
+            }
+
+            internal bool Equals(Props Compare)
+            {
+                return (((((Operators.CompareString(Compare.LogFolder, this.LogFolder, false) == 0) && 
+                    (Compare.LogVerbose == this.LogVerbose)) && ((Compare.LogWiki == this.LogWiki) && 
+                    (Compare.LogXHTML == this.LogXHTML))) && 
+                    (((Compare.UploadAddToWatchlist == this.UploadAddToWatchlist) && 
+                    (Operators.CompareString(Compare.UploadJobName, this.UploadJobName, false) == 0)) && 
+                    ((Operators.CompareString(Compare.UploadLocation, this.UploadLocation, false) == 0) && 
+                    (Compare.UploadMaxLines == this.UploadMaxLines)))) && 
+                    (((Compare.UploadOpenInBrowser == this.UploadOpenInBrowser) && 
+                    (Compare.UploadToWikiProjects == this.UploadToWikiProjects)) && 
+                    ((Compare.UploadYN == this.UploadYN) && 
+                    (Operators.CompareString(Compare.Category, this.Category, false) == 0))))               
+            }
+        }
     }
 }
