74e5010707ebf4f3f99761b01498292d4313f51e
Mets501 <mets501@users.sourceforge.net>
2007-05-20 15:15:38 +0000

    fix up splash screen why didn't I just set the image as the background instead of using a huge picture box the first time?
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@1306 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Splash.designer.cs
+++ AWB/AWB/Splash.designer.cs
@@ -29,40 +29,13 @@ namespace AutoWikiBrowser
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.lblVersion = new WikiFunctions.Controls.TransparentLabel();
             this.progressBar = new System.Windows.Forms.ProgressBar();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.lblVersion = new WikiFunctions.Controls.TransparentLabel();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(5, 5);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(481, 303);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.UseWaitCursor = true;
-            this.pictureBox1.WaitOnLoad = true;
-            this.pictureBox1.Click += new System.EventHandler(this.ClickHandler);
-            // 
-            // lblVersion
-            // 
-            this.lblVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
-            this.lblVersion.Location = new System.Drawing.Point(41, 178);
-            this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(182, 36);
-            this.lblVersion.TabIndex = 2;
-            this.lblVersion.TabStop = false;
-            this.lblVersion.Text = "Version";
-            this.lblVersion.UseWaitCursor = true;
-            this.lblVersion.Click += new System.EventHandler(this.ClickHandler);
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(41, 220);
+            this.progressBar.Location = new System.Drawing.Point(34, 222);
             this.progressBar.Name = "progressBar";
             this.progressBar.Size = new System.Drawing.Size(277, 23);
             this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
@@ -70,16 +43,27 @@ namespace AutoWikiBrowser
             this.progressBar.UseWaitCursor = true;
             this.progressBar.Click += new System.EventHandler(this.ClickHandler);
             // 
+            // lblVersion
+            // 
+            this.lblVersion.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVersion.Location = new System.Drawing.Point(33, 173);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(179, 41);
+            this.lblVersion.TabIndex = 4;
+            this.lblVersion.TabStop = false;
+            this.lblVersion.Text = "Version";
+            this.lblVersion.UseWaitCursor = true;
+            // 
             // Splash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(491, 313);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(478, 302);
             this.ControlBox = false;
-            this.Controls.Add(this.progressBar);
             this.Controls.Add(this.lblVersion);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -90,15 +74,13 @@ namespace AutoWikiBrowser
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.UseWaitCursor = true;
             this.Click += new System.EventHandler(this.ClickHandler);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private WikiFunctions.Controls.TransparentLabel lblVersion;
         private System.Windows.Forms.ProgressBar progressBar;
+        private WikiFunctions.Controls.TransparentLabel lblVersion;
     }
 }
