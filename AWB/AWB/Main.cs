53e6c3d4972254d8307e134110660e18e30c23b0
Sam Reed <sam@reedyboy.net>
2007-06-03 19:16:01 +0000

    Changed Link to regexTester to .Show() from .ShowDialog()
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@1327 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Main.cs
+++ AWB/AWB/Main.cs
@@ -2883,7 +2883,7 @@ font-size: 150%;'>No changes</h2><p>Press the ""Ignore"" button below to skip to
 
         private void testRegexToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            regexTester.ShowDialog();
+            regexTester.Show();
         }
 
         private void chkLock_CheckedChanged(object sender, EventArgs e)
