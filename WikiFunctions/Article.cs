500cacf61d8d7017812dc4ef53ac5d8152c00e6a
Max Semenik <maxsem.wiki@gmail.com>
2007-06-24 14:04:37 +0000

    stuff I forgot in previous commit
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@1353 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Article.cs
+++ WikiFunctions/Article.cs
@@ -615,7 +615,7 @@ namespace WikiFunctions
         {
             // visible parts of links may contain crap we shouldn't modify, such as
             // refs and external links
-            //if (title.Contains("[") || title.Contains("{")) return title;
+            if (title.Contains("[") || title.Contains("{")) return title;
 
             string s = Parsers.CanonicalizeTitle(title);
             if (Variables.UnderscoredTitles.Contains(new Article(Tools.TurnFirstToUpper(s))))
