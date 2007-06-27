3dd9a4e3b1bcac21c7a59f3c7427dc47c555b135
Max Semenik <maxsem.wiki@gmail.com>
2007-06-27 19:52:01 +0000

    tweaks in profiles
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@1361 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Main.cs
+++ AWB/Main.cs
@@ -3425,7 +3425,7 @@ font-size: 150%;'>No changes</h2><p>Press the ""Ignore"" button below to skip to
         private void profilesToolStripMenuItem_Click(object sender, EventArgs e)
         {
             WikiFunctions.AWBProfiles.AWBProfilesForm profiles = new WikiFunctions.AWBProfiles.AWBProfilesForm(webBrowserEdit);
-            profiles.Show();
+            profiles.Show(this);
         }
 
         private void chkMinor_CheckedChanged(object sender, EventArgs e)
