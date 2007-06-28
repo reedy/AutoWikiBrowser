6191141584f9724a14046391fca1474b6476b85e
Max Semenik <maxsem.wiki@gmail.com>
2007-06-28 20:27:06 +0000

    fixed re-parse
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@1369 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Main.cs
+++ AWB/AWB/Main.cs
@@ -2431,7 +2431,7 @@ font-size: 150%;'>No changes</h2><p>Press the ""Ignore"" button below to skip to
 
         private void reparseToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            ArticleEx a = new ArticleEx();
+            ArticleEx a = new ArticleEx(TheArticle.Name);
 
             a.OriginalArticleText = txtEdit.Text;
             ProcessPage(a);
