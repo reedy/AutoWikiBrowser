d6d5b007b4d7162ad2d2266984a61d7e9957047f
Steven Kennedy <kingboyk@users.sourceforge.net>
2007-07-17 19:18:18 +0000

    Following the carnage of r1358 & r1359, I've attempted to patch r1357 with good changes from subsequent commits to date. This doesn't compile yet.
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@1448 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ Article.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WikiFunctions.Logging;
+
+namespace AutoWikiBrowser
+{
+    // A bit of a kludge this, as Article wasn't designed to write to a Trace listener collection, 
+    // and I don't want to make the underlying object too complicated.
+    internal sealed class ArticleEx : WikiFunctions.Article
+    {
+        public override IAWBTraceListener Trace
+        { get { return GlobalObjects.MyTrace; }
+        }
+
+        public override AWBLogListener InitialiseLogListener()
+        {
+            InitialiseLogListener("AWB", GlobalObjects.MyTrace);
+            GlobalObjects.MyTrace.AddListener("AWB", mAWBLogListener);
+            return mAWBLogListener;
+        }
+
+        public ArticleEx(string mName)
+            : base(mName)
+        { InitialiseLogListener(); }
+
+        // be careful to call this before creating the next ArticleWithLogging
+        // An alternative approach would be to call Close() from the other Log tab, where the AWBLogListener
+        // gets added to either the saved or skipped list. We can be fairly sure it will always get called
+        // at the right time then.
+        internal void Close()
+        { GlobalObjects.MyTrace.RemoveListener("AWB"); }
+
+        internal static ArticleEx SwitchToNewArticleObject(ArticleEx Old,
+            ArticleEx New)
+        {
+            if(Old != null && Old.LogListener != null)
+                Old.Close(); // remove old AWBLogListener from MyTrace collection
+            New.InitialiseLogListener(); // create new listener and add to collection
+            return New;
+        }
+    }
+}
