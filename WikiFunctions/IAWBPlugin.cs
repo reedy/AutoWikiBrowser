db320e59418ce2e268e74012808338a6c89a4bd3
Steven Kennedy <kingboyk@users.sourceforge.net>
2007-07-26 21:21:54 +0000

    Document some of the members of IAWBPlugin and IAutoWikiBrowser. Move IAutoWikiBrowser code to a seperate file. Make some of the interface methods explicitly part of that interface for ease of reading, at the expense of a very slight increase in runtime complexity.
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@1487 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/IAWBPlugin.cs
+++ WikiFunctions/IAWBPlugin.cs
@@ -29,18 +29,62 @@ namespace WikiFunctions.Plugin
 {
     /* Please DO NOT CHANGE without consulting plugin authors, unless moving to a new AWB major version (v5, v6 etc).
      * This interface is a contract with external plugins. */
+    /// <summary>
+    /// An interface for plugin components to be recognised by and interract with AWB
+    /// </summary>
     public interface IAWBPlugin
     {
+        /// <summary>
+        /// AWB calls this method when it is ready to initialise your plugin
+        /// </summary>
+        /// <param name="sender">A reference to an active IAutoWikiBrowser object owned by AWB. This object may be used by the plugin to access all sorts of functionality in AWB.</param>
         void Initialise(IAutoWikiBrowser sender);
+
+        /// <summary>
+        /// The name of your plugin
+        /// </summary>
         string Name { get; }
+
+        /// <summary>
+        /// The name of your plugin in Mediawiki syntax
+        /// </summary>
         string WikiName { get; }
+
+        /// <summary>
+        /// When AWB has an article to process, it calls this function in your plugin
+        /// </summary>
+        /// <param name="sender">A reference to an active IAutoWikiBrowser object owned by AWB</param>
+        /// <param name="eventargs">An ProcessArticleEventArgs object, containing various read-only and read-write data</param>
+        /// <returns></returns>
         string ProcessArticle(IAutoWikiBrowser sender, ProcessArticleEventArgs eventargs);
 
+        /// <summary>
+        /// Called by AWB when it loads a setting file
+        /// </summary>
+        /// <param name="Prefs">An array of deserialised setting objects belonging to your plugin</param>
         void LoadSettings(object[] Prefs);
+
+        /// <summary>
+        /// Called by AWB when it is saving settings
+        /// </summary>
+        /// <returns>An array of deserialised setting objects belonging to your plugin</returns>
         object[] SaveSettings();
+
+        /// <summary>
+        /// Called by AWB when the user has requested to return to default settings. When this is called you should reset your plugin to it's default state.
+        /// </summary>
         void Reset();
 
+        /// <summary>
+        /// AWB has got stuck and wants to "nudge" (stop and restart processing)
+        /// </summary>
+        /// <param name="Cancel">True if you want to cancel the "nudging" operation</param>
         void Nudge(out bool Cancel);
+
+        /// <summary>
+        ///  AWB performed a nudge
+        /// </summary>
+        /// <param name="Nudges">How many nudges AWB has performed in this session</param>
         void Nudged(int Nudges);
     }
 
@@ -49,6 +93,10 @@ namespace WikiFunctions.Plugin
     public interface IAutoWikiBrowser
     {
         Form Form { get; }
+
+        /// <summary>
+        /// Returns a reference to a WikiFunctions.Logging.TraceManager class which handles AWB's logging. This object also implements the IMyTraceListener interface. Plugin authors can use this reference to write to all active loggers, including the AWB Log tab and logfiles.
+        /// </summary>
         TraceManager TraceManager { get; } // implements IMyTraceListener
         WikiFunctions.Logging.Uploader.UploadableLogSettings2 LoggingSettings { get; }
         TabPage MoreOptionsTab { get; }
@@ -86,25 +134,59 @@ namespace WikiFunctions.Plugin
         string AWBVersionString { get; }
         string WikiFunctionsVersionString { get; }
         string WikiDiffVersionString { get; }
+
+        /// <summary>
+        /// Display a message balloon above AWB's system tray icon
+        /// </summary>
+        /// <param name="Message"></param>
+        /// <param name="Icon"></param>
         void NotifyBalloon(string Message, ToolTipIcon Icon);
         int Nudges { get; }
+
+        /// <summary>
+        /// Add an article log entry to the AWB Log tab
+        /// </summary>
+        /// <param name="Skipped">True if the article was skipped and needs to be added to the Skipped list on the AWB log tab, false if it should be added to the Saved articles list</param>
+        /// <param name="LogListener">An AWBLogListener object - basically an object inherited from ListViewItem, which implements IMyTraceListener, and which represents a wiki article for logging purposes.</param>
         void AddLogItem(bool Skipped, AWBLogListener LogListener);
+
+        /// <summary>
+        /// Turn off any logging to files
+        /// </summary>
         void TurnOffLogging();
         void ShowHelp(string URL);
         void ShowHelpEnWiki(string Article);
 
         void Start(IAWBPlugin sender);
-        void Save(IAWBPlugin sender);
-        void SkipPage(IAWBPlugin sender, string reason);
-        void Stop(IAWBPlugin sender);
-        void GetDiff(IAWBPlugin sender);
-        void GetPreview(IAWBPlugin sender);
-
         void Start(string sender);
-        void Save(string sender);
-        void SkipPage(string sender, string reason);
+        void Stop(IAWBPlugin sender);
         void Stop(string sender);
+        void Save(IAWBPlugin sender);
+        void Save(string sender);
+
+        /// <summary>
+        /// For the purposes of the logging tab, mark the page as Skipped and provide a reason.
+        /// </summary>
+        /// <remarks>ProcessArticleEventArgs.Skip needs to be set to True if you want AWB to actually skip the article.
+        /// 
+        ///  To write to all active log listeners, including logging to file listeners, use methods from IAutoWikiBrowser.TraceManager.</remarks>
+        /// <param name="sender">A reference to the calling plugin</param>
+        /// <param name="reason">The reason for skipping</param>
+        void SkipPage(IAWBPlugin sender, string reason);
+
+        /// <summary>
+        /// For the purposes of the logging tab, mark the page as Skipped and provide a reason.
+        /// </summary>
+        /// <remarks>ProcessArticleEventArgs.Skip needs to be set to True if you want AWB to actually skip the article.
+        /// 
+        /// To write to all active log listeners, including logging to file listeners, use methods from IAutoWikiBrowser.TraceManager.</remarks>
+        /// <param name="sender">The calling plugin's name</param>
+        /// <param name="reason">The reason for skipping</param>
+        void SkipPage(string sender, string reason);
+
+        void GetDiff(IAWBPlugin sender);
         void GetDiff(string sender);
+        void GetPreview(IAWBPlugin sender);
         void GetPreview(string sender);
 
         bool CheckStatus(bool Login);
@@ -115,13 +197,39 @@ namespace WikiFunctions.Plugin
     public delegate void GetLogUploadLocationsEvent(IAutoWikiBrowser Sender, List<WikiFunctions.Logging.Uploader.LogEntry> locations);
 
     /* Members may be added to this interface, but not removed unless absolutely necessary. */
+    /// <summary>
+    /// Sent by AWB to plugins in ProcessArticle()
+    /// </summary>
     public interface ProcessArticleEventArgs
     {
+        /// <summary>
+        /// The article text
+        /// </summary>
+        /// <remarks>Read only. The plugin should return the processed article text as the return value of ProcessArticle()</remarks>
         string ArticleText { get; }
+
+        /// <summary>
+        /// The article title
+        /// </summary>
+        /// <remarks>Read only.</remarks>
         string ArticleTitle { get; }
+
+        /// <summary>
+        /// Edit summary
+        /// </summary>
+        /// <remarks>Read/write.</remarks>
         string EditSummary { get; set; }
+
+        /// <summary>
+        /// Article's namespace
+        /// </summary>
+        /// <remarks>Read only.</remarks>
         int NameSpaceKey { get; }
         //IMyTraceListener AWBLogItem { get; }
+
+        /// <summary>
+        /// Set to True if AWB should skip this article
+        /// </summary>
         bool Skip { get; set; }
     }
 
