22e71d5466f09704ea04e27ff62c4ff267b8624e
Sam Reed <sam@reedyboy.net>
2007-08-19 21:10:23 +0000

    Fix some minor lblProject bugs - Species, commons and meta shown wrongly with language prefix
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@1566 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Main.cs
+++ AWB/Main.cs
@@ -1745,7 +1745,10 @@ font-size: 150%;'>No changes</h2><p>Press the ""Ignore"" button below to skip to
                 chkAutoTagger.Checked = false;
                 chkGeneralFixes.Checked = false;
             }
-            if (Variables.Project != ProjectEnum.custom && Variables.Project != ProjectEnum.wikia) lblProject.Text = Variables.LangCode.ToString().ToLower() + "." + Variables.Project;
+            if (Variables.Project != ProjectEnum.custom && Variables.Project != ProjectEnum.wikia && Variables.Project != ProjectEnum.commons && Variables.Project != ProjectEnum.meta && Variables.Project != ProjectEnum.species)
+                lblProject.Text = Variables.LangCode.ToString().ToLower() + "." + Variables.Project;
+            else if (Variables.Project == ProjectEnum.commons || Variables.Project == ProjectEnum.meta || Variables.Project == ProjectEnum.species)
+                lblProject.Text = Variables.Project.ToString();
             else lblProject.Text = Variables.URL;
         }
 
