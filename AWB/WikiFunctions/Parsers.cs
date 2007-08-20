b60d8370a6204d952e0db9e87056b6c5ea55c25e
Max Semenik <maxsem.wiki@gmail.com>
2007-08-20 21:15:59 +0000

    more sensitive regexMainArticle
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@1573 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Parsers.cs
+++ AWB/WikiFunctions/Parsers.cs
@@ -617,7 +617,7 @@ a='" + a + "',  b='" + b + "'", "StickyLinks error");
             return ArticleText;
         }
 
-        static Regex regexMainArticle = new Regex(@"Main article:\s?'{0,3}\[\[(.*?)\]\]'{0,3}\.?", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        static Regex regexMainArticle = new Regex(@"^.*Main article:\s?'{0,3}\[\[(.*?)\]\]\.?'{0,3}\.?", RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Multiline);
         /// <summary>
         /// Fixes instances of ''Main Article: xxx'' to use {{main|xxx}}
         /// </summary>
