a14d38a8cec9ac0e927556c57593eb5dab06d198
Sam Reed <sam@reedyboy.net>
2007-09-09 19:28:05 +0000

    Bump AWB & WF to 4.0.0.0
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@1663 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Properties/AssemblyInfo.cs
+++ AWB/AWB/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AutoWikiBrowser")]
-[assembly: AssemblyCopyright("Copyright ©  2007 General Public License V2")]
+[assembly: AssemblyCopyright("Copyright © 2007 General Public License V2/V3")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.0.0.1")]
-[assembly: AssemblyFileVersion("4.0.0.1")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
