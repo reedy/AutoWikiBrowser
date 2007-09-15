c988f389939a5d761826b7010cf366597650f408
Jogers <jogers@users.sourceforge.net>
2007-09-15 20:32:25 +0000

    improved previous edit by disabling the spinner until the "enabled" check box is checked
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@1690 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Main.cs
+++ AWB/AWB/Main.cs
@@ -1522,6 +1522,7 @@ font-size: 150%;'>No changes</h2><p>Press the ""Ignore"" button below to skip to
             txtAppendMessage.Enabled = chkAppend.Checked;
             rdoAppend.Enabled = chkAppend.Checked;
             rdoPrepend.Enabled = chkAppend.Checked;
+            udNewlineChars.Enabled = chkAppend.Checked;
         }
 
         private void wordWrapToolStripMenuItem1_Click(object sender, EventArgs e)
