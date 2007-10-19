92c65d0322db558c0b010b6d90df3648cac15a7b
Sam Reed <sam@reedyboy.net>
2007-10-19 18:40:20 +0000

    Addition of setting of Checked of EditToolBar
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@1776 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Main.cs
+++ AWB/Main.cs
@@ -3674,6 +3674,8 @@ font-size: 150%;'>No changes</h2><p>Press the ""Ignore"" button below to skip to
                 imgBold.Visible = imgExtlink.Visible = imgHr.Visible = imgItalics.Visible = imgLink.Visible =
                 imgMath.Visible = imgNowiki.Visible = imgRedirect.Visible = imgStrike.Visible = imgSub.Visible =
                 imgSup.Visible = value;
+
+                showHideEditToolbarToolStripMenuItem.Checked = value;
             }
             get { return imgSup.Visible; }
         }
