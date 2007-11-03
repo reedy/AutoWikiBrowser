f9caf2bb7d867c45bb2af21e2d10fa7875187117
Max Semenik <maxsem.wiki@gmail.com>
2007-11-03 20:15:07 +0000

    bump to 4.1.4.0
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@1817 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Properties/AssemblyInfo.cs
+++ AWB/AWB/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.1.3.1")]
-[assembly: AssemblyFileVersion("4.1.3.1")]
+[assembly: AssemblyVersion("4.1.4.0")]
+[assembly: AssemblyFileVersion("4.1.4.0")]
