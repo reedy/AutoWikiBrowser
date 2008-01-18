bbb97e62d9025df01a1a4b74c495826418a6a8f0
Steven Kennedy <kingboyk@users.sourceforge.net>
2008-01-18 14:35:23 +0000

    Improved RegexTester some more: CTRL+A handler for text boxes; messagebox to report regex errors; enlarged textbox for the Find regex; checkboxes put into a box; startup centre position
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2017 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/RegexTester.Designer.cs
+++ AWB/RegexTester.Designer.cs
@@ -29,6 +29,7 @@ namespace AutoWikiBrowser
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegexTester));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.Find = new System.Windows.Forms.TextBox();
@@ -37,18 +38,19 @@ namespace AutoWikiBrowser
             this.Source = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.ResultText = new System.Windows.Forms.TextBox();
-            this.Multiline = new System.Windows.Forms.CheckBox();
-            this.Ignorecase = new System.Windows.Forms.CheckBox();
             this.Captures = new System.Windows.Forms.TreeView();
             this.FindBtn = new System.Windows.Forms.Button();
             this.Status = new System.Windows.Forms.ToolStripStatusLabel();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.label4 = new System.Windows.Forms.Label();
-            this.explicitcapture = new System.Windows.Forms.CheckBox();
-            this.label5 = new System.Windows.Forms.Label();
             this.Singleline = new System.Windows.Forms.CheckBox();
+            this.explicitcapture = new System.Windows.Forms.CheckBox();
+            this.Ignorecase = new System.Windows.Forms.CheckBox();
+            this.Multiline = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -64,7 +66,7 @@ namespace AutoWikiBrowser
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Location = new System.Drawing.Point(12, 90);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(50, 13);
             this.label2.TabIndex = 1;
@@ -76,28 +78,31 @@ namespace AutoWikiBrowser
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.Find.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Find.Location = new System.Drawing.Point(75, 6);
+            this.Find.Multiline = true;
             this.Find.Name = "Find";
-            this.Find.Size = new System.Drawing.Size(415, 22);
+            this.Find.Size = new System.Drawing.Size(401, 78);
             this.Find.TabIndex = 2;
             this.toolTip1.SetToolTip(this.Find, "The regular expression to find");
             this.Find.TextChanged += new System.EventHandler(this.ConditionsChanged);
+            this.Find.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPressHandler);
             // 
             // Replace
             // 
             this.Replace.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.Replace.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Replace.Location = new System.Drawing.Point(75, 32);
+            this.Replace.Location = new System.Drawing.Point(75, 90);
             this.Replace.Name = "Replace";
-            this.Replace.Size = new System.Drawing.Size(415, 22);
+            this.Replace.Size = new System.Drawing.Size(401, 22);
             this.Replace.TabIndex = 3;
             this.toolTip1.SetToolTip(this.Replace, "The expression for doing replacements");
             this.Replace.TextChanged += new System.EventHandler(this.ConditionsChanged);
+            this.Replace.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPressHandler);
             // 
             // ReplaceBtn
             // 
             this.ReplaceBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.ReplaceBtn.Location = new System.Drawing.Point(353, 57);
+            this.ReplaceBtn.Location = new System.Drawing.Point(577, 90);
             this.ReplaceBtn.Name = "ReplaceBtn";
             this.ReplaceBtn.Size = new System.Drawing.Size(75, 23);
             this.ReplaceBtn.TabIndex = 4;
@@ -111,20 +116,21 @@ namespace AutoWikiBrowser
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.Source.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Source.Location = new System.Drawing.Point(12, 80);
+            this.Source.Location = new System.Drawing.Point(72, 120);
             this.Source.Multiline = true;
             this.Source.Name = "Source";
             this.Source.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.Source.Size = new System.Drawing.Size(659, 160);
+            this.Source.Size = new System.Drawing.Size(596, 160);
             this.Source.TabIndex = 5;
             this.toolTip1.SetToolTip(this.Source, "Enter or paste in the text to be searched here");
             this.Source.TextChanged += new System.EventHandler(this.ConditionsChanged);
+            this.Source.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPressHandler);
             // 
             // label3
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 251);
+            this.label3.Location = new System.Drawing.Point(12, 290);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(40, 13);
             this.label3.TabIndex = 7;
@@ -137,54 +143,28 @@ namespace AutoWikiBrowser
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.ResultText.BackColor = System.Drawing.SystemColors.Window;
             this.ResultText.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ResultText.Location = new System.Drawing.Point(12, 274);
+            this.ResultText.Location = new System.Drawing.Point(73, 286);
             this.ResultText.Multiline = true;
             this.ResultText.Name = "ResultText";
             this.ResultText.ReadOnly = true;
             this.ResultText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.ResultText.Size = new System.Drawing.Size(659, 164);
+            this.ResultText.Size = new System.Drawing.Size(595, 164);
             this.ResultText.TabIndex = 10;
-            // 
-            // Multiline
-            // 
-            this.Multiline.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.Multiline.AutoSize = true;
-            this.Multiline.Location = new System.Drawing.Point(496, 31);
-            this.Multiline.Name = "Multiline";
-            this.Multiline.Size = new System.Drawing.Size(68, 17);
-            this.Multiline.TabIndex = 11;
-            this.Multiline.Text = "MultiLine";
-            this.toolTip1.SetToolTip(this.Multiline, "Multiline mode. Changes the meaning of ^ and $ so they match at the beginning and" +
-                    " end, respectively, of any line, and not just the beginning and end of the entir" +
-                    "e string");
-            this.Multiline.UseVisualStyleBackColor = true;
-            // 
-            // Ignorecase
-            // 
-            this.Ignorecase.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.Ignorecase.AutoSize = true;
-            this.Ignorecase.Location = new System.Drawing.Point(496, 57);
-            this.Ignorecase.Name = "Ignorecase";
-            this.Ignorecase.Size = new System.Drawing.Size(80, 17);
-            this.Ignorecase.TabIndex = 12;
-            this.Ignorecase.Text = "IgnoreCase";
-            this.toolTip1.SetToolTip(this.Ignorecase, "Specifies case-insensitive matching");
-            this.Ignorecase.UseVisualStyleBackColor = true;
             // 
             // Captures
             // 
             this.Captures.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.Captures.Location = new System.Drawing.Point(12, 274);
+            this.Captures.Location = new System.Drawing.Point(72, 286);
             this.Captures.Name = "Captures";
-            this.Captures.Size = new System.Drawing.Size(659, 164);
+            this.Captures.Size = new System.Drawing.Size(596, 164);
             this.Captures.TabIndex = 13;
             this.toolTip1.SetToolTip(this.Captures, "Displays the result");
             // 
             // FindBtn
             // 
             this.FindBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.FindBtn.Location = new System.Drawing.Point(257, 57);
+            this.FindBtn.Location = new System.Drawing.Point(496, 90);
             this.FindBtn.Name = "FindBtn";
             this.FindBtn.Size = new System.Drawing.Size(75, 23);
             this.FindBtn.TabIndex = 14;
@@ -210,59 +190,87 @@ namespace AutoWikiBrowser
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 64);
+            this.label4.Location = new System.Drawing.Point(12, 129);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.Size = new System.Drawing.Size(45, 26);
             this.label4.TabIndex = 15;
-            this.label4.Text = "Text to search :";
+            this.label4.Text = "Text to\r\nsearch :";
             this.toolTip1.SetToolTip(this.label4, "Enter or paste in the text to be searched here");
-            // 
-            // explicitcapture
-            // 
-            this.explicitcapture.AutoSize = true;
-            this.explicitcapture.Location = new System.Drawing.Point(583, 57);
-            this.explicitcapture.Name = "explicitcapture";
-            this.explicitcapture.Size = new System.Drawing.Size(96, 17);
-            this.explicitcapture.TabIndex = 16;
-            this.explicitcapture.Text = "ExplicitCapture";
-            this.explicitcapture.UseVisualStyleBackColor = true;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(534, 10);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(77, 13);
-            this.label5.TabIndex = 17;
-            this.label5.Text = "RegexOptions:";
-            this.toolTip1.SetToolTip(this.label5, ".net RegexOptions:");
             // 
             // Singleline
             // 
-            this.Singleline.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Singleline.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.Singleline.AutoSize = true;
-            this.Singleline.Location = new System.Drawing.Point(583, 31);
+            this.Singleline.Location = new System.Drawing.Point(90, 20);
             this.Singleline.Name = "Singleline";
             this.Singleline.Size = new System.Drawing.Size(75, 17);
-            this.Singleline.TabIndex = 18;
+            this.Singleline.TabIndex = 22;
             this.Singleline.Text = "SingleLine";
             this.toolTip1.SetToolTip(this.Singleline, "Specifies single-line mode. Changes the meaning of the dot (.) so it matches ever" +
                     "y character (instead of every character except \\n)");
             this.Singleline.UseVisualStyleBackColor = true;
+            // 
+            // explicitcapture
+            // 
+            this.explicitcapture.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.explicitcapture.AutoSize = true;
+            this.explicitcapture.Location = new System.Drawing.Point(90, 43);
+            this.explicitcapture.Name = "explicitcapture";
+            this.explicitcapture.Size = new System.Drawing.Size(96, 17);
+            this.explicitcapture.TabIndex = 21;
+            this.explicitcapture.Text = "ExplicitCapture";
+            this.toolTip1.SetToolTip(this.explicitcapture, resources.GetString("explicitcapture.ToolTip"));
+            this.explicitcapture.UseVisualStyleBackColor = true;
+            // 
+            // Ignorecase
+            // 
+            this.Ignorecase.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Ignorecase.AutoSize = true;
+            this.Ignorecase.Location = new System.Drawing.Point(6, 43);
+            this.Ignorecase.Name = "Ignorecase";
+            this.Ignorecase.Size = new System.Drawing.Size(80, 17);
+            this.Ignorecase.TabIndex = 20;
+            this.Ignorecase.Text = "IgnoreCase";
+            this.toolTip1.SetToolTip(this.Ignorecase, "Specifies case-insensitive matching");
+            this.Ignorecase.UseVisualStyleBackColor = true;
+            // 
+            // Multiline
+            // 
+            this.Multiline.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Multiline.AutoSize = true;
+            this.Multiline.Location = new System.Drawing.Point(6, 20);
+            this.Multiline.Name = "Multiline";
+            this.Multiline.Size = new System.Drawing.Size(68, 17);
+            this.Multiline.TabIndex = 19;
+            this.Multiline.Text = "MultiLine";
+            this.toolTip1.SetToolTip(this.Multiline, "Multiline mode. Changes the meaning of ^ and $ so they match at the beginning and" +
+                    " end, respectively, of any line, and not just the beginning and end of the entir" +
+                    "e string");
+            this.Multiline.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.Singleline);
+            this.groupBox1.Controls.Add(this.explicitcapture);
+            this.groupBox1.Controls.Add(this.Ignorecase);
+            this.groupBox1.Controls.Add(this.Multiline);
+            this.groupBox1.Location = new System.Drawing.Point(482, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(189, 69);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "RegexOptions";
             // 
             // RegexTester
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(683, 479);
-            this.Controls.Add(this.Singleline);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.explicitcapture);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.FindBtn);
             this.Controls.Add(this.Captures);
-            this.Controls.Add(this.Ignorecase);
-            this.Controls.Add(this.Multiline);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.Source);
@@ -277,11 +285,14 @@ namespace AutoWikiBrowser
             this.MinimumSize = new System.Drawing.Size(699, 513);
             this.Name = "RegexTester";
             this.ShowIcon = false;
-            this.Text = "Test regexes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AWB Regex Tester";
             this.toolTip1.SetToolTip(this, "The expression for doing replacements");
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RegexTester_KeyPress);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -297,17 +308,17 @@ namespace AutoWikiBrowser
         private System.Windows.Forms.TextBox Source;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox ResultText;
-        private System.Windows.Forms.CheckBox Multiline;
-        private System.Windows.Forms.CheckBox Ignorecase;
         private System.Windows.Forms.TreeView Captures;
         private System.Windows.Forms.Button FindBtn;
         private System.Windows.Forms.ToolStripStatusLabel Status;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.CheckBox explicitcapture;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.CheckBox Singleline;
+        private System.Windows.Forms.CheckBox explicitcapture;
+        private System.Windows.Forms.CheckBox Ignorecase;
+        private System.Windows.Forms.CheckBox Multiline;
     }
 }
 
