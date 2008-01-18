89b1ba2427dc9e458a2eb8deedb9ffd9be3ff2bb
Sam Reed <sam@reedyboy.net>
2008-01-18 21:20:44 +0000

    Remove some duplicated code that is already sorted in FixSyntax
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@2020 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/RegexTester.cs
+++ AWB/AWB/RegexTester.cs
@@ -122,9 +122,7 @@ namespace AutoWikiBrowser
                 Regex r = new Regex(Find.Text, Options);
                 MatchCollection matches = r.Matches(Source.Text.Replace("\r\n", "\n"));
                 foreach (Match m in matches)
-                //for(int i = 0; i < matches.Count; i++)
                 {
-                    //Match m = matches[i];
                     TreeNode n = Captures.Nodes.Add(ReplaceNewLines(m.Value));
                     foreach (Group g in m.Groups)
                     { // TODO: Is there any way to get the name of the group when explicit capture is on?
