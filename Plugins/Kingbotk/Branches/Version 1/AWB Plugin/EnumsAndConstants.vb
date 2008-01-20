b2726ce0ef0244dfeffe3792f01c7642d763b37d
Steven Kennedy <kingboyk@users.sourceforge.net>
2008-01-20 15:59:24 +0000

    Tidy up regexes, constants and enums
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2824 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/Kingbotk/Branches/Version 1/AWB Plugin/EnumsAndConstants.vb
+++ Plugins/Kingbotk/Branches/Version 1/AWB Plugin/EnumsAndConstants.vb
@@ -6,7 +6,6 @@ Namespace AutoWikiBrowser.Plugins.SDKSoftware.Kingbotk
         SkipBadTag
         SkipRegex
     End Enum
-
     Friend Enum Classification As Integer
         Unassessed = 0
         Stub
@@ -27,8 +26,45 @@ Namespace AutoWikiBrowser.Plugins.SDKSoftware.Kingbotk
         NA
         Code = 100
     End Enum
+    Friend Enum ProcessTalkPageMode As Integer
+        Normal
+        ManualAssessment
+        NonStandardTalk
+    End Enum
 
     Friend Module Constants
+        ' Regular expression strings:
+        Friend Const TemplatePrefix As String = "\{\{\s*(?<tl>template *:)?\s*"
+        Friend Const conRegexpLeft As String = TemplatePrefix & "(?<tlname>" ' put "(?<!<nowiki[\n\r]*>\s*)" at the start to ignore nowiki, but was difficult to get secondchanceregex adapted (without becoming too strict) so I gave up. It seemed the engine was trying to do it's best to *avoid* matching this negative group
+        Friend Const conRegexpRight As String = _
+           ")\b\s*((([\s\|]*|\s*\|\s*(?<parm>[^}{|\s=]*))\s*)+(=\s*(?<val>[^}{|\n\r]*?)\s*)?)*\}\}\s*" ' put "\s(?!</nowiki[\n\r]*>)" at the end to ignore nowiki
+        ' Don't delete these just yet:
+        ' Last known good before loosened up to accept null parameters ("|||")
+        '")\b\s*((\s*\|\s*(?<parm>[^}{|\s=]*)\s*)+(=\s*(?<val>[^}{|\n\r]*)\s*)?)*\}\}\s*"
+        ' This version ignored stray vertical bars but would freeze up at with some text like the following:
+        ' )\b\s*((\s*\|\s*([\|\s]*|(?<parm>[^}{|\s=]*)\s*)+(=\s*(?<val>[^}{|\n\r]*?)\s*)?))*\}\}\s*
+        '{{WPBiography
+        '|small = 
+        '|living = no
+        '|class = Stub
+        '|priority = Low
+        '|needs-infobox = no
+        '<!--|listas = Cech, Martin == remove sneaky, hidden missorting-->
+        '|needs-photo = yes
+        '|sports-work-group = yes
+        '}}
+        '{{Ice hockey|class=Stub}}
+        '{{DEFAULTSORT:Cech, Martin}}
+        Friend Const conRegexpRightNotStrict As String = ")\b" '")\b[^}]*"
+        Friend Const conRegexpOptions As RegexOptions = RegexOptions.Compiled Or RegexOptions.Multiline Or _
+           RegexOptions.IgnoreCase Or RegexOptions.ExplicitCapture
+
+        ' Identifiers:
         Friend Const Biography As String = "Biography"
+        Friend Const conWikiPlugin As String = "[[User:Kingbotk/P|Kingbotk Plugin++]]"
+        Friend Const conWikiPluginBrackets As String = "(" & conWikiPlugin & ") "
+        Friend Const conAWBPluginName As String = "Kingbotk Plugin"
+        'Placeholders:
+        Friend Const conTemplatePlaceholder As String = "{{xxxTEMPLATExxx}}"
     End Module
 End Namespace
