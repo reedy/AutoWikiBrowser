d872a9f1e11a5048106b01476357f8268cbfe283
Max Semenik <maxsem.wiki@gmail.com>
2008-02-03 10:12:56 +0000

    add unit tests
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2080 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ UnitTests/ParserTests.cs
@@ -0,0 +1,61 @@
+/*
+AWB unit tests
+Copyright (C) 2008 Max Semenik
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+Portions Copyright © 2002-2007 Charlie Poole or 
+Copyright © 2002-2004 James W. Newkirk, Michael C. Two, Alexei A. Vorontsov or 
+Copyright © 2000-2002 Philip A. Craig
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using WikiFunctions;
+using WikiFunctions.Parse;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class FootnotesTests
+    {
+        Parsers parser = new Parsers();
+
+        [Test]
+        public void PrecededByEqualSign()
+        {
+            Assert.That(parser.FixFootnotes("a=<ref>b</ref>"), Text.DoesNotContain("\n"));
+        }
+    }
+
+    [TestFixture]
+    public class LinkTests
+    {
+        Parsers parser = new Parsers();
+
+        [Test]
+        public void DontFixTrailedLinks()
+        {
+            bool dummy;
+
+            //System.Threading.Thread.CurrentThread.SetApartmentState(System.Threading.ApartmentState.STA);
+            Assert.AreEqual(parser.FixLinks("[[a ]]b", out dummy), "[[a ]]b");
+        }
+    }
+}
