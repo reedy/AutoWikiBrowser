735339654775826babf2fe924df6e4d8ee6e904d
Sam Reed <sam@reedyboy.net>
2008-02-26 22:03:54 +0000

    Placeholders for Stats.php
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@2134 5227b50d-9349-4574-b055-4b0a7525e11c

--- UsageStats/includes/Stats.php
+++ UsageStats/includes/Stats.php
@@ -18,15 +18,78 @@ Some queries we might want on a stats page:
 function stats(){
 	$db=new DB();
 	$db->db_connect();
-		
-	$retval = $db->db_mysql_query("SELECT COUNT(sessionid) AS nosessions, SUM(saves) AS totalsaves FROM sessions", 'stats', 'STATS') ;
+	
+	//Number of sessions, Number of saves
+	$query = "SELECT COUNT(sessionid) AS nosessions, SUM(saves) AS totalsaves FROM sessions";
+	$retval = $db->db_mysql_query($query, 'stats', 'STATS') ;
 
 	while($row = mysqli_fetch_array($retval, MYSQL_ASSOC))
 	{
 		echo "No of Sessions: {$row['nosessions']}<br />";
-
 		echo "No of Saves: {$row['totalsaves']}<br />";
-	} 
-}
+	}
+	
+	//Unique users count (username/wiki)
+	/*$query = "";
+	$retval = $db->db_mysql_query($query, 'stats', 'STATS') ;
 
+	while($row = mysqli_fetch_array($retval, MYSQL_ASSOC))
+	{
+		echo "No of : {$row['']}<br />";
+	}*/
+		
+	//Unique username count
+	$query = "SELECT COUNT(DISTINCT User) AS usercount FROM lkpUsers";
+	$retval = $db->db_mysql_query($query, 'stats', 'STATS') ;
+
+	while($row = mysqli_fetch_array($retval, MYSQL_ASSOC))
+	{
+		echo "No of Unique Users: {$row['usercount']}<br />";
+	}
+	
+	//Sessions per sites
+	/*$query = "";
+	$retval = $db->db_mysql_query($query, 'stats', 'STATS') ;
+
+	while($row = mysqli_fetch_array($retval, MYSQL_ASSOC))
+	{
+		echo "No of : {$row['']}<br />"
+	}*/
+	
+	//Saves per sites
+	/*$query = "";
+	$retval = $db->db_mysql_query($query, 'stats', 'STATS') ;
+
+	while($row = mysqli_fetch_array($retval, MYSQL_ASSOC))
+	{
+		echo "No of : {$row['']}<br />"
+	}*/
+	
+	//Most popular OS of last x days (unique users)
+	/*$query = "";
+	$retval = $db->db_mysql_query($query, 'stats', 'STATS') ;
+
+	while($row = mysqli_fetch_array($retval, MYSQL_ASSOC))
+	{
+		echo "No of : {$row['']}<br />"
+	}*/
+	
+	//Number of plugins known
+	/*$query = "";
+	$retval = $db->db_mysql_query($query, 'stats', 'STATS') ;
+
+	while($row = mysqli_fetch_array($retval, MYSQL_ASSOC))
+	{
+		echo "No of : {$row['']}<br />"
+	}*/
+	
+	//Number of saves by language (culture)
+	/*$query = "";
+	$retval = $db->db_mysql_query($query, 'stats', 'STATS') ;
+
+	while($row = mysqli_fetch_array($retval, MYSQL_ASSOC))
+	{
+		echo "No of : {$row['']}<br />"
+	}*/
+}
 ?>
