a4c44af2fa13771b8218dd87ef0d0101f02a9302
Sam Reed <sam@reedyboy.net>
2008-03-02 20:43:01 +0000

    Tweaks to IFD and CFD to match coding guidelines/reccomendations
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2179 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/CFD/Properties/AssemblyInfo.cs
+++ Plugins/CFD/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CFD")]
-[assembly: AssemblyCopyright("Copyright ©  2006")]
+[assembly: AssemblyCopyright("Copyright ©  2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
