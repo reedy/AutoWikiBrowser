d0a9c5f7ac954cd6f56c266d842f9e03981e463d
Sam Reed <sam@reedyboy.net>
2008-03-07 18:45:46 +0000

    Bump AWBUpdater to 1.4.0.0
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@2239 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWBUpdater/Properties/AssemblyInfo.cs
+++ AWB/AWBUpdater/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.5.4")]
-[assembly: AssemblyFileVersion("1.3.5.4")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
