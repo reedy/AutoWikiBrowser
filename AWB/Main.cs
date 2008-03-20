1db5909b0d963d34115929b16d30526c2085b4e7
Sam Reed <sam@reedyboy.net>
2008-03-20 00:46:18 +0000

    Extra if to get Release to compile
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2284 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Main.cs
+++ AWB/Main.cs
@@ -1853,7 +1853,9 @@ font-size: 150%;'>No changes</h2><p>Press the ""Ignore"" button below to skip to
             profileTyposToolStripMenuItem.Visible = true;
             toolStripSeparator29.Visible = true;
 
+#if DEBUG
             Variables.Profiler = new Profiler("profiling.txt", true);
+#endif
         }
 
         #endregion
