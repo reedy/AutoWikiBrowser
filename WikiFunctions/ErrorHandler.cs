6a65184944381025c7c7f3c7e5bef4ee49d8cd89
Max Semenik <maxsem.wiki@gmail.com>
2008-03-29 18:39:47 +0000

    ErrorHandler tweaks
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2322 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/ErrorHandler.cs
+++ WikiFunctions/ErrorHandler.cs
@@ -84,8 +84,13 @@ namespace WikiFunctions
 
                     handler.txtError.Text = ex.Message;
 
-                    handler.txtDetails.Text = "{{AWB bug\r\n | status      = new <!-- when fixed replace with \"fixed\" -->\r\n | description = <table><tr><td>Exception:<td><code>" + ex.GetType().Name + "</code><tr><td>Message:<td><code>" +
-                        ex.Message + "</code><tr><td>Call stack:<td><pre>" + ex.StackTrace + "</pre>\r\nThread:" + Thread.CurrentThread.Name + "</table>\r\n~~~~\r\n | OS          = " + Environment.OSVersion.ToString() + "\r\n | version     = " + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                    string errorMessage = "{{AWB bug\r\n | status      = new <!-- when fixed replace with \"fixed\" -->\r\n | description = <table><tr><td>Exception:<td><code>" + ex.GetType().Name + "</code><tr><td>Message:<td><code>" +
+                        ex.Message + "</code><tr><td>Call stack:<td><pre>" + ex.StackTrace + "</pre>";
+
+                    if (Thread.CurrentThread.Name != "Main thread")
+                        errorMessage += "\r\nThread: " + Thread.CurrentThread.Name;
+                    
+                    errorMessage += "</table>\r\n~~~~\r\n | OS          = " + Environment.OSVersion.ToString() + "\r\n | version     = " + Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
                     if (!string.IsNullOrEmpty(CurrentArticle) &&
                         ex.StackTrace.Contains("AutoWikiBrowser.MainForm.ProcessPage("))
@@ -94,15 +99,17 @@ namespace WikiFunctions
                         if (CurrentRevision != 0 && !string.IsNullOrEmpty(LangCode) && !string.IsNullOrEmpty(Project))
                             link = "[http://" + LangCode + "." + Project + ".org/w/index.php?title=" + CurrentArticle.Replace(" ", "_") + "&oldid=" + CurrentRevision.ToString() + "]";
                         else link = "[[:" + CurrentArticle + "]]";
-                        handler.txtDetails.Text +=
+                        errorMessage +=
                             "\r\n | duplicate = [encountered while processing page ''" + link + "'']";
                     }
 
                     if (!string.IsNullOrEmpty(ListMakerText))
-                        handler.txtDetails.Text +=
+                        errorMessage +=
                             "\r\n | duplicate = '''ListMaker Text:''' " + ListMakerText;
 
-                    handler.txtDetails.Text += "\r\n}}";
+                    errorMessage += "\r\n}}";
+
+                    handler.txtDetails.Text = errorMessage;
 
                     handler.textBox1.Text = "AWB encountered " + ex.GetType().Name;
 
