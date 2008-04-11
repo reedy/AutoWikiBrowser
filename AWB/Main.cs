62ea84420478075be51ed28a749fffd0f7b78885
Sam Reed <sam@reedyboy.net>
2008-04-11 17:55:24 +0000

    Truncate edit summary to append "Using AWB" correctly
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2346 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Main.cs
+++ AWB/Main.cs
@@ -1430,8 +1430,14 @@ font-size: 150%;'>No changes</h2><p>Press the ""Ignore"" button below to skip to
         private string MakeSummary()
         {
             string tag = cmboEditSummary.Text + TheArticle.SavedSummary;
-            if (!BotMode || !chkSuppressTag.Checked ||
-                (!Variables.IsWikimediaProject && !SupressUsingAWB)) tag += " " + Variables.SummaryTag;
+            if (!BotMode || !chkSuppressTag.Checked || 
+                (!Variables.IsWikimediaProject && !SupressUsingAWB))
+            {
+                if (tag.Length >= 255)
+                    tag = tag.Substring(0, (255 - (Variables.SummaryTag.Length + 1)));
+
+                tag += " " + Variables.SummaryTag;
+            }
 
             return tag;
         }
