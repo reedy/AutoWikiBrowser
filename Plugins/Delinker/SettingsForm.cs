2f630fbc47c7b2bbad17f7427fa7461c3ed9d0c8
Max Semenik <maxsem.wiki@gmail.com>
2008-04-20 07:49:51 +0000

    added delinker plugin, not ready for production yet
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2363 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ Plugins/Delinker/SettingsForm.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DelinkerPlugin
+{
+    public partial class SettingsForm : Form
+    {
+        public SettingsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            DelinkerPlugin.Skip = Skip.Checked;
+            DelinkerPlugin.Link = Link.Text;
+            DelinkerPlugin.RemoveEmptiedSections = RemoveSections.Checked;
+        }
+
+        private void SettingsForm_Load(object sender, EventArgs e)
+        {
+            Link.Text = DelinkerPlugin.Link;
+            Skip.Checked = DelinkerPlugin.Skip;
+            RemoveSections.Checked = DelinkerPlugin.RemoveEmptiedSections;
+        }
+    }
+}
