74d9c1b2546b3a46a4527c51a3a86429cff28109
Steven Kennedy <kingboyk@users.sourceforge.net>
2008-05-11 17:52:16 +0000

    Precautionary commit, been called away
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@2418 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ AWB/Plugins/Server/Server.cs
@@ -0,0 +1,46 @@
+﻿/*
+(C) 2008 Stephen Kennedy (Kingboyk) http://www.sdk-software.com/
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoWikiBrowser.Plugins.Server
+{
+    internal enum ServerResponseCode
+    {
+        HELLO = 200,
+        REQUIRE = 201,
+        BYE = 210,
+        OK = 250,
+        PROTOCOL = 260,
+        READY = 270,
+        BUSY = 500,
+        REJECTED = 501,
+        UNAUTHORISED = 502,
+        DISCON = 510
+    }
+
+    internal static class Server
+    {
+        private static string SendResponse(ServerResponseCode ResponseCode, string Data)
+        {
+            return ResponseCode + " " + ResponseCode.ToString() + Data;
+        }
+    }
+}
