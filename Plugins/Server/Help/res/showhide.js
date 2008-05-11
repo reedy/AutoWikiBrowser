95d431a9d628f4b3905bce120f7f9f161f5edfe7
Steven Kennedy <kingboyk@users.sourceforge.net>
2008-05-11 18:53:26 +0000

    Plugins: Code simplification. Typo fix in JS file/disable debugging.
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2419 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/Server/Help/res/showhide.js
+++ Plugins/Server/Help/res/showhide.js
@@ -1,9 +1,8 @@
-// From http://blog.movalog.com/a/javascript-toggle-visibility/
 function toggle_visibility(id) {
-var e = document.getElementById(id);
-alert (e.display);
-if(e.style.display == 'none')
-  e.style.display = 'block';
-else
-  e.style.display = 'none';
+	var e = document.getElementById(id);
+	// alert (e.style.display); // why is this null at first call despite me setting the property in the .css?
+	if(e.style.display == 'none')
+	  e.style.display = 'block';
+	else
+	  e.style.display = 'none';
 }
