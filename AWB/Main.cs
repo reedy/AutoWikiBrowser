730a3eac638203b41dddbb4e5893c429c2cf6e23
Steven Kennedy <kingboyk@users.sourceforge.net>
2008-05-12 08:47:12 +0000

    Put in another splash message where for me AWB is taking a while to proceed
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2430 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Main.cs
+++ AWB/Main.cs
@@ -1936,6 +1936,7 @@ font-size: 150%;'>No changes</h2><p>Press the ""Ignore"" button below to skip to
 
         private void SetProject(LangCodeEnum code, ProjectEnum project, string customProject)
         {
+            splash.SetProgress(81);
             try
             {
                 //set namespaces
