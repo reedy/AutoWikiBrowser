8cfc9d965c27c4353450d17ff3d2815d077c6a70
Steven Kennedy <kingboyk@users.sourceforge.net>
2008-05-12 11:14:31 +0000

    More work on Server plugin
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@2432 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Plugins/Server/Server.cs
+++ AWB/Plugins/Server/Server.cs
@@ -1,5 +1,6 @@
 ﻿/*
-(C) 2008 Stephen Kennedy (Kingboyk) http://www.sdk-software.com/
+(C) 2008 Stephen Kennedy (Kingboyk) http://www.sdk-software.com
+ With some Net.Sockets help and snippets from http://www.gnu.org/projects/dotgnu/pnetlib-doc/System/Net/Sockets/Socket.html
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,7 +19,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace AutoWikiBrowser.Plugins.Server
 {
@@ -38,10 +42,83 @@ namespace AutoWikiBrowser.Plugins.Server
 
     internal static class Server
     {
+        // Management
+        // This sub is from http://www.gnu.org/projects/dotgnu/pnetlib-doc/System/Net/Sockets/Socket.html with modifications
+        internal static void Init(int port)
+        {
+            IAsyncResult asyncAccept = null;
+
+            try
+            {
+                string hostname = Dns.GetHostName();
+
+                // Create a listening socket:
+                Socket listenSocket =
+                    new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Bind to our preferred port on all interfaces:
+                listenSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+
+                // Start listening:
+                listenSocket.Listen(Math.Min((int)SocketOptionName.MaxConnections, 10));
+                asyncAccept = listenSocket.BeginAccept(new AsyncCallback(Server.acceptCallback), listenSocket);
+            }
+            catch (SocketException ex)
+            {
+                // TODO
+            }
+            catch
+            {
+                throw;
+            }
+
+            // could call listenSocket.EndAccept(asyncAccept) here
+            // instead of in the callback method, but since 
+            // EndAccept blocks, the behavior would be similar to 
+            // calling the synchronous Accept method
+
+            //if (writeDot(asyncAccept) == true)
+            //{
+            //    // allow time for callbacks to
+            //    // finish before the program ends 
+            //    Thread.Sleep(3000);
+            //}
+        }
+
+        internal static void Stop()
+        {
+        }
+
+        // Delegates
+        internal static void acceptCallback(IAsyncResult asyncAccept)
+        {
+        }
+
+        // Server functionality:
         private static string SendResponse(ServerResponseCode ResponseCode, string Data)
         {
             return ResponseCode + " " + ResponseCode.ToString() + Data;
         }
+
+        // Helper routines
+
+        // From http://www.gnu.org/projects/dotgnu/pnetlib-doc/System/Net/Sockets/Socket.html:
+        // times out after 20 seconds but operation continues
+        //private static bool writeDot(IAsyncResult ar)
+        //{
+        //    int i = 0;
+        //    while (ar.IsCompleted == false)
+        //    {
+        //        if (i++ > 40)
+        //        {
+        //            Console.WriteLine("Timed out.");
+        //            return false;
+        //        }
+        //        Console.Write(".");
+        //        Thread.Sleep(500);
+        //    }
+        //    return true;
+        //}
 
         /*
          * When receiving new connection, check if somebody is logged in or not, and if they are
