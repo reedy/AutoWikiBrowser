dac83c6ed06b51f0caff770a800896c1ac041d7c
Steven Kennedy <kingboyk@users.sourceforge.net>
2008-05-13 12:05:01 +0000

    Add file
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@2446 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ AWB/Plugins/Server/Server.cs
@@ -0,0 +1,39 @@
+﻿/*
+(C) 2008 Stephen Kennedy (Kingboyk) http://www.sdk-software.com
+ With some Net.Sockets help and snippets from http://www.gnu.org/projects/dotgnu/pnetlib-doc/System/Net/Sockets/Socket.html
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AutoWikiBrowser.Plugins.Server
+{
+    partial class ServerControl
+    {
+        internal static class Server
+        {
+        }
+    }
+}
