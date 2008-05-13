04bbc5e712bd2dd9616ba4f2ec8be4e218ee373a
Steven Kennedy <kingboyk@users.sourceforge.net>
2008-05-13 14:01:49 +0000

    Server plugin is starting to take shape now
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@2447 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Plugins/Server/ServerControl.cs
+++ AWB/Plugins/Server/ServerControl.cs
@@ -20,96 +20,13 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
 
 namespace AutoWikiBrowser.Plugins.Server
 {
-    internal enum ServerResponseCode
-    {
-        HELLO = 200,
-        REQUIRE = 201,
-        BYE = 210,
-        OK = 250,
-        PROTOCOL = 260,
-        READY = 270,
-        BUSY = 500,
-        REJECTED = 501,
-        UNAUTHORISED = 502,
-        DISCON = 510
-    }
-
     internal partial class ServerControl : UserControl
     {
         public ServerControl()
         { InitializeComponent(); }
-
-        private static Thread thread; // TODO: Thread management (if needed)
-
-        // Management
-        internal static void Init(int port)
-        {
-            //}
-
-            //private static void ThreadEntryPoint()
-            //{ // based on http://www.gnu.org/projects/dotgnu/pnetlib-doc/System/Net/Sockets/Socket.html
-            IAsyncResult asyncAccept = null;
-
-            try
-            {
-                string hostname = Dns.GetHostName();
-
-                // Create a listening socket:
-                Socket listenSocket =
-                    new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-                // Bind to our preferred port on all interfaces:
-                listenSocket.Bind(new IPEndPoint(IPAddress.Any, port));
-
-                // Start listening:
-                listenSocket.Listen(Math.Min((int)SocketOptionName.MaxConnections, 10));
-                asyncAccept = listenSocket.BeginAccept(new AsyncCallback(ServerControl.acceptCallback), listenSocket);
-            }
-            catch (SocketException ex)
-            {
-                // TODO
-            }
-            catch
-            {
-                throw;
-            }
-        }
-
-        internal static void Stop()
-        {
-        }
-
-        // Delegates
-        internal static void acceptCallback(IAsyncResult asyncAccept)
-        {
-        }
-
-        // Server functionality:
-        private static string SendResponse(ServerResponseCode ResponseCode, string Data)
-        {
-            return ResponseCode + " " + ResponseCode.ToString() + Data;
-        }
-
-        // Helper routines
-
-        /*
-         * When receiving new connection, check if somebody is logged in or not, and if they are
-         * issue a BUSY. Reject all other commands except FORCE LOGIN; if that is received close the
-         * earlier connection *after* the new one is authenticated.
-         * 
-         * When interracting with the command track the state of what command we receive next, and
-         * accept only that command or commands which can be issued at any time (NB: If login is required
-         * VERSION can only be processed *after* authentication).
-         * 
-         * CAN PROBABLY DO SOME OF THIS BY ADDING AND REMOVING HANDLERS DEPENDING ON STATE (vs logic).
-         * 
-         * Store details of current login and the last 5 or 10 connection attempts, and have a pop up
-         * form the user can view.
-         * 
-         * Don't forget to write to the trace listener where applicable.
-        */
     }
 }
