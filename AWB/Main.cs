348b18d181ccc4c8c2cbfd9da0b67ed223132862
Sam Reed <sam@reedyboy.net>
2008-05-22 21:15:50 +0000

    Tweak numbers to stop edit box being truncated
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2471 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Main.cs
+++ AWB/Main.cs
@@ -3732,13 +3732,13 @@ font-size: 150%;'>No changes</h2><p>Press the ""Ignore"" button below to skip to
                 {
                     if (value)
                     {
-                        txtEdit.Location = new Point(txtEdit.Location.X, txtEdit.Location.Y + 32);
-                        txtEdit.Size = new Size(txtEdit.Size.Width, txtEdit.Size.Height - 32);
+                        txtEdit.Location = new Point(txtEdit.Location.X, txtEdit.Location.Y + 30);
+                        txtEdit.Size = new Size(txtEdit.Size.Width, txtEdit.Size.Height - 30);
                     }
                     else
                     {
-                        txtEdit.Location = new Point(txtEdit.Location.X, txtEdit.Location.Y - 32);
-                        txtEdit.Size = new Size(txtEdit.Size.Width, txtEdit.Size.Height + 32);
+                        txtEdit.Location = new Point(txtEdit.Location.X, txtEdit.Location.Y - 30);
+                        txtEdit.Size = new Size(txtEdit.Size.Width, txtEdit.Size.Height + 30);
                     }
 
                     imgBold.Visible = imgExtlink.Visible = imgHr.Visible = imgItalics.Visible = imgLink.Visible =
