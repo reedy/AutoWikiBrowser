5f92838a53754a3e54cb964f2736e87ecc768bfe
Sam Reed <sam@reedyboy.net>
2008-05-22 15:48:24 +0000

    Reverting the Reversion in rev 2459
    Added migration code to fix regression caused by moving of registry keys (To be removed in a later release)
    
    Moved Recent Setting registry keys to same location as profiles
    
    
    Recent Setting code needs migrating to newer code, but works as it is
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2464 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Encryption.cs
+++ WikiFunctions/Encryption.cs
@@ -17,12 +17,12 @@ namespace WikiFunctions.Encryption
     /// This class uses a symmetric key algorithm (Rijndael/AES) to encrypt and 
     /// decrypt data. As long as encryption and decryption routines use the same
     /// parameters to generate the keys, the keys are guaranteed to be the same.
+    /// </summary>
+    public sealed class RijndaelSimple
     /// The class uses static functions with duplicate code to make it easier to
     /// demonstrate encryption and decryption logic. In a real-life application, 
     /// this may not be the most efficient way of handling encryption, so - as
     /// soon as you feel comfortable with it - you may want to redesign this class.
-    /// </summary>
-    public sealed class RijndaelSimple
     {
         private RijndaelSimple() { }
 
