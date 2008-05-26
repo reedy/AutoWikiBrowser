6623eea897b65dba76d0b5d318b7dc178d36362a
Sam Reed <sam@reedyboy.net>
2008-05-26 22:18:00 +0000

    GPL Header and a comment
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2495 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/YahooSearch/YahooSearchCore.cs
+++ Plugins/YahooSearch/YahooSearchCore.cs
@@ -1,3 +1,22 @@
+/*
+Yahoo Search Plugin
+Copyright (C) 2008 Sam Reed
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,6 +29,7 @@ using WikiFunctions.Plugin;
 
 namespace WikiFunctions.Plugins.ListMaker.YahooSearch
 {
+	//http://developer.yahoo.com/search/
     public class YahooSearchListMakerPlugin : IListMakerPlugin
     {
         internal const string appID = "3mG9u3PV34GC4rnRXJlID0_3aUb0.XVxGZYrbFcYClzQYUqtlkn0u6iXVwYVv9sW1Q--";
