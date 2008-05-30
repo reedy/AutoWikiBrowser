e51b4cb9c76c1908d21c10d7db17500853f0588e
Sam Reed <sam@reedyboy.net>
2008-05-30 14:48:39 +0000

    Update GPL Headers
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2872 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/Kingbotk/AWB Plugin/DryRunLog.vb
+++ Plugins/Kingbotk/AWB Plugin/DryRunLog.vb
@@ -1,19 +1,12 @@
 'Copyright © 2008 Stephen Kennedy (Kingboyk) http://www.sdk-software.com/
 'Copyright © 2008 Sam Reed (Reedy) http://www.reedyboy.net/
 
-'This program is free software; you can redistribute it and/or modify
-'it under the terms of the GNU General Public License as published by
-'the Free Software Foundation; either version 2 of the License, or
-'(at your option) any later version.
+'This program is free software; you can redistribute it and/or modify it under the terms of Version 2 of the GNU General Public License as published by the Free Software Foundation.
 
 'This program is distributed in the hope that it will be useful,
-'but WITHOUT ANY WARRANTY; without even the implied warranty of
-'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-'GNU General Public License for more details.
+'but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
-'You should have received a copy of the GNU General Public License
-'along with this program; if not, write to the Free Software
-'Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+'You should have received a copy of the GNU General Public License Version 2 along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 Imports System.IO
 Imports System.Text.encoding
