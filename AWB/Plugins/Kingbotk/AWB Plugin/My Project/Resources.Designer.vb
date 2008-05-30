e69e2c3e2d36efb8f7adff140e1912d572e51239
Steven Kennedy <kingboyk@users.sourceforge.net>
2008-05-30 08:00:57 +0000

    Correction
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@2870 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Plugins/Kingbotk/AWB Plugin/My Project/Resources.Designer.vb
+++ AWB/Plugins/Kingbotk/AWB Plugin/My Project/Resources.Designer.vb
@@ -64,14 +64,10 @@ Namespace My.Resources
         '''  Looks up a localized string similar to Copyright © 2008 Stephen Kennedy (Kingboyk) http://www.sdk-software.com/
         '''Copyright © 2008 Sam Reed (Reedy) http://www.reedyboy.net/
         '''
-        '''This program is free software; you can redistribute it and/or modify
-        '''it under the terms of the GNU General Public License as published by
-        '''the Free Software Foundation; either version 2 of the License, or
-        '''(at your option) any later version.
+        '''This program is free software; you can redistribute it and/or modify it under the terms of Version 2 of the GNU General Public License as published by the Free Software Foundation.
         '''
         '''This program is distributed in the hope that it will be useful,
-        '''but WITHOUT ANY WARRANTY; without even the implied warranty of
-        '''M [rest of string was truncated]&quot;;.
+        '''but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GN [rest of string was truncated]&quot;;.
         '''</summary>
         Friend ReadOnly Property GPL() As String
             Get
