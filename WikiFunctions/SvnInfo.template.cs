d54f4fe90a740d63acc5630e1a19ef9c3d1dc7b1
Sam Reed <sam@reedyboy.net>
2008-05-31 22:37:36 +0000

    One more line to SvnInfo.template.cs
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2879 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/SvnInfo.template.cs
+++ WikiFunctions/SvnInfo.template.cs
@@ -6,6 +6,7 @@
 /// If it's not, create a batch file C:\Program Files\TortoiseSVN\bin\SubWCRev.bat,
 /// consisting of single line:
 /// path_to_SubWCRev.exe_on_your_system %1 %2 %3 %4 %5 %6 %7 %8 %9
+/// Otherwise, create a Directory Junction/similar - http://www.howtogeek.com/howto/windows-vista/using-symlinks-in-windows-vista/
 //////////////////////////////////////////////////////////////////////////////////////////////////
 using System;
 
