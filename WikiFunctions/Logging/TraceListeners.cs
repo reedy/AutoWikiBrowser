b078bff29cfab852056e157d9ea823c8fb1a0955
Sam Reed <sam@reedyboy.net>
2008-06-17 18:35:46 +0000

    Comment out unused regex and also currently unused MySQLTraceListener
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@2970 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Logging/TraceListeners.cs
+++ WikiFunctions/Logging/TraceListeners.cs
@@ -216,94 +216,94 @@ namespace WikiFunctions.Logging
     }
 
     // TODO: MySQLTraceListener
-    public class MySQLTraceListener : IMyTraceListener, ITraceStatusProvider
-    {
-        public WikiFunctions.Logging.Uploader.TraceStatus TraceStatus
-        {
-            get { throw new Exception("The method or operation is not implemented."); }
-        }
+    //public class MySQLTraceListener : IMyTraceListener, ITraceStatusProvider
+    //{
+    //    public WikiFunctions.Logging.Uploader.TraceStatus TraceStatus
+    //    {
+    //        get { throw new Exception("The method or operation is not implemented."); }
+    //    }
 
-        #region IMyTraceListener Members
-            public void Close()
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //    #region IMyTraceListener Members
+    //        public void Close()
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void Flush()
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void Flush()
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void ProcessingArticle(string FullArticleTitle, Namespaces NS)
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void ProcessingArticle(string FullArticleTitle, Namespaces NS)
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void SkippedArticle(string SkippedBy, string Reason)
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void SkippedArticle(string SkippedBy, string Reason)
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void SkippedArticleBadTag(string SkippedBy, string FullArticleTitle, Namespaces NS)
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void SkippedArticleBadTag(string SkippedBy, string FullArticleTitle, Namespaces NS)
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void SkippedArticleRedlink(string SkippedBy, string FullArticleTitle, Namespaces NS)
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void SkippedArticleRedlink(string SkippedBy, string FullArticleTitle, Namespaces NS)
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void Write(string Text)
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void Write(string Text)
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void WriteArticleActionLine(string Line, string PluginName)
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void WriteArticleActionLine(string Line, string PluginName)
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void WriteArticleActionLine(string Line, string PluginName, bool VerboseOnly)
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void WriteArticleActionLine(string Line, string PluginName, bool VerboseOnly)
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void WriteBulletedLine(string Line, bool Bold, bool VerboseOnly)
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void WriteBulletedLine(string Line, bool Bold, bool VerboseOnly)
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void WriteBulletedLine(string Line, bool Bold, bool VerboseOnly, bool DateStamp)
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void WriteBulletedLine(string Line, bool Bold, bool VerboseOnly, bool DateStamp)
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void WriteComment(string Line)
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void WriteComment(string Line)
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void WriteCommentAndNewLine(string Line)
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void WriteCommentAndNewLine(string Line)
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void WriteLine(string Line)
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void WriteLine(string Line)
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public void WriteTemplateAdded(string Template, string PluginName)
-            {
-                throw new Exception("The method or operation is not implemented.");
-            }
+    //        public void WriteTemplateAdded(string Template, string PluginName)
+    //        {
+    //            throw new Exception("The method or operation is not implemented.");
+    //        }
 
-            public bool Uploadable
-            {
-                get { return false; }
-            } // Should take care of it's own upload management; as far as client is concerned it's a static log
-        #endregion
-    }
+    //        public bool Uploadable
+    //        {
+    //            get { return false; }
+    //        } // Should take care of it's own upload management; as far as client is concerned it's a static log
+    //    #endregion
+    //}
 }
 
