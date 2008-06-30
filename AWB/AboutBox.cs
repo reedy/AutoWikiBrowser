e5508f20fffb62a94d876fa05df9c4c10f086f78
Sam Reed <sam@reedyboy.net>
2008-06-30 11:42:36 +0000

    Apply Dispensers About Box changes with a few tweaks
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@3016 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AboutBox.cs
+++ AWB/AboutBox.cs
@@ -31,19 +31,23 @@ namespace AutoWikiBrowser
 {
     internal sealed partial class AboutBox : Form
     {
-        public AboutBox() { } //default
-
         public AboutBox(string ieVersion, TimeSpan time, int Edits)
         {
             InitializeComponent();
 
-            lblIEVersion.Text = "IE version: " + ieVersion;
-            lblAWBVersion.Text = "AWB Version " + Program.VersionString;
-            lblRevision.Text = "SVN revision: " + Variables.Revision;
-            textBoxDescription.Text = WikiFunctions.Controls.AboutBox.GetDetailedMessage(Assembly.GetExecutingAssembly());
+            lblAWBVersion.Text = "Version " + Program.VersionString;
+            lblRevision.Text = "SVN " + Variables.Revision;
+            lblIEVersion.Text = "Internet Explorer version: " + ieVersion;
+            txtWarning.Text = WikiFunctions.Controls.AboutBox.GetDetailedMessage(Assembly.GetExecutingAssembly());
             lblOSVersion.Text = "Windows version: " + Environment.OSVersion.Version.Major.ToString() + "." + Environment.OSVersion.Version.Minor.ToString();
-            lblNETVersion.Text = ".NET Version: " + Environment.Version.ToString();
-            lblTimeAndEdits.Text = "You have made " + Edits.ToString() + " edits in " + time.ToString();
+            lblNETVersion.Text = ".NET version: " + Environment.Version.ToString();
+
+            if (Edits == 0)
+            {
+                lblTimeAndEdits.Visible = true;
+
+                lblTimeAndEdits.Text = "You have made " + Edits.ToString() + " edits in " + time.ToString();
+            }
         }
 
         private void okButton_Click(object sender, EventArgs e)
@@ -78,7 +82,7 @@ namespace AutoWikiBrowser
         private void linkReedy_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             linkReedy.LinkVisited = true;
-            Tools.OpenENArticleInBrowser("Reedy Boy", true);
+            Tools.OpenENArticleInBrowser("Reedy", true);
         }
 
         private void linkKingboy_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -93,28 +97,28 @@ namespace AutoWikiBrowser
             Tools.OpenENArticleInBrowser("Martinp23", true);
         }
 
-        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        private void linkJogers_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkJogers.LinkVisited = true;
+            Tools.OpenENArticleInBrowser("Jogers", true);
+        }
+
+        private void linkAWBPage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             linkAWBPage.LinkVisited = true;
             Tools.OpenENArticleInBrowser("Wikipedia:AutoWikiBrowser", false);
         }
-        
-        private void linkLabel6_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+
+        private void linkBugs_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             linkBugs.LinkVisited = true;
             Tools.OpenENArticleInBrowser("Wikipedia_talk:AutoWikiBrowser/Bugs", false);
         }
 
-        private void linkLabel7_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        private void linkFeatureRequests_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             linkFeatureRequests.LinkVisited = true;
             Tools.OpenENArticleInBrowser("Wikipedia_talk:AutoWikiBrowser/Feature_requests", false);
-        }
-
-        private void linkJogers_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            linkJogers.LinkVisited = true;
-            Tools.OpenENArticleInBrowser("Jogers", true);
         }
 
         private void UsageStatsLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
