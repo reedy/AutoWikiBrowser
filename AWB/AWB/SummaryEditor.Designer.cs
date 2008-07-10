49d3dacbf13aef41f8d3fc27f21eb7d9882c3699
Sam Reed <sam@reedyboy.net>
2008-07-10 15:06:09 +0000

    Couple more code cleanups and tweaks to remove duplicate code
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@3063 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/SummaryEditor.Designer.cs
+++ AWB/AWB/SummaryEditor.Designer.cs
@@ -63,7 +63,6 @@ namespace AutoWikiBrowser
             // 
             // SummaryEditor
             // 
-            this.AcceptButton = this.btnOK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
