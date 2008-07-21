1dca753754125eb4fd3eb657fb99f0cd774c8f8e
Max Semenik <maxsem.wiki@gmail.com>
2008-07-21 06:23:34 +0000

    refactored LevelNumber, added an opportunity to cancel
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@3100 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Controls/LevelNumber.Designer.cs
+++ AWB/WikiFunctions/Controls/LevelNumber.Designer.cs
@@ -31,21 +31,22 @@ namespace WikiFunctions.Controls
             this.label1 = new System.Windows.Forms.Label();
             this.numLevels = new System.Windows.Forms.NumericUpDown();
             this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numLevels)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.Size = new System.Drawing.Size(111, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Levels:";
+            this.label1.Text = "Enter recursion depth:";
             // 
             // numLevels
             // 
-            this.numLevels.Location = new System.Drawing.Point(59, 7);
+            this.numLevels.Location = new System.Drawing.Point(171, 12);
             this.numLevels.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -67,20 +68,32 @@ namespace WikiFunctions.Controls
             // 
             // btnOk
             // 
-            this.btnOk.Location = new System.Drawing.Point(80, 33);
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(101, 53);
             this.btnOk.Name = "btnOk";
             this.btnOk.Size = new System.Drawing.Size(75, 23);
             this.btnOk.TabIndex = 2;
-            this.btnOk.Text = "&Ok";
+            this.btnOk.Text = "&OK";
             this.btnOk.UseVisualStyleBackColor = true;
-            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(192, 53);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // LevelNumber
             // 
             this.AcceptButton = this.btnOk;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(162, 63);
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(279, 88);
+            this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOk);
             this.Controls.Add(this.numLevels);
             this.Controls.Add(this.label1);
@@ -99,5 +112,6 @@ namespace WikiFunctions.Controls
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.NumericUpDown numLevels;
         private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
