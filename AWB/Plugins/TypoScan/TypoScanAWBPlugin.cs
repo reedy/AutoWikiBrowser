9216d1dae6ac08c0a638f25348bc03495e949682
Sam Reed <sam@reedyboy.net>
2008-08-01 22:59:41 +0000

    Adding TypoScan Project to Plugins Folder
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@3151 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ AWB/Plugins/TypoScan/TypoScanAWBPlugin.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using WikiFunctions;
+using WikiFunctions.Plugin;
+
+namespace WikiFunctions.Plugins.ListMaker.TypoScan
+{
+    //class TypoScanAWBPlugin : IAWBPlugin
+    //{
+    //    #region IAWBPlugin Members
+
+    //    public void Initialise(IAutoWikiBrowser sender)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public string Name
+    //    {
+    //        get { throw new NotImplementedException(); }
+    //    }
+
+    //    public string WikiName
+    //    {
+    //        get { throw new NotImplementedException(); }
+    //    }
+
+    //    public string ProcessArticle(IAutoWikiBrowser sender, ProcessArticleEventArgs eventargs)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void LoadSettings(object[] prefs)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public object[] SaveSettings()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void Reset()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void Nudge(out bool Cancel)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void Nudged(int Nudges)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    #endregion
+    //}
+}
---
+++ AWB/Plugins/TypoScan/TypoScanListMakerPlugin.cs
@@ -0,0 +1,82 @@
+﻿/*
+TypoScan ListMakerPlugin
+Copyright (C) 2008 Sam Reed
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Xml;
+using System.IO;
+
+using WikiFunctions;
+using WikiFunctions.Plugin;
+
+namespace WikiFunctions.Plugins.ListMaker.TypoScan
+{
+    public class TypoScanListMakerPlugin : IListMakerPlugin
+    {
+        public string Name
+        {
+            get { return "TypoScan Plugin"; }
+        }
+
+        public List<Article> MakeList(params string[] searchCriteria)
+        {
+            List<Article> articles = new List<Article>();
+
+            string html = Tools.GetHTML("http://typoscan.reedyboy.net/");
+
+            using (XmlTextReader reader = new XmlTextReader(new StringReader(html)))
+            {
+                while (reader.Read())
+                {
+                    if (reader.Name.Equals("article"))
+                    {
+                        articles.Add(new WikiFunctions.Article(reader.ReadString()));
+                    }
+                }
+            }
+
+            return articles;
+        }
+
+        public string DisplayText
+        {
+            get { return "TypoScan"; }
+        }
+
+        public string UserInputTextBoxText
+        {
+            get { return ""; }
+        }
+
+        public bool UserInputTextBoxEnabled
+        {
+            get { return false; }
+        }
+
+        public void Selected()
+        { }
+
+        public bool RunOnSeparateThread
+        {
+            get { return true; }
+        }
+    }
+}
