89fa5d1a40ff633326d39916cd03d427352c9d23
Sam Reed <sam@reedyboy.net>
2008-08-02 23:42:35 +0000

    Add start of About Box to TypoScan
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@3181 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ Plugins/TypoScan/About.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WikiFunctions.Plugins.ListMaker.TypoScan
+{
+    public partial class About : Form
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void linkReedy_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkReedy.LinkVisited = true;
+            Tools.OpenENArticleInBrowser("Reedy", true);
+        }
+
+        private void linkMboverload_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkMboverload.LinkVisited = true;
+            Tools.OpenENArticleInBrowser("Mboverload", true);
+        }
+
+        private void linkTypoScanPage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkTypoScanPage.LinkVisited = true;
+            Tools.OpenENArticleInBrowser("Wikipedia:TypoScan", false);
+        }
+
+        private void linkStats_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkStats.LinkVisited = true;
+            Tools.OpenURLInBrowser("http://typoscan.reedyboy.net");
+        }
+    }
+}
