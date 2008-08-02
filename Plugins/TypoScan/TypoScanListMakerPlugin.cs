4606eb77046a50187b68d7f8005d83a196c7a8a1
Sam Reed <sam@reedyboy.net>
2008-08-02 12:48:33 +0000

    Switch round dictionary
    When log added, check if page list contains the article, if it does, add its ID to FinishedPages so it can be later sent to webpage
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@3158 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/TypoScan/TypoScanListMakerPlugin.cs
+++ Plugins/TypoScan/TypoScanListMakerPlugin.cs
@@ -52,7 +52,7 @@ namespace WikiFunctions.Plugins.ListMaker.TypoScan
                         int id = int.Parse(reader.Value);
                         string title = reader.ReadString();
                         articles.Add(new WikiFunctions.Article(title));
-                        TypoScanAWBPlugin.PageList.Add(id, title);
+                        TypoScanAWBPlugin.PageList.Add(title, id);
                     }
                 }
             }
