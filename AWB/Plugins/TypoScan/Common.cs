573f70e5510fa8f09f951e76c2379a902580a331
Sam Reed <sam@reedyboy.net>
2008-08-04 23:15:44 +0000

    Tweaks to TypoScan plugin
    Text tweak in TypoStatsControl
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@3232 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ AWB/Plugins/TypoScan/Common.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace WikiFunctions.Plugins.ListMaker.TypoScan
+{
+    static class Common
+    {
+        /// <summary>
+        /// The URL, script and action=
+        /// </summary>
+        internal const string Url = "http://typoscan.reedyboy.net/index.php?action=";
+    }
+}
