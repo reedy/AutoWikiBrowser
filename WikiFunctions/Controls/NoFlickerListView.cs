99cf8587f3bef1bd060d138a84d9087a1c5916c1
Max Semenik <maxsem.wiki@gmail.com>
2008-08-05 19:44:17 +0000

    Exception-based date parsing in NoFlickerExtendedListView column sorter was imposing huge performance penalty. Rewrote it to use TryParse, added support for int SubItems
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@3240 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Controls/NoFlickerListView.cs
+++ WikiFunctions/Controls/NoFlickerListView.cs
@@ -25,9 +25,19 @@ using System.Drawing;
 using System.Data;
 using System.Text;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace WikiFunctions.Controls
 {
+    /// <summary>
+    /// Inreface for creation of custom sorters
+    /// </summary>
+    public interface IListViewItemComparerFactory
+    {
+        IComparer Create(int column, SortOrder order);
+    }
+
+    //TODO: make column sorter customizable by using IListViewItemComparerFactory
     public partial class NoFlickerExtendedListView : ListView
     {
         /// <summary>
@@ -53,8 +63,9 @@ namespace WikiFunctions.Controls
         }
 
         bool sortColumnsOnClick;
+        [DefaultValue(false)]
         /// <summary>
-        /// 
+        /// Enables or disables sorting upon click on column header
         /// </summary>
         public bool SortColumnsOnClick
         {
@@ -153,7 +164,7 @@ namespace WikiFunctions.Controls
         }
 
         /// <remarks>From http://msdn2.microsoft.com/en-us/library/ms996467.aspx </remarks>
-        sealed class ListViewItemComparer : System.Collections.IComparer
+        sealed class ListViewItemComparer : IComparer
         {
             private int col;
             private SortOrder order;
@@ -172,25 +183,32 @@ namespace WikiFunctions.Controls
             public int Compare(object x, object y)
             {
                 int returnVal;
-                // Determine whether the type being compared is a date type.
-                try
+
+                string sx = ((ListViewItem)x).SubItems[col].Text;
+                string sy = ((ListViewItem)y).SubItems[col].Text;
+
+                System.DateTime firstDate, secondDate;
+                int intX, intY;
+
+                // first try to parse as ints
+                if (int.TryParse(sx, out intX) && int.TryParse(sy, out intY))
                 {
-                    // Parse the two objects passed as a parameter as a DateTime.
-                    System.DateTime firstDate =
-                            DateTime.Parse(((ListViewItem)x).SubItems[col].Text);
-                    System.DateTime secondDate =
-                            DateTime.Parse(((ListViewItem)y).SubItems[col].Text);
+                    returnVal = intX.CompareTo(intY);
+                }
+                else
+                // Determine whether the type being compared is a DateTime type
+                if (DateTime.TryParse(sx, out firstDate) && DateTime.TryParse(sy, out secondDate))
+                {
                     // Compare the two dates.
                     returnVal = DateTime.Compare(firstDate, secondDate);
                 }
-                // If neither compared object has a valid date format, compare
-                // as a string.
-                catch
+                else
                 {
-                    // Compare the two items as a string.
-                    returnVal = String.Compare(((ListViewItem)x).SubItems[col].Text,
-                                ((ListViewItem)y).SubItems[col].Text);
+                    // If neither compared object has a valid int or date format, compare
+                    // as a string.
+                    returnVal = String.Compare(sx, sy);
                 }
+
                 // Determine whether the sort order is descending.
                 if (order == SortOrder.Descending)
                     // Invert the value returned by String.Compare.
