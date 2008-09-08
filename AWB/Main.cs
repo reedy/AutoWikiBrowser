261509b5f17e8ed8c7770febb7445434027dd021
Sam Reed <sam@reedyboy.net>
2008-09-08 20:58:19 +0000

    Trim trailing whitespace from disambiguation box
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@3296 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Main.cs
+++ AWB/Main.cs
@@ -3281,6 +3281,7 @@ font-size: 150%;'>No changes</h2><p>Press the ""Ignore"" button below to skip to
 
                     txtDabVariants.Text += a.Name + "\r\n";
                 }
+                txtDabVariants.Text = txtDabVariants.Text.Trim();
             }
             catch (Exception ex)
             {
