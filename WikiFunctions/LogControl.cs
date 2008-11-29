226c9abab816ec4b0db35387d03208e67c606eac
Sam Reed <sam@reedyboy.net>
2008-11-29 23:35:15 +0000

    Remove some usings
    Remove un-needed reference from WF
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@3513 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/LogControl.cs
+++ WikiFunctions/LogControl.cs
@@ -20,7 +20,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
-using System.Data;
 using System.Text;
 using System.IO;
 using System.Windows.Forms;
