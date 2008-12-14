4201948554ef2687789b5eca92c6bb620076ef4f
Max Semenik <maxsem.wiki@gmail.com>
2008-12-14 17:13:19 +0000

    added API test
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@3560 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ AWB/APIEdit/APITest/Form1.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using WikiFunctions.API;
+using Microsoft.Win32;
+
+namespace APITest
+{
+    public partial class Form1 : Form
+    {
+        RegistryKey Config = Registry.CurrentUser.CreateSubKey("Wikipedia\\AutoWikiBrowser\\ApiTest");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ApiEdit Editor;
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            groupBox2.Enabled = false;
+            try
+            {
+                Editor = new ApiEdit(txtURL.Text);
+                Editor.Login(txtUsername.Text, txtPassword.Text);
+
+                groupBox2.Enabled = true;
+                txtEdit.Text = "";
+                btnSave.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().Name);
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            Editor.Maxlag = 5;
+            try
+            {
+                btnSave.Enabled = false;
+                txtEdit.Text = Editor.Open(txtTitle.Text).Replace("\n", "\r\n");
+                btnSave.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().Name);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Editor.Save(txtEdit.Text.Replace("\r\n", "\n"), txtSummary.Text, chkMinor.Checked, chkWatch.Checked);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().Name);
+            }
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            Editor.Logout();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtURL.Text = (string)Config.GetValue("Wiki", "http://test.wikipedia.org/w/");
+            txtUsername.Text = (string)Config.GetValue("User", "");
+            txtPassword.Text = (string)Config.GetValue("Password", "");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Editor.Delete(txtTitle.Text, txtSummary.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().Name);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Config.SetValue("Wiki", txtURL.Text);
+            Config.SetValue("User", txtUsername.Text);
+            Config.SetValue("Password", txtPassword.Text);
+        }
+    }
+}
