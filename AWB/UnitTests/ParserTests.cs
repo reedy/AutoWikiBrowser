c70153693d67db745186e676abd32b9a4bd22190
Sam Reed <sam@reedyboy.net>
2008-12-26 18:35:12 +0000

    Update 1 unit test, add another as per             http://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Bugs#Wrong_link_simplification_capitalisation
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@3600 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/UnitTests/ParserTests.cs
+++ AWB/UnitTests/ParserTests.cs
@@ -125,7 +125,10 @@ namespace UnitTests
 
             // case insensitivity of the first char
             Assert.AreEqual("[[dog]]s", Parsers.SimplifyLinks("[[Dog|dogs]]"));
-            Assert.AreEqual("[[Dog]]s", Parsers.SimplifyLinks("[[dog|Dogs]]"));
+
+            // http://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Bugs#Wrong_link_simplification_capitalisation
+            Assert.AreEqual("[[dog]]s", Parsers.SimplifyLinks("[[dog|Dogs]]"));
+            Assert.AreEqual("[[dog]]", Parsers.SimplifyLinks("[[Dog|dog]]"));
 
             // ...and sensitivity of others
             Assert.AreEqual("[[dog|dOgs]]", Parsers.SimplifyLinks("[[dog|dOgs]]"));
