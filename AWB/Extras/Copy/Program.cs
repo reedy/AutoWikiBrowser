61837887286ddaeba88e2494db6293b275b736e9
Sam Reed <sam@reedyboy.net>
2009-01-21 15:18:51 +0000

    Update Copy.exe
    Return 0 if ok, 1 if fault
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@3810 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Extras/Copy/Program.cs
+++ AWB/Extras/Copy/Program.cs
@@ -4,11 +4,20 @@ namespace Copy
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            if (args.Length == 2 && File.Exists(args[0]))
+            try
             {
-                File.Copy(args[0], args[1], true);
+                if (args.Length == 2 && File.Exists(args[0]))
+                {
+                    File.Copy(args[0], args[1], true);
+                    return 0;
+                }
+                return 1;
+            }
+            catch
+            {
+                return 1;
             }
         }
     }
