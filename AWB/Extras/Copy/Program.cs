10d1c4843dbb76145c2731ef986a49fb29c99f23
Sam Reed <sam@reedyboy.net>
2009-01-21 15:11:03 +0000

    Add a very basic copy program to Extras
    Add version of Copy to AWB Solution dir
    
    Update Project solutions to copy dll to AWB dir using "$(SolutionDir)Copy.exe "$(TargetDir)$(TargetFileName)" "$(SolutionDir)AWB\$(OutDir)$(TargetFileName)""
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@3809 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ AWB/Extras/Copy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace Copy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2 && File.Exists(args[0]))
+            {
+                File.Copy(args[0], args[1], true);
+            }
+        }
+    }
+}
