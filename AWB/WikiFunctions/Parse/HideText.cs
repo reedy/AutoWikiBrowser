e1d0d1076512a8a5c08c7bc6392a9c886cc5bc6b
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2009-01-27 18:05:00 +0000

    fix typo in code comment
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@3850 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Parse/HideText.cs
+++ AWB/WikiFunctions/Parse/HideText.cs
@@ -223,7 +223,7 @@ namespace WikiFunctions.Parse
             //TODO: replace with gallery-only regex, all normal images should be hidden by now as simple wikilinks
             ReplaceMore(WikiRegexes.Images.Matches(ArticleText), ref ArticleText);
 
-            // hide untemplated quotes between some form of quotation masks (most particularly for typo fixing)
+            // hide untemplated quotes between some form of quotation marks (most particularly for typo fixing)
             ReplaceMore(WikiRegexes.UntemplatedQuotes.Matches(ArticleText), ref ArticleText);
 
             return ArticleText;
