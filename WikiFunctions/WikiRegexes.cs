ec4c56d59cb5fba7d1a31e753e1be2bf0c329b63
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2009-01-29 23:56:07 +0000

    non-breaking spaces for SI units genfix: catch micro (u or µ) metres or grams too
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@3872 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/WikiRegexes.cs
+++ WikiFunctions/WikiRegexes.cs
@@ -345,7 +345,7 @@ namespace WikiFunctions
         /// Matches abbreviated SI units without a non-breaking space
         /// </summary>
         /// http://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Feature_requests#Non_breaking_spaces
-        public static readonly Regex SiUnitsWithoutNonBreakingSpaces = new Regex(@"\b(\d?\.?\d+)\s*((?:c|m|k|n)(?:m|g)|m?mol|cd)\b", RegexOptions.Compiled);
+        public static readonly Regex SiUnitsWithoutNonBreakingSpaces = new Regex(@"\b(\d?\.?\d+)\s*((?:[cmknuµ])(?:m|g)|m?mol|cd)\b", RegexOptions.Compiled);
 
         #region en only
         /// <summary>
