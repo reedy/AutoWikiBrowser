804049540eeaf064a2b5a8bf0820c5a36c1014a8
Max Semenik <maxsem.wiki@gmail.com>
2009-02-02 20:51:20 +0000

    gah
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@3932 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/API/AsyncApiEdit.cs
+++ AWB/WikiFunctions/API/AsyncApiEdit.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Diagnostics;
 
 namespace WikiFunctions.API
 {
@@ -7,32 +9,77 @@ namespace WikiFunctions.API
     /// </summary>
     public class AsyncApiEdit : IApiEdit
     {
+        ApiEdit m_Editor;
+        Thread m_Thread;
+
+        public AsyncApiEdit(string url)
+        {
+            m_Editor = new ApiEdit(url);
+        }
+
         /// <summary>
         /// Provides access to the underlying ApiEdit
         /// </summary>
         public ApiEdit Editor { get; private set; }
-
-        bool m_AsyncMode = true;
-
-        /// <summary>
-        /// Whether all operations should return immediately or wait for completition
-        /// </summary>
-        public bool AsyncMode
-        {
-            get { return m_AsyncMode; }
-            set 
-            {
-                m_AsyncMode = false;
-                Wait();
-            }
-        }
 
         /// <summary>
         /// Waits for asyncronous operation to complete
         /// </summary>
         public void Wait()
         {
-            throw new NotImplementedException();
+            if (m_Thread != null) m_Thread.Join();
+        }
+
+        private class InvokeArgs
+        {
+            public string Function;
+            public object[] Arguments;
+
+            public InvokeArgs(string func, params object[] args)
+            {
+                Function = func;
+                Arguments = args;
+            }
+        };
+
+        private void InvokerThread(object genericArgs)
+        {
+            try
+            {
+                InvokeArgs args = (InvokeArgs)genericArgs;
+
+                Thread.CurrentThread.Name = string.Format("InvokerThread ({0})", args.Function);
+
+                Type t = m_Editor.GetType();
+
+                object res = t.InvokeMember(
+                    args.Function,                                  // name
+                    System.Reflection.BindingFlags.InvokeMethod,    // invokeAttr
+                    null,                                           // binder
+                    m_Editor,                                       // target
+                    args.Arguments                                  // args
+                    );
+
+                Debug.WriteLine(Thread.CurrentThread.Name + " successfully completed");
+            }
+            catch (Exception ex)
+            {
+                // TODO: 
+            }
+        }
+
+        private void InvokeFunction(InvokeArgs args)
+        {
+            if (m_Thread != null && m_Thread.IsAlive)
+                throw new ApiInvokeException("An asynchronous call is already being performed");
+
+            m_Thread = new Thread(InvokerThread);
+            m_Thread.Start(args);
+        }
+
+        private void InvokeFunction(string name, params object[] args)
+        {
+            InvokeFunction(new InvokeArgs(name, args));
         }
 
         #region IApiEdit Members
@@ -71,27 +118,29 @@ namespace WikiFunctions.API
 
         public string HttpGet(string url)
         {
-            throw new NotImplementedException();
+            InvokeFunction("HttpGet", url);
+            return null;
         }
 
         public void Login(string username, string password)
         {
-            throw new NotImplementedException();
+            InvokeFunction("Login", username, password);
         }
 
         public void Logout()
         {
-            throw new NotImplementedException();
+            InvokeFunction("Logout");
         }
 
         public string Open(string title)
         {
-            throw new NotImplementedException();
+            InvokeFunction("Open", title);
+            return null;
         }
 
         public void Save(string pageText, string summary, bool minor, bool watch)
         {
-            throw new NotImplementedException();
+            InvokeFunction("Save", pageText, summary, minor, watch);
         }
 
         public void Delete(string title, string reason)
@@ -136,9 +185,32 @@ namespace WikiFunctions.API
 
         public void Abort()
         {
-            throw new NotImplementedException();
+            m_Editor.Abort();
+        }
+
+        public bool Asynchronous
+        {
+            get { return true; }
         }
 
         #endregion
+    }
+
+    public class ApiInvokeException : Exception
+    {
+        public ApiInvokeException(string message)
+            : base(message)
+        {
+        }
+
+        public ApiInvokeException(Exception innerException)
+            : this("There was a problem with an asynchronous API call", innerException)
+        {
+        }
+
+        public ApiInvokeException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
     }
 }
