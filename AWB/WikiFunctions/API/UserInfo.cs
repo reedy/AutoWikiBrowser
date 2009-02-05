6177f9a588b35009b4d9df32f28f1e3fe71c0605
Max Semenik <maxsem.wiki@gmail.com>
2009-02-05 16:13:29 +0000

    UserInfo class for API
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@3944 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ AWB/WikiFunctions/API/UserInfo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WikiFunctions.API
+{
+    /// <summary>
+    /// Information about a user
+    /// </summary>
+    public class UserInfo
+    {
+        /// <summary>
+        /// Username
+        /// </summary>
+        public string Name
+        { get; private set; }
+
+        /// <summary>
+        /// Database ID
+        /// </summary>
+        public int Id
+        { get; private set; }
+
+        /// <summary>
+        /// Whether the user is registered
+        /// </summary>
+        public bool IsRegistered
+        { get { return Id != 0; } }
+
+        /// <summary>
+        /// Whether the current user is an administrator
+        /// </summary>
+        public bool IsSysop
+        { get { return Groups.Contains("sysop"); } }
+
+        /// <summary>
+        /// Whether the current user is a flagged bot
+        /// </summary>
+        public bool IsBot
+        { get { return Groups.Contains("bot"); } }
+
+        /// <summary>
+        /// Whether the user has an unread user talk message
+        /// </summary>
+        public bool HasMessage
+        { get; internal set; }
+
+        /// <summary>
+        /// Creates a 
+        /// </summary>
+        /// <param name="xml"></param>
+        internal UserInfo(string xml)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates a UserInfo object for an unregistered user
+        /// </summary>
+        internal UserInfo()
+        {
+        }
+
+        private List<string> Groups = new List<string>();
+        private List<string> Rights = new List<string>();
+    }
+}
