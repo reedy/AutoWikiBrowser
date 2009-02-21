4305c69b16a60273fefe9dd6c00c3f6503ac6eb8
Max Semenik <maxsem.wiki@gmail.com>
2009-02-21 17:42:05 +0000

    refactored TalkPageHeaders
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@4001 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/TalkPageHeaders.cs
+++ WikiFunctions/TalkPageHeaders.cs
@@ -31,15 +31,41 @@ namespace WikiFunctions.TalkPages
         MoveToBottom
     }
 
+    internal class Processor
+    {
+        public string DefaultSortKey;
+        public bool FoundDefaultSort;
+        public bool FoundSkipTOC;
+        public bool FoundTalkHeader;
+
+        // Match evaluators:
+        public string DefaultSortMatchEvaluator(Match match)
+        {
+            FoundDefaultSort = true;
+            if (match.Groups["key"].Captures.Count > 0)
+                DefaultSortKey = match.Groups["key"].Captures[0].Value.Trim();
+            return "";
+        }
+
+        public string SkipTOCMatchEvaluator(Match match)
+        {
+            FoundSkipTOC = true;
+            return "";
+        }
+
+        public string TalkHeaderMatchEvaluator(Match match)
+        {
+            FoundTalkHeader = true;
+            return "";
+        }
+    }
+
     public static class TalkPageHeaders
     {
-        private static string DefaultSortKey;
-        private static bool FoundDefaultSort,
-            FoundSkipTOC, FoundTalkheader;
         private static readonly Regex SkipTOCTemplateRegex = new Regex(
            @"\{\{\s*(template *:)?\s*(skiptotoctalk|Skiptotoc|Skiptotoc-talk)\s*\}\}\s*",
            RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.IgnoreCase);
-        private static readonly Regex TalkheaderTemplateRegex = new Regex(
+        private static readonly Regex TalkHeaderTemplateRegex = new Regex(
            @"\{\{\s*(template *:)?\s*(talkheader|Talkheaderlong|Comment Guidelines|Categorytalkheader|Newtalk|Templatetalkheader|Talkheader2|Talkheader3|Talkpagelong|Talk box|Talkpageheader|TalkHeader|User Talkheader)\s*\}\}\s*", 
            RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.IgnoreCase);
 
@@ -62,32 +88,33 @@ namespace WikiFunctions.TalkPages
         public static bool ProcessTalkPage(ref string ArticleText, IMyTraceListener Trace, 
         DEFAULTSORT MoveDEFAULTSORT, string PluginName)
         {
-            FoundTalkheader = false; FoundSkipTOC = false; FoundDefaultSort = false;
-            ArticleText = TalkheaderTemplateRegex.Replace(ArticleText, new MatchEvaluator(TalkheaderMatchEvaluator), 1);
-            ArticleText = SkipTOCTemplateRegex.Replace(ArticleText, new MatchEvaluator(SkipTOCMatchEvaluator), 1);
-            if (FoundTalkheader)
+            Processor pr = new Processor();
+            ArticleText = TalkHeaderTemplateRegex.Replace(ArticleText, 
+                new MatchEvaluator(pr.TalkHeaderMatchEvaluator),
+                1);
+            ArticleText = SkipTOCTemplateRegex.Replace(ArticleText, new MatchEvaluator(pr.SkipTOCMatchEvaluator), 1);
+            if (pr.FoundTalkHeader)
                 WriteHeaderTemplate("talkheader", ref ArticleText, Trace, PluginName);
-            if (FoundSkipTOC)
+            if (pr.FoundSkipTOC)
                 WriteHeaderTemplate("skiptotoctalk", ref ArticleText, Trace, PluginName);
             if (MoveDEFAULTSORT != DEFAULTSORT.NoChange)
             {
                 ArticleText = WikiRegexes.Defaultsort.Replace(ArticleText, 
-                    new MatchEvaluator(DefaultSortMatchEvaluator), 1);
-                if (FoundDefaultSort)
+                    new MatchEvaluator(pr.DefaultSortMatchEvaluator), 1);
+                if (pr.FoundDefaultSort)
                 {
-                    if (string.IsNullOrEmpty(DefaultSortKey))
+                    if (string.IsNullOrEmpty(pr.DefaultSortKey))
                     {
                         if (Trace != null)
                             Trace.WriteArticleActionLine("DEFAULTSORT has no key; removed", PluginName);
                     }
                     else
                     {
-                        WriteDefaultSortMagicWord(MoveDEFAULTSORT, Trace, PluginName, ref ArticleText);
+                        ArticleText = SetDefaultSort(pr.DefaultSortKey, MoveDEFAULTSORT, Trace, PluginName, ArticleText);
                     }
                 }
-                DefaultSortKey = "";
             }
-            return (FoundTalkheader || FoundSkipTOC) || FoundDefaultSort;
+            return pr.FoundTalkHeader || pr.FoundSkipTOC || pr.FoundDefaultSort;
         }
 
         public static string FormatDefaultSort(string ArticleText)
@@ -95,43 +122,24 @@ namespace WikiFunctions.TalkPages
             return WikiRegexes.Defaultsort.Replace(ArticleText, "{{DEFAULTSORT:${key}}}");
         }
         
-        // Match evaluators:
-        private static string DefaultSortMatchEvaluator(Match match)
-        {
-            FoundDefaultSort = true;
-            if (match.Groups["key"].Captures.Count > 0)
-                DefaultSortKey = match.Groups["key"].Captures[0].Value.Trim();
-            return "";
-        }
-
-        private static string SkipTOCMatchEvaluator(Match match)
-        {
-            FoundSkipTOC = true;
-            return "";
-        }
-
-        private static string TalkheaderMatchEvaluator(Match match)
-        {
-            FoundTalkheader = true;
-            return "";
-        }
-
         // Helper routines:
-        private static void WriteDefaultSortMagicWord(DEFAULTSORT Location, IMyTraceListener Trace, string PluginName, ref string ArticleText)
+        private static string SetDefaultSort(string key, DEFAULTSORT Location, IMyTraceListener Trace, string PluginName, string ArticleText)
         {
             string strMovedTo;
             if (Location == DEFAULTSORT.MoveToTop)
             {
-                ArticleText = "{{DEFAULTSORT:" + DefaultSortKey + "}}\r\n" + ArticleText;
+                ArticleText = "{{DEFAULTSORT:" + key + "}}\r\n" + ArticleText;
                 strMovedTo = " given top billing";
             }
             else
             {
-                ArticleText = ArticleText + "\r\n{{DEFAULTSORT:" + DefaultSortKey + "}}";
+                ArticleText = ArticleText + "\r\n{{DEFAULTSORT:" + key + "}}";
                 strMovedTo = " sent to the bottom";
             }
             if (Trace != null)
                 Trace.WriteArticleActionLine("DEFAULTSORT" + strMovedTo, PluginName, false);
+
+            return ArticleText;
         }
 
         private static void WriteHeaderTemplate(string Name, ref string ArticleText, IMyTraceListener Trace, string PluginName)
