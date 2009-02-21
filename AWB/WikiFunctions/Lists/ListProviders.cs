7ff329522dddaee6c42389d883282189a52bdfe8
Sam Reed <sam@reedyboy.net>
2009-02-21 23:33:22 +0000

    Implement LinksOnPageExcludingRedLinksListProvider
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@4010 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Lists/ListProviders.cs
+++ AWB/WikiFunctions/Lists/ListProviders.cs
@@ -24,6 +24,7 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Xml;
 using WikiFunctions.Controls.Lists;
 
 namespace WikiFunctions.Lists
@@ -549,6 +550,83 @@ namespace WikiFunctions.Lists
 
         public override void Selected() { }
         #endregion
+    }
+
+    /// <summary>
+    /// Gets a list of all non-redlink links on the Named Pages
+    /// </summary>
+    public class LinksOnPageExcludingRedLinksListProvider : IListProvider
+    {
+        public List<Article> MakeList(params string[] searchCriteria)
+        {
+            searchCriteria = Tools.FirstToUpperAndRemoveHashOnArray(searchCriteria);
+
+            List<Article> list = new List<Article>();
+
+            foreach (string page in searchCriteria)
+            {
+                string postfix = "";
+
+                while (list.Count < 5000) //Cant imagine a page having more than 5000 links...
+                {
+                    string text = Tools.GetHTML(Variables.URLApi + "?action=query&generator=links&titles="
+                                                + HttpUtility.UrlEncode(page) + "&gpllimit=max&format=xml" + postfix);
+
+                    postfix = "";
+
+                    XmlTextReader xml = new XmlTextReader(new StringReader(text));
+                    xml.MoveToContent();
+
+                    while (xml.Read())
+                    {
+                        if (xml.Name == "query-continue")
+                        {
+                            XmlReader r = xml.ReadSubtree();
+
+                            r.Read();
+
+                            while (r.Read())
+                            {
+                                if (!r.IsStartElement()) continue;
+                                if (!r.MoveToFirstAttribute())
+                                    throw new FormatException("Malformed element '" + r.Name + "' in <query-continue>");
+
+                                postfix += "&" + r.Name + "=" + HttpUtility.UrlEncode(r.Value);
+                            }
+                        }
+                        else if (xml.IsStartElement())
+                        {
+                            if (xml.MoveToAttribute("missing"))
+                                continue;
+
+                            string name = xml.GetAttribute("title");
+
+                            if (!string.IsNullOrEmpty(name))
+                                list.Add(new Article(name));
+                        }
+                    }
+
+                    if (string.IsNullOrEmpty(postfix)) break;
+                }
+            }
+            return list;
+        }
+
+        public string DisplayText
+        { get { return "Links on page (no redlinks)"; } }
+
+        public string UserInputTextBoxText
+        { get { return "Links on"; } }
+
+        public bool UserInputTextBoxEnabled
+        { get { return true; } }
+
+        public void Selected()
+        {
+        }
+
+        public bool RunOnSeparateThread
+        { get { return true; } }
     }
 
     /// <summary>
