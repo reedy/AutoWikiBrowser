cce186dd8547057f040ccdb08db5805a12cd85d3
Max Semenik <maxsem.wiki@gmail.com>
2009-02-21 12:37:31 +0000

    refactored MockSkipOptions for simplicity
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@3990 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/UnitTests/Mocks.cs
+++ AWB/UnitTests/Mocks.cs
@@ -4,46 +4,25 @@ namespace UnitTests
 {
     class MockSkipOptions : ISkipOptions
     {
-        public bool m_SkipNoUnicode;
         public bool SkipNoUnicode
-        {
-            get { return m_SkipNoUnicode; }
-        }
+        { get; set; }
 
-        public bool m_SkipNoTag;
         public bool SkipNoTag
-        {
-            get { return m_SkipNoTag; }
-        }
+        { get; set; }
 
-        public bool m_SkipNoHeaderError;
         public bool SkipNoHeaderError
-        {
-            get { return m_SkipNoHeaderError; }
-        }
+        { get; set; }
 
-        public bool m_SkipNoBoldTitle;
         public bool SkipNoBoldTitle
-        {
-            get { return m_SkipNoBoldTitle; }
-        }
+        { get; set; }
 
-        public bool m_SkipNoBulletedLink;
         public bool SkipNoBulletedLink
-        {
-            get { return m_SkipNoBulletedLink; }
-        }
+        { get; set; }
 
-        public bool m_SkipNoBadLink;
         public bool SkipNoBadLink
-        {
-            get { return m_SkipNoBadLink; }
-        }
+        { get; set; }
 
-        public bool m_SkipNoDefaultSortAdded;
         public bool SkipNoDefaultSortAdded
-        {
-            get { return m_SkipNoDefaultSortAdded; }
-        }
+        { get; set; }
     }
 }
