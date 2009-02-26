14da270f1bd4bcb5e673d0c07f006ca39360f706
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2009-02-26 18:34:16 +0000

    resolve FR http://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Feature_requests#.7B.7Bcommons.7CCategory:XXX.7D.7D_.3E_.7B.7Bcommonscat.7CXXX.7D.7D
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@4025 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Parse/Parsers.cs
+++ WikiFunctions/Parse/Parsers.cs
@@ -94,6 +94,8 @@ namespace WikiFunctions.Parse
 
             // articleissues with one issue -> single issue tag (e.g. {{articleissues|cleanup=January 2008}} to {{cleanup|date=January 2008}} etc.)
             RegexConversion.Add(new Regex(@"\{\{[Aa]rticle ?issues\s*\|\s*([^\|{}=]{3,}?)\s*(=\s*\w{3,10}\s+20\d\d\s*\}\})", RegexOptions.Compiled), "{{$1|date$2");
+
+            RegexConversion.Add(new Regex(@"\{\{[Cc]ommons\|\s*[Cc]ategory:\s*([^{}]+?)\s*\}\}", RegexOptions.Compiled), @"{{commons cat|$1}}");
         }
 
         private static readonly Dictionary<Regex, string> RegexUnicode = new Dictionary<Regex, string>();
