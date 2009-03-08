7877a194c864e6b616f18db0ebe7c66bdf30e632
Max Semenik <maxsem.wiki@gmail.com>
2009-03-08 17:08:08 +0000

    switched ApiEdit usage to asynchro
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@4052 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/API/PageInfo.cs
+++ WikiFunctions/API/PageInfo.cs
@@ -16,6 +16,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+using System.Xml;
+using System.IO;
+using System;
 namespace WikiFunctions.API
 {
     /// <summary>
@@ -30,14 +33,83 @@ namespace WikiFunctions.API
     };
 
     /// <summary>
-    /// 
+    /// This class represents information about the page currently being edited
     /// </summary>
     public class PageInfo
     {
+        internal PageInfo()
+        {
+        }
+
+        internal PageInfo(string xml)
+        {
+            XmlReader xr = XmlReader.Create(new StringReader(xml));
+            if (!xr.ReadToFollowing("page")) throw new Exception("Cannot find <page> element");
+
+            Exists = (xr.GetAttribute("missing") != "");
+            EditToken = xr.GetAttribute("edittoken");
+            Title = xr.GetAttribute("title");
+
+            if (xr.ReadToDescendant("protection") && !xr.IsEmptyElement)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xr.ReadOuterXml());
+
+                foreach (XmlNode xn in doc.GetElementsByTagName("pr"))
+                {
+                    switch (xn.Attributes["type"].Value)
+                    {
+                        case "edit":
+                            Edit = StringToProtection(xn.Attributes["level"].Value);
+                            break;
+                        case "move":
+                            Move = StringToProtection(xn.Attributes["level"].Value);
+                            break;
+                    }
+                }
+            }
+            else
+                xr.ReadToFollowing("revisions");
+
+            xr.ReadToDescendant("rev");
+            Timestamp = xr.GetAttribute("timestamp");
+            Text = xr.ReadString();
+        }
+
         public string Title
         { get; private set; }
 
         public string Text
         { get; private set; }
+
+        public bool Exists
+        { get; internal set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Timestamp
+        { get; private set; }
+
+        /// <summary>
+        /// Edit token (http://www.mediawiki.org/wiki/Manual:Edit_token)
+        /// </summary>
+        public string EditToken { get; internal set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Protection Edit { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Protection Move { get; private set; }
+
+        private static Protection StringToProtection(string val)
+        {
+            return (Protection)Enum.Parse(typeof(Protection), Tools.TurnFirstToUpperNoProjectCheck(val));
+        }
+
     }
 }
