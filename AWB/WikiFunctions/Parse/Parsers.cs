1c9d71c4cdc2deeb13617777aa30886e9a10d37c
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2009-04-02 18:27:49 +0000

    Tweak PipedExternalLink regex and add another unit test
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@4129 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Parse/Parsers.cs
+++ AWB/WikiFunctions/Parse/Parsers.cs
@@ -848,7 +848,7 @@ namespace WikiFunctions.Parse
         private static readonly Regex SyntaxRemoveBr = new Regex(@"(?<!^[!\|].*)(<br[\s/]*> *){2,}", RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Compiled);
 
         private static readonly Regex MultipleHttpInLink = new Regex(@"([\s\[>=])((?:ht|f)tp:?/+)(\2)+", RegexOptions.IgnoreCase | RegexOptions.Compiled);
-        private static readonly Regex PipedExternalLink = new Regex(@"(\[\w+://[^][<>\""\s]*?)\|''", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private static readonly Regex PipedExternalLink = new Regex(@"(\[\w+://[^\]\[<>\""\s]*?)\|''", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
         private static readonly Regex MissingColonInHttpLink = new Regex(@"([\s\[>=](?:ht|f))tp//?:?(\w+)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
         private static readonly Regex SingleTripleSlashInHttpLink = new Regex(@"([\s\[>=](?:ht|f))tp:(?:/|///)(\w+)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
