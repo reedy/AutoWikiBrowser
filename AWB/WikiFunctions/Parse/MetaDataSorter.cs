d006e1014e445fb0aa911e907abeb10dc0e548a7
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2009-04-20 20:09:18 +0000

    further to previous, allow defaultsort or category
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@4240 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Parse/MetaDataSorter.cs
+++ AWB/WikiFunctions/Parse/MetaDataSorter.cs
@@ -729,7 +729,7 @@ en, sq, ru
 
         private static readonly Regex ReferencesSection = new Regex(@"(^== *[Rr]eferences *==.*?)(?=^==[^=][^\r\n]*?[^=]==(\r\n?|\n)$)", RegexOptions.Multiline | RegexOptions.Singleline);
 
-        private static readonly Regex ReferencesToEnd = new Regex(@"^== *[Rr]eferences *==\s*" + WikiRegexes.ReferencesTemplates + @"\s*(?=\[\[Category\:)", RegexOptions.Multiline);
+        private static readonly Regex ReferencesToEnd = new Regex(@"^== *[Rr]eferences *==\s*" + WikiRegexes.ReferencesTemplates + @"\s*(?={{DEFAULTSORT\:|\[\[Category\:)", RegexOptions.Multiline);
 
         // http://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Feature_requests#Place_.22External_links.22_section_after_.22References.22
         /// <summary>
