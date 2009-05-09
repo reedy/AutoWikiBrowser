b3b342d6db13c03a980fbff5dc7e3f418f134882
Sam Reed <sam@reedyboy.net>
2009-05-09 12:17:03 +0000

    Tagging for 4.5.3 release
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@4297 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ tags/REL_4_5_3/AWB/Stats.cs
@@ -0,0 +1,358 @@
+/*
+Autowikibrowser
+Copyright (C) 2007 Martin Richards
+(C) 2008 Stephen Kennedy (Kingboyk) http://www.sdk-software.com/
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#undef INSTASTATS // turn on here and in Main.cs to make AWB log (empty) stats at startup (The scope of a symbol created by using #define is the file in which it was defined)
+
+using System;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using WikiFunctions;
+using System.Net;
+using System.IO;
+using WikiFunctions.Plugin;
+using System.Xml;
+
+namespace AutoWikiBrowser
+{
+    partial class MainForm
+    {
+        // Unfortunately, NotifyIcon is sealed, otherwise I would inherit from that and do tooltiptext/stats management there
+        // Even more unfortunately, it seems it's tooltip is limited to 64 chars. Stinking great, Microsoft!
+        // T-O-D-O: Maybe an alternative approach using mouse events? - doesn't seem to be a reliable way of doing even that! see e.g. http://64.233.183.104/search?q=cache:34QVls9xRoUJ:www.experts-exchange.com/Programming/Languages/.NET/Visual_Basic.NET/Q_21161863.html+notifyicon+mouseover&hl=en&ct=clnk&cd=1&gl=uk&lr=lang_en
+        private int intEdits;
+        public int NumberOfEdits
+        {
+            get { return intEdits; }
+            private set
+            {
+                intEdits = value;
+                lblEditCount.Text = "Edits: " + value;
+                //UpdateNotifyIconTooltip();
+                if (value == 100 || (value > 0 && value % 1000 == 0)) // we'll first report to remote db when we have 100 saves or app is exiting, whichever comes first; we'll also update db at 1000 and each 1000 thereafter
+                    UsageStats.Do(false);
+            }
+        }
+
+        private int intIgnoredEdits;
+        public int NumberOfIgnoredEdits
+        {
+            get { return intIgnoredEdits; }
+            private set
+            {
+                intIgnoredEdits = value;
+                lblIgnoredArticles.Text = "Ignored: " + value;
+                //UpdateNotifyIconTooltip();
+            }
+        }
+
+        private int intEditsPerMin;
+        public int NumberOfEditsPerMinute
+        {
+            get { return intEditsPerMin; }
+            private set
+            {
+                intEditsPerMin = value;
+                lblEditsPerMin.Text = "Edits/min: " + value;
+                //UpdateNotifyIconTooltip();
+            }
+        }
+    }
+
+    /// <summary>
+    /// A class to collect and submit some non-invasive usage stats, to help AWB developers track usage and plan development
+    /// Stats can be viewed at http://awb.kingboyk.com/
+    /// SQL and FTP access is available to Sourceforge-registered AWB developers only: please ask Reedy or Kingboyk for
+    /// the login details.
+    /// </summary>
+    internal static class UsageStats
+    {
+        // TODO: Add other stuff we'd like to track. e.g. I'd quite like to know if anybody is using log to file.
+
+        private const string StatsURL = "http://awb.kingboyk.com/";
+        private static int RecordId;
+        private static int SecretNumber;
+        private static int LastEditCount;
+        private static string UserName = "";
+        private static bool SentUserName;
+        private static readonly List<IAWBPlugin> NewAWBPlugins = new List<IAWBPlugin>();
+        private static readonly List<IListMakerPlugin> NewListMakerPlugins = new List<IListMakerPlugin>();
+
+        #region Public
+        internal static void Initialise()
+        {
+            // static constructor only gets called when an member of the class is first accessed, not at startup (surprised me!)
+            // so, we'll have to call this code at startup manually instead
+            Variables.User.UserNameChanged += UserNameChanged;
+        }
+
+        /// <summary>
+        /// Call this when it's time to consider submitting some data
+        /// </summary>
+        internal static void Do(bool appexit)
+        {
+            try
+            {
+                if (EstablishedContact)
+                {
+                    if (Program.AWB.NumberOfEdits > LastEditCount || NewAWBPlugins.Count > 0
+                        || HaveUserNameToSend) SubsequentContact();
+                }
+                else
+                    FirstContact();
+
+                // success:
+                LastEditCount = Program.AWB.NumberOfEdits;
+            }
+            catch (Exception ex)
+            {
+                if (appexit) ErrorHandler.Handle(ex); // else try again later
+            }
+        }
+
+        /// <summary>
+        /// Call when a plugin was added *after* application startup
+        /// </summary>
+        internal static void AddedPlugin(IAWBPlugin plugin)
+        {
+            // if we've already written to the remote database, we'll need to add details of this plugin when we next contact it, otherwise do nothing
+            if (EstablishedContact) NewAWBPlugins.Add(plugin);
+        }
+
+        /// <summary>
+        /// Call when a plugin was added *after* application startup
+        /// </summary>
+        internal static void AddedPlugin(IListMakerPlugin plugin)
+        {
+            if (EstablishedContact) NewListMakerPlugins.Add(plugin);
+        }
+        #endregion
+
+        #region Server Contact
+        /// <summary>
+        /// Send usage stats to server
+        /// </summary>
+        private static void FirstContact()
+        {
+#if !DEBUG && !INSTASTATS
+            if (Program.AWB.NumberOfEdits == 0) return;
+#endif
+            NameValueCollection postvars = new NameValueCollection();
+
+            // Greetings and AWB version:
+            postvars.Add("Action", "Hello");
+            postvars.Add("Version", Program.VersionString);
+
+            // Site/project name:
+            // TODO: Here or in PHP: tl.wikipedia.org  	CUS: Translate to site name/lang code any Wikimedia site set up as custom
+            if (Variables.IsCustomProject || Variables.IsWikia)
+                postvars.Add("Wiki", new Uri(Variables.URL).Host);
+            else
+                postvars.Add("Wiki", Variables.Project.ToString());
+                    // This returns a short string such as "wikipedia"; may want to convert to int and then to string so we store less in the db
+
+            // Language code:
+            if (Variables.IsWikia)
+            {
+                postvars.Add("Language", "WIK");
+            }
+            else if (Variables.IsCustomProject || Variables.IsWikimediaMonolingualProject)
+            {
+                postvars.Add("Language", "CUS");
+            }
+            else
+            {
+                postvars.Add("Language", Variables.LangCodeEnumString());
+            }
+
+            // UI culture:
+            postvars.Add("Culture", System.Threading.Thread.CurrentThread.CurrentCulture.ToString());
+
+            // Username:
+            ProcessUsername(postvars);
+
+            // Other details:
+            postvars.Add("Saves", Program.AWB.NumberOfEdits.ToString());
+            postvars.Add("OS", Environment.OSVersion.VersionString);
+#if DEBUG
+                postvars.Add("Debug", "Y");
+#else
+            postvars.Add("Debug", "N");
+#endif
+            EnumeratePlugins(postvars, Plugins.Plugin.Items.Values,
+                             WikiFunctions.Controls.Lists.ListMaker.GetListMakerPlugins());
+
+            ReadXML(PostData(postvars));
+        }
+
+        /// <summary>
+        /// Send updated usage stats to server
+        /// </summary>
+        private static void SubsequentContact()
+        {
+            NameValueCollection postvars = new NameValueCollection();
+
+            postvars.Add("Action", "Update");
+            postvars.Add("RecordID", RecordId.ToString());
+            postvars.Add("Verify", SecretNumber.ToString());
+
+            EnumeratePlugins(postvars, NewAWBPlugins, NewListMakerPlugins);
+            ProcessUsername(postvars);
+
+            if (Program.AWB.NumberOfEdits > LastEditCount)
+                postvars.Add("Saves", Program.AWB.NumberOfEdits.ToString());
+
+            PostData(postvars);
+            NewAWBPlugins.Clear();
+            NewListMakerPlugins.Clear();
+        }
+
+        /// <summary>
+        /// Returns true if we've sent initial stats to server
+        /// </summary>
+        private static bool EstablishedContact
+        { get { return (RecordId > 0); } }
+
+        /// <summary>
+        /// Post a collection of names/values to server
+        /// </summary>
+        /// <param name="postvars"></param>
+        /// <returns></returns>
+        private static string PostData(NameValueCollection postvars)
+        {
+            try
+            {
+                Program.AWB.StartProgressBar();
+                StatusLabelText = "Contacting stats server...";
+                Program.AWB.Form.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+
+                return Tools.PostData(postvars, StatsURL);
+            }
+            catch (WebException ex)
+            {
+                Tools.WriteDebug("UsageStats", ex.Message);
+            }
+            catch (IOException ex)
+            {
+                Tools.WriteDebug("UsageStats", ex.Message);
+            }
+            finally
+            {
+                Program.AWB.StopProgressBar();
+                StatusLabelText = "";
+                Program.AWB.Form.Cursor = System.Windows.Forms.Cursors.Default;
+            }
+            return null;
+        }
+        #endregion
+
+        #region Helper routines
+        private static string StatusLabelText { set { Program.AWB.StatusLabelText = value; } }
+
+        private static void EnumeratePlugins(NameValueCollection postvars, ICollection<IAWBPlugin> awbPlugins, ICollection<IListMakerPlugin> listLakerPlugins)
+        {
+            int i = 0;
+
+            postvars.Add("PluginCount", (awbPlugins.Count + listLakerPlugins.Count).ToString());
+
+            foreach (IAWBPlugin plugin in awbPlugins)
+            {
+                i++;
+                string p = "P" + i;
+                postvars.Add(p + "N", plugin.Name);
+                postvars.Add(p + "V", Plugins.Plugin.GetPluginVersionString(plugin));
+                postvars.Add(p + "T", "0");
+            }
+
+            foreach (IListMakerPlugin plugin in listLakerPlugins)
+            {
+                i++;
+                string p = "P" + i;
+                postvars.Add(p + "N", plugin.Name);
+                postvars.Add(p + "V", Plugins.Plugin.GetPluginVersionString(plugin));
+                postvars.Add(p + "T", "1");
+            }
+        }
+
+        private static void ReadXML(string xml)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(xml)) return; // handle network errors
+
+                // we don't *need* these IDs if we're exiting, but I think it does no harm to check we received a valid response
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xml);
+                XmlNodeList nodes = doc.GetElementsByTagName("DB");
+                if (nodes.Count == 1 && nodes[0].Attributes.Count == 2)
+                {
+                    RecordId = int.Parse(nodes[0].Attributes["Record"].Value);
+                    SecretNumber = int.Parse(nodes[0].Attributes["Verify"].Value);
+                }
+                else
+                {
+                    throw new XmlException("Error parsing XML returned from UsageStats server");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is XmlException)
+                    throw;
+                
+                throw new XmlException("Error parsing XML returned from UsageStats server", ex);
+            }
+        }
+
+        private static void ProcessUsername(NameValueCollection postvars)
+        {
+            if (!SentUserName)
+            {
+                if (Properties.Settings.Default.Privacy)
+                {
+                    postvars.Add("User", "<Withheld>");
+                    SentUserName = true;
+                }
+                else if (!string.IsNullOrEmpty(UserName))
+                {
+                    postvars.Add("User", UserName);
+                    SentUserName = true;
+                }
+            }
+        }
+
+        private static bool HaveUserNameToSend
+        {
+            get
+            {
+                return (!SentUserName &&
+                    (Properties.Settings.Default.Privacy || !string.IsNullOrEmpty(UserName)));
+            }
+        }
+
+        private static void UserNameChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Variables.User.Name))
+                UserName = Variables.User.Name;
+        }
+        #endregion
+
+        internal static void OpenUsageStatsURL()
+        { Tools.OpenURLInBrowser(StatsURL); }
+    }
+}
---
+++ tags/REL_4_5_3/Plugins/TypoScan/TypoScanAWBPlugin.cs
@@ -0,0 +1,266 @@
+﻿/*
+TypoScan AWBPlugin
+Copyright (C) 2008 Sam Reed, Max Semenik
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+using System;
+using System.Collections.Generic;
+
+using System.Collections.Specialized;
+using System.Windows.Forms;
+using WikiFunctions.Background;
+using WikiFunctions.Plugin;
+
+namespace WikiFunctions.Plugins.ListMaker.TypoScan
+{
+    class TypoScanAWBPlugin : IAWBPlugin
+    {
+        #region IAWBPlugin Members
+        private static IAutoWikiBrowser AWB;
+        internal static readonly Dictionary<string, int> PageList = new Dictionary<string, int>();
+        private static readonly List<string> SavedPages = new List<string>();
+
+        private static readonly List<string> SkippedPages = new List<string>();
+        private static readonly List<string> SkippedReasons = new List<string>();
+
+        internal static readonly List<string> SavedPagesThisSession = new List<string>();
+        internal static readonly List<string> SkippedPagesThisSession = new List<string>();
+        internal static int UploadedThisSession;
+
+        internal static DateTime CheckoutTime;
+
+        private readonly ToolStripMenuItem pluginMenuItem = new ToolStripMenuItem("TypoScan plugin");
+        private readonly ToolStripMenuItem pluginUploadMenuItem = new ToolStripMenuItem("Upload finished articles to server now");
+        private readonly ToolStripMenuItem pluginReAddArticlesMenuItem = new ToolStripMenuItem("Re-add Unprocessed TypoScan articles to ListMaker");
+        private readonly ToolStripMenuItem aboutMenuItem = new ToolStripMenuItem("About the TypoScan plugin");
+
+        public void Initialise(IAutoWikiBrowser sender)
+        {
+            AWB = sender;
+            AWB.LogControl.LogAdded += LogControl_LogAdded;
+            AWB.AddMainFormClosingEventHandler(UploadFinishedArticlesToServer);
+            AWB.AddArticleRedirectedEventHandler(ArticleRedirected);
+
+            pluginMenuItem.DropDownItems.Add(pluginUploadMenuItem);
+            pluginMenuItem.DropDownItems.Add(pluginReAddArticlesMenuItem);
+            pluginUploadMenuItem.Click += pluginUploadMenuItem_Click;
+
+            pluginReAddArticlesMenuItem.Click += pluginReAddArticlesMenuItem_Click;
+            sender.PluginsToolStripMenuItem.DropDownItems.Add(pluginMenuItem);
+
+            aboutMenuItem.Click += aboutMenuItem_Click;
+            sender.HelpToolStripMenuItem.DropDownItems.Add(aboutMenuItem);
+        }
+
+        private static void ArticleRedirected(string oldTitle, string newTitle)
+        {
+            int id;
+            if (PageList.TryGetValue(oldTitle, out id))
+            {
+                PageList.Remove(oldTitle);
+                PageList.Add(newTitle, id);
+            }
+        }
+
+        private static void pluginReAddArticlesMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (string a in PageList.Keys)
+            {
+                if (SkippedPagesThisSession.Contains(a) || SavedPagesThisSession.Contains(a))
+                    continue;
+
+                AWB.ListMaker.Add(new Article(a));
+            }
+        }
+
+        private static void aboutMenuItem_Click(object sender, EventArgs e)
+        {
+            new About().Show();
+        }
+
+        private static void pluginUploadMenuItem_Click(object sender, EventArgs e)
+        {
+            UploadFinishedArticlesToServer();
+        }
+
+        private static void LogControl_LogAdded(bool Skipped, Logging.AWBLogListener LogListener)
+        {
+            int articleID;
+            if ((PageList.Count > 0) && (PageList.TryGetValue(LogListener.Text, out articleID)))
+            {
+                if (Skipped)
+                {
+                    SkippedPages.Add(articleID.ToString());
+                    SkippedReasons.Add(LogListener.SkipReason);
+                    SkippedPagesThisSession.Add(articleID.ToString());
+                }
+                else
+                {
+                    SavedPages.Add(articleID.ToString());
+                    SavedPagesThisSession.Add(articleID.ToString());
+                }
+
+                if (EditAndIgnoredPages >= 25)
+                    UploadFinishedArticlesToServer();
+            }
+        }
+
+        public string Name
+        {
+            get { return "TypoScan AWB Plugin"; }
+        }
+
+        public string WikiName
+        {
+            get
+            {
+                return "[[Wikipedia:TypoScan|TypoScan AWB Plugin]], Plugin version " +
+                    System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            }
+        }
+
+        public string ProcessArticle(IAutoWikiBrowser sender, ProcessArticleEventArgs eventargs)
+        {
+            return eventargs.ArticleText;
+        }
+
+        public void LoadSettings(object[] prefs)
+        {
+            //if (prefs == null) return;
+
+            //foreach (object o in prefs)
+            //{
+            //    PrefsKeyPair p = o as PrefsKeyPair;
+            //    if (p == null) continue;
+
+            //    switch (p.Name.ToLower())
+            //    {
+            //        case "pagelist":
+            //            PageList = (Dictionary<string, int>)p.Setting;
+            //            break;
+            //    }
+            //}
+        }
+
+        public object[] SaveSettings()
+        {
+            //PrefsKeyPair[] prefs = new PrefsKeyPair[1];
+            //prefs[0] = new PrefsKeyPair("PageList", PageList);
+
+            //return prefs;
+            return new object[0];
+        }
+
+        public void Reset()
+        { }
+
+        public void Nudge(out bool Cancel)
+        {
+            Cancel = false;
+        }
+
+        public void Nudged(int Nudges)
+        { }
+
+        #endregion
+
+        private static void UploadFinishedArticlesToServer(object sender, FormClosingEventArgs e)
+        {
+            UploadFinishedArticlesToServer();
+        }
+
+        private static int editsAndIgnored;
+        private static bool isUploading;
+
+        static readonly BackgroundRequest thread = new BackgroundRequest(UploadFinishedArticlesToServerFinished,
+                                                            UploadFinishedArticlesToServerErrored);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void UploadFinishedArticlesToServer()
+        {
+            if (isUploading || EditAndIgnoredPages == 0)
+                return;
+
+            isUploading = true;
+
+            editsAndIgnored = EditAndIgnoredPages;
+
+            AWB.StartProgressBar();
+            AWB.StatusLabelText = "Uploading " + editsAndIgnored + " TypoScan articles to server...";
+
+            NameValueCollection postVars = new NameValueCollection();
+
+            postVars.Add("articles", string.Join(",", SavedPages.ToArray()));
+            postVars.Add("skipped", string.Join(",", SkippedPages.ToArray()));
+            postVars.Add("skipreason", string.Join(",", SkippedReasons.ToArray()));
+
+            postVars.Add("user", AWB.Privacy ? "[withheld]" : Variables.User.Name);
+
+            if (!AWB.Shutdown)
+                thread.PostData(Common.GetUrlFor("finished"), postVars);
+            else
+                UploadResult(Tools.PostData(postVars, Common.GetUrlFor("finished")));
+
+        }
+
+        private static void UploadFinishedArticlesToServerFinished(BackgroundRequest req)
+        {
+            UploadResult(req.Result.ToString());
+        }
+
+        private static void UploadResult(string result)
+        {
+            if (string.IsNullOrEmpty(Common.CheckOperation(result)))
+            {
+                UploadedThisSession += editsAndIgnored;
+                SavedPages.Clear();
+                SkippedPages.Clear();
+                SkippedReasons.Clear();
+
+                if ((UploadedThisSession % 100) == 0)
+                    CheckoutTime = DateTime.Now;
+            }
+
+            AWB.StopProgressBar();
+            AWB.StatusLabelText = "";
+            isUploading = false;
+        }
+
+        private static void UploadFinishedArticlesToServerErrored(BackgroundRequest req)
+        {
+            AWB.StopProgressBar();
+            AWB.StatusLabelText = "TypoScan reporting failed";
+            isUploading = false;
+
+            if (req.ErrorException is System.IO.IOException)
+            {
+                Tools.WriteDebug("TypoScanAWBPlugin", req.ErrorException.Message);
+            }
+            else if (req.ErrorException is System.Net.WebException)
+            {
+                Tools.WriteDebug("TypoScanAWBPlugin", req.ErrorException.Message);
+            }
+        }
+
+        internal static int EditAndIgnoredPages
+        {
+            get { return (SavedPages.Count + SkippedPages.Count); }
+        }
+    }
+}
