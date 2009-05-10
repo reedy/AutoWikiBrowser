4b458a3e2cd0a62ae79986c879372f85d65ee371
Sam Reed <sam@reedyboy.net>
2009-05-10 20:47:07 +0000

    4.5.3.2 for release (bugfixding!)
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@4312 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Properties/AssemblyInfo.cs
+++ AWB/AWB/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.5.3.1")]
-[assembly: AssemblyFileVersion("4.5.3.1")]
+[assembly: AssemblyVersion("4.5.3.2")]
+[assembly: AssemblyFileVersion("4.5.3.2")]
