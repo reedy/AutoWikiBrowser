db38ff14f2b3a5738a9120e5e688f11cb464316f
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2009-06-16 18:29:07 +0000

    another bugfix on FixPeopleCategories
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@4542 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Parse/Parsers.cs
+++ WikiFunctions/Parse/Parsers.cs
@@ -3219,7 +3219,7 @@ a='" + a + "',  b='" + b + "'", "StickyLinks error");
 
         private static readonly Regex PersonYearOfBirth = new Regex(@"(?<='''.{0,100}?)\( *[Bb]orn[^\)\.;]{1,150}?(?<!.*(?:[Dd]ied|&[nm]dash;|—).*)([12]?\d{3}(?: BC)?)\b[^\)]{0,200}");
         private static readonly Regex PersonYearOfDeath = new Regex(@"(?<='''.{0,100}?)\([^\(\)]*?[Dd]ied[^\)\.;]+?([12]?\d{3}(?: BC)?)\b");
-        private static readonly Regex PersonYearOfBirthAndDeath = new Regex(@"'''\s*\([^\)\r\n]*?(?<![Dd]ied)\b([12]?\d{3})\b[^\)\r\n]*?(-|–|—|&[nm]dash;)[^\)\r\n]*?([12]?\d{3})\b[^\)]{0,200}");
+        private static readonly Regex PersonYearOfBirthAndDeath = new Regex(@"^.{0,100}'''\s*\([^\)\r\n]*?(?<![Dd]ied)\b([12]?\d{3})\b[^\)\r\n]*?(-|–|—|&[nm]dash;)[^\)\r\n]*?([12]?\d{3})\b[^\)]{0,200}", RegexOptions.Singleline);
 
         private static readonly Regex UncertainWordings = new Regex(@"(?:\b(about|before|after|either|around|late|[Cc]irca|between|\d{3,4}(?:\]\])?/(?:\[\[)?\d{1,4}|or +\d{3,})\b|\d{3} *\?|\bca?(?:'')?\.|\bca\b|\b(bef|abt)\.)");
         private static readonly Regex ReignedRuledUnsure = new Regex(@"(?:\?|[Rr](?:uled|eigned)|\br\.|(chr|fl(?:\]\])?)\.)");
