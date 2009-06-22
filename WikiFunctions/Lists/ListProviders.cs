c52a763b7916d0b312fbdfc58494ca5d1d1d54c4
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2009-06-22 21:46:35 +0000

    HTML scraper to handle &amp; when extracting page names
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@4593 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Lists/ListProviders.cs
+++ WikiFunctions/Lists/ListProviders.cs
@@ -1772,6 +1772,7 @@ namespace WikiFunctions.Lists
 
         protected virtual string ModifyArticleName(string title)
         {
+            title = title.Replace(@"&amp;", "&");
             return title.Replace("<br />", "");
         }
 
