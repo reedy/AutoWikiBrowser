7d97e9e533efa41b87e56ad3cd48a89111a41868
Sam Reed <sam@reedyboy.net>
2009-06-28 20:49:51 +0000

    Remove comments
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@4639 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Logging/TraceManager.cs
+++ AWB/WikiFunctions/Logging/TraceManager.cs
@@ -16,8 +16,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-// Originally from WikiFunctions2.dll (converted from VB to C#).
-
 using System;
 using System.Collections.Generic;
 using WikiFunctions.Logging.Uploader;
