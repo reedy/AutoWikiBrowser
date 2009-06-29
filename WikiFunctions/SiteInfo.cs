27bf5b43955197fe7b4c163e47f7747d2be21a0c
Sam Reed <sam@reedyboy.net>
2009-06-29 22:10:25 +0000

    GPL Header + Update
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@4677 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/SiteInfo.cs
+++ WikiFunctions/SiteInfo.cs
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2008 Max Semenik
+Copyright (C) 2009 Max Semenik
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
 using System;
 using System.Collections.Generic;
 using System.Xml;
