5a0e698c9207aa6bb0c0cd68787ba7ccee9719b4
Sam Reed <sam@reedyboy.net>
2009-07-10 18:31:55 +0000

    More bugfixing
    AssessmentComments is probably borked.. Not sure if we needed that webcontrol.. Maybe just display the text to the user in the future.. Needs user input there!
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@4837 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/Kingbotk/AWB Plugin/AssessmentComments.Designer.vb
+++ Plugins/Kingbotk/AWB Plugin/AssessmentComments.Designer.vb
@@ -21,11 +21,8 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
         <System.Diagnostics.DebuggerStepThrough()> _
         Private Sub InitializeComponent()
             Me.components = New System.ComponentModel.Container
-            Me.WebControl1 = New WikiFunctions.Browser.WebControl
             Me.PictureBox1 = New System.Windows.Forms.PictureBox
             Me.ReferencesButton = New System.Windows.Forms.Button
-            Me.StatusStrip1 = New System.Windows.Forms.StatusStrip
-            Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel
             Me.CitationsButton = New System.Windows.Forms.Button
             Me.PhotoButton = New System.Windows.Forms.Button
             Me.CleanupButton = New System.Windows.Forms.Button
@@ -37,29 +34,17 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             Me.LeadButton = New System.Windows.Forms.Button
             Me.SectionsButton = New System.Windows.Forms.Button
             Me.ToneButton = New System.Windows.Forms.Button
-            Me.Label2 = New System.Windows.Forms.Label
             Me.SkipButton = New System.Windows.Forms.Button
+            Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel
+            Me.StatusStrip1 = New System.Windows.Forms.StatusStrip
             CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
             Me.StatusStrip1.SuspendLayout()
             Me.SuspendLayout()
             '
-            'WebControl1
-            '
-            Me.WebControl1.AllowNavigation = False
-            Me.WebControl1.Busy = False
-            Me.WebControl1.Location = New System.Drawing.Point(0, 0)
-            Me.WebControl1.MinimumSize = New System.Drawing.Size(20, 20)
-            Me.WebControl1.Name = "WebControl1"
-            Me.WebControl1.ScriptErrorsSuppressed = True
-            Me.WebControl1.Size = New System.Drawing.Size(807, 450)
-            Me.WebControl1.TabIndex = 0
-            Me.WebControl1.TimeoutLimit = 30
-            Me.WebControl1.WebBrowserShortcutsEnabled = False
-            '
             'PictureBox1
             '
             Me.PictureBox1.Image = Global.My.Resources.Resources.WP1
-            Me.PictureBox1.Location = New System.Drawing.Point(12, 460)
+            Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
             Me.PictureBox1.Name = "PictureBox1"
             Me.PictureBox1.Size = New System.Drawing.Size(64, 61)
             Me.PictureBox1.TabIndex = 3
@@ -67,7 +52,7 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             '
             'ReferencesButton
             '
-            Me.ReferencesButton.Location = New System.Drawing.Point(119, 460)
+            Me.ReferencesButton.Location = New System.Drawing.Point(119, 12)
             Me.ReferencesButton.Name = "ReferencesButton"
             Me.ReferencesButton.Size = New System.Drawing.Size(75, 23)
             Me.ReferencesButton.TabIndex = 4
@@ -75,23 +60,9 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             Me.ToolTip1.SetToolTip(Me.ReferencesButton, "Article needs references")
             Me.ReferencesButton.UseVisualStyleBackColor = True
             '
-            'StatusStrip1
-            '
-            Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
-            Me.StatusStrip1.Location = New System.Drawing.Point(0, 561)
-            Me.StatusStrip1.Name = "StatusStrip1"
-            Me.StatusStrip1.Size = New System.Drawing.Size(808, 22)
-            Me.StatusStrip1.TabIndex = 5
-            Me.StatusStrip1.Text = "StatusStrip1"
-            '
-            'ToolStripStatusLabel1
-            '
-            Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
-            Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(0, 17)
-            '
             'CitationsButton
             '
-            Me.CitationsButton.Location = New System.Drawing.Point(119, 489)
+            Me.CitationsButton.Location = New System.Drawing.Point(119, 41)
             Me.CitationsButton.Name = "CitationsButton"
             Me.CitationsButton.Size = New System.Drawing.Size(75, 23)
             Me.CitationsButton.TabIndex = 6
@@ -101,7 +72,7 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             '
             'PhotoButton
             '
-            Me.PhotoButton.Location = New System.Drawing.Point(119, 518)
+            Me.PhotoButton.Location = New System.Drawing.Point(119, 70)
             Me.PhotoButton.Name = "PhotoButton"
             Me.PhotoButton.Size = New System.Drawing.Size(75, 23)
             Me.PhotoButton.TabIndex = 7
@@ -111,7 +82,7 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             '
             'CleanupButton
             '
-            Me.CleanupButton.Location = New System.Drawing.Point(200, 460)
+            Me.CleanupButton.Location = New System.Drawing.Point(200, 12)
             Me.CleanupButton.Name = "CleanupButton"
             Me.CleanupButton.Size = New System.Drawing.Size(75, 23)
             Me.CleanupButton.TabIndex = 8
@@ -121,7 +92,7 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             '
             'ExpansionButton
             '
-            Me.ExpansionButton.Location = New System.Drawing.Point(200, 489)
+            Me.ExpansionButton.Location = New System.Drawing.Point(200, 41)
             Me.ExpansionButton.Name = "ExpansionButton"
             Me.ExpansionButton.Size = New System.Drawing.Size(75, 23)
             Me.ExpansionButton.TabIndex = 9
@@ -131,7 +102,7 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             '
             'CopyeditButton
             '
-            Me.CopyeditButton.Location = New System.Drawing.Point(200, 518)
+            Me.CopyeditButton.Location = New System.Drawing.Point(200, 70)
             Me.CopyeditButton.Name = "CopyeditButton"
             Me.CopyeditButton.Size = New System.Drawing.Size(75, 23)
             Me.CopyeditButton.TabIndex = 10
@@ -144,7 +115,7 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             Me.btnSave.Enabled = False
             Me.btnSave.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
             Me.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-            Me.btnSave.Location = New System.Drawing.Point(681, 460)
+            Me.btnSave.Location = New System.Drawing.Point(681, 12)
             Me.btnSave.Name = "btnSave"
             Me.btnSave.Size = New System.Drawing.Size(102, 32)
             Me.btnSave.TabIndex = 31
@@ -156,7 +127,7 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             'Label1
             '
             Me.Label1.AutoSize = True
-            Me.Label1.Location = New System.Drawing.Point(362, 460)
+            Me.Label1.Location = New System.Drawing.Point(362, 12)
             Me.Label1.Name = "Label1"
             Me.Label1.Size = New System.Drawing.Size(174, 52)
             Me.Label1.TabIndex = 32
@@ -165,7 +136,7 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             '
             'LeadButton
             '
-            Me.LeadButton.Location = New System.Drawing.Point(281, 460)
+            Me.LeadButton.Location = New System.Drawing.Point(281, 12)
             Me.LeadButton.Name = "LeadButton"
             Me.LeadButton.Size = New System.Drawing.Size(75, 23)
             Me.LeadButton.TabIndex = 35
@@ -175,7 +146,7 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             '
             'SectionsButton
             '
-            Me.SectionsButton.Location = New System.Drawing.Point(281, 489)
+            Me.SectionsButton.Location = New System.Drawing.Point(281, 41)
             Me.SectionsButton.Name = "SectionsButton"
             Me.SectionsButton.Size = New System.Drawing.Size(75, 23)
             Me.SectionsButton.TabIndex = 36
@@ -185,7 +156,7 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             '
             'ToneButton
             '
-            Me.ToneButton.Location = New System.Drawing.Point(281, 518)
+            Me.ToneButton.Location = New System.Drawing.Point(281, 70)
             Me.ToneButton.Name = "ToneButton"
             Me.ToneButton.Size = New System.Drawing.Size(75, 23)
             Me.ToneButton.TabIndex = 37
@@ -193,36 +164,38 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             Me.ToolTip1.SetToolTip(Me.ToneButton, "Article needs enyclopedic tone")
             Me.ToneButton.UseVisualStyleBackColor = True
             '
-            'Label2
-            '
-            Me.Label2.AutoSize = True
-            Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-            Me.Label2.Location = New System.Drawing.Point(507, 502)
-            Me.Label2.Name = "Label2"
-            Me.Label2.Size = New System.Drawing.Size(276, 39)
-            Me.Label2.TabIndex = 33
-            Me.Label2.Text = "Please do NOT click the save button inside the" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "above browser window. Click the s" & _
-                "ave button" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "above this label instead."
-            '
             'SkipButton
             '
-            Me.SkipButton.Location = New System.Drawing.Point(590, 460)
+            Me.SkipButton.Location = New System.Drawing.Point(565, 12)
             Me.SkipButton.Name = "SkipButton"
-            Me.SkipButton.Size = New System.Drawing.Size(75, 23)
+            Me.SkipButton.Size = New System.Drawing.Size(100, 32)
             Me.SkipButton.TabIndex = 34
             Me.SkipButton.Text = "Skip"
             Me.SkipButton.UseVisualStyleBackColor = True
+            '
+            'ToolStripStatusLabel1
+            '
+            Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+            Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(0, 17)
+            '
+            'StatusStrip1
+            '
+            Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+            Me.StatusStrip1.Location = New System.Drawing.Point(0, 102)
+            Me.StatusStrip1.Name = "StatusStrip1"
+            Me.StatusStrip1.Size = New System.Drawing.Size(803, 22)
+            Me.StatusStrip1.TabIndex = 5
+            Me.StatusStrip1.Text = "StatusStrip1"
             '
             'AssessmentComments
             '
             Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-            Me.ClientSize = New System.Drawing.Size(808, 583)
+            Me.ClientSize = New System.Drawing.Size(803, 124)
             Me.Controls.Add(Me.ToneButton)
             Me.Controls.Add(Me.SectionsButton)
             Me.Controls.Add(Me.LeadButton)
             Me.Controls.Add(Me.SkipButton)
-            Me.Controls.Add(Me.Label2)
             Me.Controls.Add(Me.Label1)
             Me.Controls.Add(Me.btnSave)
             Me.Controls.Add(Me.CopyeditButton)
@@ -233,7 +206,6 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             Me.Controls.Add(Me.StatusStrip1)
             Me.Controls.Add(Me.ReferencesButton)
             Me.Controls.Add(Me.PictureBox1)
-            Me.Controls.Add(Me.WebControl1)
             Me.Name = "AssessmentComments"
             Me.ShowIcon = False
             Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
@@ -245,10 +217,8 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
             Me.PerformLayout()
 
         End Sub
-        Private WithEvents WebControl1 As WikiFunctions.Browser.WebControl
         Private WithEvents PictureBox1 As System.Windows.Forms.PictureBox
         Private WithEvents ReferencesButton As System.Windows.Forms.Button
-        Private WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
         Private WithEvents CitationsButton As System.Windows.Forms.Button
         Private WithEvents PhotoButton As System.Windows.Forms.Button
         Private WithEvents CleanupButton As System.Windows.Forms.Button
@@ -257,11 +227,11 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
         Private WithEvents btnSave As System.Windows.Forms.Button
         Private WithEvents Label1 As System.Windows.Forms.Label
         Private WithEvents ToolTip1 As System.Windows.Forms.ToolTip
-        Private WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
-        Private WithEvents Label2 As System.Windows.Forms.Label
         Private WithEvents SkipButton As System.Windows.Forms.Button
         Private WithEvents LeadButton As System.Windows.Forms.Button
         Private WithEvents SectionsButton As System.Windows.Forms.Button
         Private WithEvents ToneButton As System.Windows.Forms.Button
+        Private WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+        Private WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
     End Class
 End Namespace
