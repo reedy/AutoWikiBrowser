65ec1449366b76594ae287484eeb73c538ac9125
Rick <delichon_urbica@users.sourceforge.net>
2009-07-12 08:04:01 +0000

    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/branches/WebBrowser@4869 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ Plugins/TheTemplator/DebugOutput.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace AutoWikiBrowser.Plugins.TheTemplator
+{
+	partial class DebugOutput
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox = new System.Windows.Forms.RichTextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox
+			// 
+			this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox.Location = new System.Drawing.Point(12, 12);
+			this.textBox.Name = "textBox";
+			this.textBox.Size = new System.Drawing.Size(529, 319);
+			this.textBox.TabIndex = 0;
+			this.textBox.Text = "";
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.button1.Location = new System.Drawing.Point(466, 337);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "&Close";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// DebugOutput
+			// 
+			this.AcceptButton = this.button1;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.button1;
+			this.ClientSize = new System.Drawing.Size(553, 372);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox);
+			this.Name = "DebugOutput";
+			this.ShowIcon = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Debug output";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		internal System.Windows.Forms.RichTextBox textBox;
+	}
+}
---
+++ Plugins/TheTemplator/DebugOutput.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutoWikiBrowser.Plugins.TheTemplator
+{
+	public partial class DebugOutput : Form
+	{
+		public DebugOutput()
+		{
+			InitializeComponent();
+
+			HeaderFonts = new Font[] { 
+				new Font("Arial", 14, FontStyle.Bold | FontStyle.Underline),
+				new Font("Arial", 13, FontStyle.Bold),
+				new Font("Arial", 12, FontStyle.Bold | FontStyle.Underline),
+				new Font("Arial", 11, FontStyle.Bold),
+				new Font("Arial", 10, FontStyle.Bold | FontStyle.Underline),
+			};
+		}
+
+		public void AddSection(string Heading, string Text)
+		{
+			textBox.SelectionFont = HeaderFonts[Math.Max(0, Math.Min(HeaderFont, HeaderFonts.GetUpperBound(0)))];
+			textBox.SelectedText = Heading + "\n";
+			textBox.SelectionFont = body;
+			textBox.SelectionIndent += indent;
+			textBox.SelectedText = Text + "\n\n";
+			textBox.SelectionIndent -= indent;
+		}
+
+		public void StartSection(string Heading)
+		{
+			textBox.SelectionFont = HeaderFonts[Math.Max(0, Math.Min(HeaderFont, HeaderFonts.GetUpperBound(0)))];
+			textBox.SelectedText = Heading + "\n";
+			++HeaderFont;
+			textBox.SelectionIndent += indent;
+		}
+
+		public void EndSection()
+		{
+			System.Diagnostics.Debug.Assert(HeaderFont > 0);
+			if (HeaderFont > 0)
+				--HeaderFont;
+			textBox.SelectionIndent -= indent;
+		}
+
+		Font body = new Font("Lucida Console", 10);
+		int HeaderFont = 0;
+		Font[] HeaderFonts;
+		const int indent = 20;
+	}
+}
