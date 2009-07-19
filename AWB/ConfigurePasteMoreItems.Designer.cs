5f504ab7da7f866c0eebafdcab6bc0587a5933e1
Sam Reed <sam@reedyboy.net>
2009-07-19 15:37:35 +0000

    Fixup compile error
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@4940 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/ConfigurePasteMoreItems.Designer.cs
+++ AWB/ConfigurePasteMoreItems.Designer.cs
@@ -62,7 +62,6 @@
             this.okButton.TabIndex = 20;
             this.okButton.Text = "OK";
             this.okButton.UseVisualStyleBackColor = true;
-            this.okButton.Click += new System.EventHandler(this.okButton_Click);
             // 
             // cancelButton
             // 
