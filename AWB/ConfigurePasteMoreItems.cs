af097a9370d45a1de58ebd7385e8018f0fa77b1f
Sam Reed <sam@reedyboy.net>
2009-07-19 15:29:08 +0000

    Some minor tweaks to ConfigurePasteMoreItems
    GPL Header
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@4939 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/ConfigurePasteMoreItems.cs
+++ AWB/ConfigurePasteMoreItems.cs
@@ -1,9 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
+﻿/*
+Copyright (C) 2009
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
 using System.Windows.Forms;
 
 namespace AutoWikiBrowser
@@ -40,10 +53,5 @@ namespace AutoWikiBrowser
         public string String8 { get { return textBox8.Text; } private set { textBox8.Text = value; } }
         public string String9 { get { return textBox9.Text; } private set { textBox9.Text = value; } }
         public string String10 { get { return textBox10.Text; } private set { textBox10.Text = value; } }
-
-        private void okButton_Click(object sender, EventArgs e)
-        {
-            DialogResult = DialogResult.OK;
-        }
     }
 }
