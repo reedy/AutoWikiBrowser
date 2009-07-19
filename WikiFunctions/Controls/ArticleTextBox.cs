a2edf5f92813259aafd1f072e2d2ec3d94e4ac12
Sam Reed <sam@reedyboy.net>
2009-07-19 16:38:12 +0000

    4929,4933-4936 merged to WebBrowser branch
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/branches/WebBrowser@4945 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Controls/ArticleTextBox.cs
+++ WikiFunctions/Controls/ArticleTextBox.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+Copyright (C) 2009
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+using System;
+using System.Drawing;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
@@ -128,6 +148,161 @@ namespace WikiFunctions.Controls
             SuspendLayout();
             DetectUrls = false;
             ResumeLayout(false);
+        }
+
+        /// <summary>
+        /// Applies syntax highlighting to the input ArticleTextBox 
+        /// </summary>
+        /// <returns></returns>
+        public void HighlightSyntax()
+        {
+            Font currentFont = SelectionFont;
+            Font boldFont = new Font(currentFont.FontFamily, currentFont.Size, FontStyle.Bold);
+            Font italicFont = new Font(currentFont.FontFamily, currentFont.Size, FontStyle.Italic);
+            Font boldItalicFont = new Font(currentFont.FontFamily, currentFont.Size, FontStyle.Bold | FontStyle.Italic);
+
+            // headings text in bold
+            foreach (Match m in WikiRegexes.Heading.Matches(RawText))
+            {
+                SetEditBoxSelection(m.Groups[2].Index, m.Groups[2].Length);
+                SelectionFont = boldFont;
+            }
+
+            // templates grey background
+            foreach (Match m in WikiRegexes.NestedTemplates.Matches(RawText))
+            {
+                SetEditBoxSelection(m.Index, m.Length);
+                SelectionBackColor = Color.LightGray;
+            }
+
+            // * items grey background
+            foreach (Match m in WikiRegexes.StarRows.Matches(RawText))
+            {
+                SetEditBoxSelection(m.Index, m.Length);
+                SelectionBackColor = Color.LightGray;
+
+                SetEditBoxSelection(m.Groups[1].Index, m.Groups[1].Length);
+                SelectionFont = boldFont;
+            }
+
+            // template names dark blue font
+            foreach (Match m in WikiRegexes.TemplateName.Matches(RawText))
+            {
+                SetEditBoxSelection(m.Groups[1].Index, m.Groups[1].Length);
+                SelectionColor = Color.DarkBlue;
+            }
+
+            // refs grey background
+            foreach (Match m in WikiRegexes.Refs.Matches(RawText))
+            {
+                SetEditBoxSelection(m.Index, m.Length);
+                SelectionBackColor = Color.LightGray;
+            }
+
+            // external links grey background, blue bold
+            foreach (Match m in WikiRegexes.ExternalLinks.Matches(RawText))
+            {
+                SetEditBoxSelection(m.Index, m.Length);
+                SelectionColor = Color.Blue;
+                SelectionFont = boldFont;
+            }
+
+            // images green background
+            //foreach (Match m in WikiRegexes.Images.Matches(txtEdit.RawText))
+            //{
+            //    txtEdit.SetEditBoxSelection(m.Index, m.Length);
+            //    txtEdit.SelectionBackColor = Color.Green;
+
+            //}
+
+            // italics
+            foreach (Match m in WikiRegexes.Italics.Matches(RawText))
+            {
+                SetEditBoxSelection(m.Groups[1].Index, m.Groups[1].Length);
+                SelectionFont = italicFont;
+            }
+
+            // bold  
+            foreach (Match m in WikiRegexes.Bold.Matches(RawText))
+            {
+                // reset anything incorrectly done by italics  earlier
+                SetEditBoxSelection(m.Index, m.Length);
+                SelectionFont = currentFont;
+
+                SetEditBoxSelection(m.Groups[1].Index, m.Groups[1].Length);
+                SelectionFont = boldFont;
+            }
+
+            // bold italics 
+            foreach (Match m in WikiRegexes.BoldItalics.Matches(RawText))
+            {
+                // reset anything incorrectly done by italics/bold earlier
+                SetEditBoxSelection(m.Index, m.Length);
+                SelectionFont = currentFont;
+
+                SetEditBoxSelection(m.Groups[1].Index, m.Groups[1].Length);
+                SelectionFont = boldItalicFont;
+            }
+
+            // piped wikilink text in blue, piped part in bold
+            foreach (Match m in WikiRegexes.PipedWikiLink.Matches(RawText))
+            {
+                SetEditBoxSelection(m.Groups[2].Index, m.Groups[2].Length);
+                SelectionColor = Color.Blue;
+                SelectionFont = boldFont;
+
+                SetEditBoxSelection(m.Groups[1].Index, m.Groups[1].Length);
+                SelectionColor = Color.Blue;
+            }
+
+            // unpiped wikilinks in blue and bold
+            foreach (Match m in WikiRegexes.UnPipedWikiLink.Matches(RawText))
+            {
+                SetEditBoxSelection(m.Groups[1].Index, m.Groups[1].Length);
+                SelectionColor = Color.Blue;
+                SelectionFont = boldFont;
+            }
+
+            // pipe trick: in blue bold too
+            foreach (Match m in WikiRegexes.WikiLinksOnlyPlusWord.Matches(RawText))
+            {
+                SetEditBoxSelection(m.Groups[1].Index, m.Groups[1].Length);
+                SelectionColor = Color.Blue;
+                SelectionFont = boldFont;
+            }
+
+            // cats grey background
+            foreach (Match m in WikiRegexes.Category.Matches(RawText))
+            {
+                SetEditBoxSelection(m.Index, m.Length);
+                SelectionBackColor = Color.LightGray;
+                SelectionFont = currentFont;
+                SelectionColor = Color.Black;
+
+                SetEditBoxSelection(m.Groups[1].Index, m.Groups[1].Length);
+                SelectionColor = Color.Blue;
+            }
+
+            // interwikis dark grey background
+            foreach (Match m in WikiRegexes.PossibleInterwikis.Matches(RawText))
+            {
+                SetEditBoxSelection(m.Index, m.Length);
+                SelectionBackColor = Color.Gray;
+                SelectionFont = currentFont;
+
+                SetEditBoxSelection(m.Groups[2].Index, m.Groups[2].Length);
+                SelectionColor = Color.Blue;
+
+                SetEditBoxSelection(m.Groups[1].Index, m.Groups[1].Length);
+                SelectionColor = Color.Black;
+            }
+
+            // comments dark orange background
+            foreach (Match m in WikiRegexes.Comments.Matches(RawText))
+            {
+                SetEditBoxSelection(m.Index, m.Length);
+                SelectionBackColor = Color.PaleGoldenrod;
+            }
         }
     }
 }
