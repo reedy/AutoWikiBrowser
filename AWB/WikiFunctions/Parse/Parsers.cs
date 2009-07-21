6f5d7c9bbae2e9a7832f43aba891405e731ed2f9
Sam Reed <sam@reedyboy.net>
2009-07-21 21:51:47 +0000

    Modifier tweaks. Fixup xml comment blocks
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@4968 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Parse/Parsers.cs
+++ AWB/WikiFunctions/Parse/Parsers.cs
@@ -716,7 +716,7 @@ namespace WikiFunctions.Parse
         /// Extracts strings from an input string using the input regex to derive a name for a reference
         /// </summary>
         /// <param name="reference">value of the reference needing a name</param>
-        /// <param name="mask">regular expression to apply</param>
+        /// <param name="referenceNameMask">regular expression to apply</param>
         /// <param name="components">number of groups to extract</param>
         /// <returns>the derived reference name</returns>
         private static string ExtractReferenceNameComponents(string reference, Regex referenceNameMask, int components)
@@ -940,20 +940,22 @@ namespace WikiFunctions.Parse
             return newText;
         }
 
-        private static readonly string siCitStart = @"(?si)(\{\{\s*cit[^{}]*\|\s*";
-        private static readonly string citAccessdate = siCitStart + @"(?:access|archive)date\s*=\s*";
-        private static readonly string citDate = siCitStart + @"(?:archive|air)?date2?\s*=\s*";
-        private static readonly string citYMonthD = siCitStart + @"(?:archive|air)?date2?\s*=\s*\d{4})[-/\s]";
-        private static readonly string dTemEnd = @"?[-/\s]([0-3]?\d\s*(?:\||}}))";
+        private const string siCitStart = @"(?si)(\{\{\s*cit[^{}]*\|\s*";
+        private const string citAccessdate = siCitStart + @"(?:access|archive)date\s*=\s*";
+        private const string citDate = siCitStart + @"(?:archive|air)?date2?\s*=\s*";
+        private const string citYMonthD = siCitStart + @"(?:archive|air)?date2?\s*=\s*\d{4})[-/\s]";
+        private const string dTemEnd = @"?[-/\s]([0-3]?\d\s*(?:\||}}))";
+
         struct RegexReplacement {
             public RegexReplacement(Regex regex, string replacement) { Regex=regex; Replacement=replacement; }
-            public Regex Regex;
-            public string Replacement;
+            public readonly Regex Regex;
+            public readonly string Replacement;
             // This could get extended to be a class with some of the Regex methods, such as IsMatch, Replace, etc, but there's little point
         }
+
         //TODO: give these regexes, used in CiteTemplateDates, more meaningful names
         private static readonly Regex CTD1 = new Regex(@"\b(access|archive)date\s*=", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-        private static readonly RegexReplacement[] CTD2 = new RegexReplacement[] {
+        private static readonly RegexReplacement[] CTD2 = new [] {
             new RegexReplacement(new Regex(citAccessdate + @")(1[0-2])[/_\-\.]?(1[3-9])[/_\-\.]?(?:20)?([01]\d)(?=\s*(?:\||}}))", RegexOptions.Compiled), "${1}20$4-$2-$3"),
             new RegexReplacement(new Regex(citAccessdate + @")(1[0-2])[/_\-\.]?([2-3]\d)[/_\-\.]?(?:20)?([01]\d)(?=\s*(?:\||}}))", RegexOptions.Compiled), "${1}20$4-$2-$3"),
             new RegexReplacement(new Regex(citAccessdate + @")(1[0-2])[/_\-\.]?\2[/_\-\.]?(?:20)?([01]\d)(?=\s*(?:\||}}))", RegexOptions.Compiled), "${1}20$3-$2-$2"), // nn-nn-2004 and nn-nn-04 to ISO format (both nn the same)
@@ -972,8 +974,9 @@ namespace WikiFunctions.Parse
             new RegexReplacement(new Regex(citAccessdate + @")0?([1-9])[/_\-\.]?(1[3-9]|[2-3]\d)[/_\-\.]?(?:20)?([01]\d)(?=\s*(?:\||}}))", RegexOptions.Compiled), "${1}20$4-0$2-$3"),
             new RegexReplacement(new Regex(citAccessdate + @")0?([1-9])[/_\-\.]?0?\2[/_\-\.]?(?:20)?([01]\d)(?=\s*(?:\||}}))", RegexOptions.Compiled), "${1}20$3-0$2-0$2"), // n-n-2004 and n-n-04 to ISO format (both n the same)
         };
+
         private static readonly Regex CTD3 = new Regex(@"{{\s*cit[^{}]*\|\s*(?:archive|air)?date2?\s*=", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
-        private static readonly RegexReplacement[] CTD4 = new RegexReplacement[] {
+        private static readonly RegexReplacement[] CTD4 = new [] {
             new RegexReplacement(new Regex(citDate + @"\[?\[?)(200\d|19[7-9]\d)[/_]?([0-1]\d)[/_]?([0-3]\d\s*(?:\||}}))", RegexOptions.Compiled), "$1$2-$3-$4"),
             new RegexReplacement(new Regex(citDate + @"\[?\[?)(1[0-2])[/_\-\.]?([2-3]\d)[/_\-\.]?(19[7-9]\d)(?=\s*(?:\||}}))", RegexOptions.Compiled), "$1$4-$2-$3"),
             new RegexReplacement(new Regex(citDate + @"\[?\[?)0?([1-9])[/_\-\.]?([2-3]\d)[/_\-\.]?(19[7-9]\d)(?=\s*(?:\||}}))", RegexOptions.Compiled), "$1$4-0$2-$3"),
@@ -1002,7 +1005,8 @@ namespace WikiFunctions.Parse
             new RegexReplacement(new Regex(citDate + @")((?:\[\[)?200\d|19[7-9]\d)([0-1]\d)[/_\-\.]([0-3]\d(?:\]\])?\s*(?:\||}}))", RegexOptions.Compiled), "$1$2-$3-$4"),
             new RegexReplacement(new Regex(citDate + @")((?:\[\[)?200\d|19[7-9]\d)[/_\-\.]([0-1]\d)0?([0-3]\d(?:\]\])?\s*(?:\||}}))", RegexOptions.Compiled), "$1$2-$3-$4"),
         };
-        private static readonly RegexReplacement[] CTD5 = new RegexReplacement[] {
+
+        private static readonly RegexReplacement[] CTD5 = new [] {
             new RegexReplacement(new Regex(citYMonthD + @"Jan(?:uary|\.)" + dTemEnd, RegexOptions.Compiled), "$1-01-$2"),
             new RegexReplacement(new Regex(citYMonthD + @"Feb(?:r?uary|\.)" + dTemEnd, RegexOptions.Compiled), "$1-02-$2"),
             new RegexReplacement(new Regex(citYMonthD + @"Mar(?:ch|\.)" + dTemEnd, RegexOptions.Compiled), "$1-03-$2"),
@@ -1016,6 +1020,7 @@ namespace WikiFunctions.Parse
             new RegexReplacement(new Regex(citYMonthD + @"Nov(?:ember|\.)" + dTemEnd, RegexOptions.Compiled), "$1-11-$2"),
             new RegexReplacement(new Regex(citYMonthD + @"Dec(?:ember|\.)" + dTemEnd, RegexOptions.Compiled), "$1-12-$2"),
         };
+
         private static readonly Regex CTD6 = new Regex(siCitStart + @"(?:archive|air|access)?date2?\s*=\s*(?:\[\[)?200\d)-([2-3]\d|1[3-9])-(0[1-9]|1[0-2])(\]\])?", RegexOptions.Compiled);
         private static readonly Regex CTD7 = new Regex(@"(\{\{\s*cite[^\{\}]*\|\s*(?:archive|air|access)?date2?\s*=\s*(?:(?:200\d|19[7-9]\d)-[01]?\d-[0-3]?\d|[0-3]?\d\s*\w+,?\s*(?:200\d|19[7-9]\d)|\w+\s*[0-3]?\d,?\s*(?:200\d|19[7-9]\d)))(\s*[,-:]?\s+[0-2]?\d\:?[0-5]\d(?:\:?[0-5]\d)?\s*[^\|\}]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
 
@@ -1224,7 +1229,7 @@ namespace WikiFunctions.Parse
         }
 
         // whitespace cleaning
-        private static readonly RegexReplacement[] RefWhitespace = new RegexReplacement[] {
+        private static readonly RegexReplacement[] RefWhitespace = new [] {
             new RegexReplacement(new Regex(@"<\s*(?:\s+ref\s*|\s*ref\s+)>", RegexOptions.Compiled | RegexOptions.Singleline), "<ref>"),
             new RegexReplacement(new Regex(@"<(?:\s*/(?:\s+ref\s*|\s*ref\s+)|\s+/\s*ref\s*)>", RegexOptions.Compiled | RegexOptions.Singleline), "</ref>"),
         // remove any whitespace between consecutive references
