1b0c3da3752dffd25444803f90c893b392e2f856
Rick <delichon_urbica@users.sourceforge.net>
2009-07-23 20:01:22 +0000

    Reworked UI to specify a set of output parameters; reworked regex to manage unnamed and empty parameters; preparing to rework replacement to be more whitespace-sensitive.
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/branches/WebBrowser@4990 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/TheTemplator/DebugOutput.cs
+++ Plugins/TheTemplator/DebugOutput.cs
@@ -9,50 +9,57 @@ using System.Windows.Forms;
 
 namespace AutoWikiBrowser.Plugins.TheTemplator
 {
-	public partial class DebugOutput : Form
-	{
-		public DebugOutput()
-		{
-			InitializeComponent();
+    public partial class DebugOutput : Form
+    {
+        public DebugOutput()
+        {
+            InitializeComponent();
 
-			HeaderFonts = new Font[] { 
+            HeaderFonts = new Font[] { 
 				new Font("Arial", 14, FontStyle.Bold | FontStyle.Underline),
 				new Font("Arial", 13, FontStyle.Bold),
 				new Font("Arial", 12, FontStyle.Bold | FontStyle.Underline),
 				new Font("Arial", 11, FontStyle.Bold),
 				new Font("Arial", 10, FontStyle.Bold | FontStyle.Underline),
 			};
-		}
+        }
 
-		public void AddSection(string Heading, string Text)
-		{
-			textBox.SelectionFont = HeaderFonts[Math.Max(0, Math.Min(HeaderFont, HeaderFonts.GetUpperBound(0)))];
-			textBox.SelectedText = Heading + "\n";
-			textBox.SelectionFont = body;
-			textBox.SelectionIndent += indent;
-			textBox.SelectedText = Text + "\n\n";
-			textBox.SelectionIndent -= indent;
-		}
+        public void AddSection(string Heading, string Text)
+        {
+            textBox.SelectionFont = HeaderFonts[Math.Max(0, Math.Min(HeaderFont, HeaderFonts.GetUpperBound(0)))];
+            textBox.SelectedText = Heading + "\n";
+            textBox.SelectionFont = body;
+            textBox.SelectionIndent += indent;
+            textBox.SelectedText = Text + "\n\n";
+            textBox.SelectionIndent -= indent;
+        }
 
-		public void StartSection(string Heading)
-		{
-			textBox.SelectionFont = HeaderFonts[Math.Max(0, Math.Min(HeaderFont, HeaderFonts.GetUpperBound(0)))];
-			textBox.SelectedText = Heading + "\n";
-			++HeaderFont;
-			textBox.SelectionIndent += indent;
-		}
+        public void StartSection(string Heading)
+        {
+            textBox.SelectionFont = HeaderFonts[Math.Max(0, Math.Min(HeaderFont, HeaderFonts.GetUpperBound(0)))];
+            textBox.SelectedText = Heading + "\n";
+            ++HeaderFont;
+            textBox.SelectionIndent += indent;
+        }
 
-		public void EndSection()
-		{
-			System.Diagnostics.Debug.Assert(HeaderFont > 0);
-			if (HeaderFont > 0)
-				--HeaderFont;
-			textBox.SelectionIndent -= indent;
-		}
+        public void EndSection()
+        {
+            System.Diagnostics.Debug.Assert(HeaderFont > 0);
+            if (HeaderFont > 0)
+                --HeaderFont;
+            textBox.SelectionIndent -= indent;
+        }
 
-		Font body = new Font("Lucida Console", 10);
-		int HeaderFont = 0;
-		Font[] HeaderFonts;
-		const int indent = 20;
-	}
+        public void Clear()
+        {
+            HeaderFont = 0;
+            textBox.Text = "";
+            textBox.SelectionIndent = 0;
+        }
+
+        Font body = new Font("Lucida Console", 10);
+        int HeaderFont = 0;
+        Font[] HeaderFonts;
+        const int indent = 20;
+    }
 }
--- Plugins/TheTemplator/TemplatorConfig.cs
+++ Plugins/TheTemplator/TemplatorConfig.cs
@@ -17,33 +17,41 @@ namespace AutoWikiBrowser.Plugins.TheTemplator
 			InitializeComponent();
 			SetButtonStates();
 		}
-		public TemplatorConfig(string name, Dictionary<string, string> parameters, string replacement, bool skip)
+        public TemplatorConfig(string name,
+                               Dictionary<string, string> parameters,
+                               Dictionary<string, string> replacements,
+                               bool skip,
+                               bool removeExcessPipes)
 			: this()
 		{
 			// local vars
 			TemplateName = templateName.Text = name;
 			Parameters = parameters;
-			Replacement = replacementText.Text = replacement;
+			Replacements = replacements;
 			SkipIfNone = skipIfNone.Checked = skip;
+            RemoveExcessPipes = removeExcessPipes;
 
-			// Populate the listview
-			foreach (KeyValuePair<string, string> kvp in parameters)
-				templateParameters.Items.Add(new ListViewItem(new string[] { kvp.Key, kvp.Value }));
-		}
+			// Populate the listviews
+            foreach (KeyValuePair<string, string> kvp in parameters)
+                templateParameters.Items.Add(new ListViewItem(new string[] { kvp.Key, kvp.Value }));
+            foreach (KeyValuePair<string, string> kvp in replacements)
+                replacementParameters.Items.Add(new ListViewItem(new string[] { kvp.Key, kvp.Value }));
+        }
 
 		private void SetButtonStates()
 		{
-			editParam.Enabled = templateParameters.SelectedItems.Count == 1;
-			delParam.Enabled = templateParameters.SelectedItems.Count > 0;
-		}
+            editParam.Enabled = templateParameters.SelectedItems.Count == 1;
+            delParam.Enabled = templateParameters.SelectedItems.Count > 0;
+            editReplacement.Enabled = replacementParameters.SelectedItems.Count == 1;
+            delReplacement.Enabled = replacementParameters.SelectedItems.Count > 0;
+        }
 
-		private void OnAddParam(object sender, EventArgs e)
+		private void OnNewParam(object sender, EventArgs e)
 		{
 			TemplatorParameterDetails dlg = new TemplatorParameterDetails();
 			if (dlg.ShowDialog() == DialogResult.OK)
 				templateParameters.Items.Add(new ListViewItem(new string[] { dlg.ParamName, dlg.ParamRegex }));
 		}
-
 		private void OnEditParam(object sender, EventArgs e)
 		{
 			System.Diagnostics.Debug.Assert(templateParameters.SelectedItems.Count == 1);
@@ -55,70 +63,112 @@ namespace AutoWikiBrowser.Plugins.TheTemplator
 				item.SubItems[1].Text = dlg.ParamRegex;
 			}
 		}
-
 		private void OnDelParam(object sender, EventArgs e)
 		{
 			foreach (ListViewItem item in templateParameters.SelectedItems)
 				templateParameters.Items.Remove(item);
 		}
+        private void OnNewReplacementParam(object sender, EventArgs e)
+        {
+            TemplatorParameterDetails dlg = new TemplatorParameterDetails();
+            if (dlg.ShowDialog() == DialogResult.OK)
+                replacementParameters.Items.Add(new ListViewItem(new string[] { dlg.ParamName, dlg.ParamRegex }));
+        }
+        private void OnEditReplacementParam(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.Assert(replacementParameters.SelectedItems.Count == 1);
+            ListViewItem item = replacementParameters.SelectedItems[0];
+            TemplatorParameterDetails dlg = new TemplatorParameterDetails(item.SubItems[0].Text, item.SubItems[1].Text);
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                item.SubItems[0].Text = dlg.ParamName;
+                item.SubItems[1].Text = dlg.ParamRegex;
+            }
+        }
+        private void OnDelReplacementParam(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in replacementParameters.SelectedItems)
+                replacementParameters.Items.Remove(item);
+        }
 
 		private void OnSelectedIndexChangedTemplateParameters(object sender, EventArgs e)
 		{
 			SetButtonStates();
 		}
 
-		private void OnDoubleClickTemplateParameters(object sender, EventArgs e)
-		{
-			if (templateParameters.SelectedItems.Count == 1)
-				OnEditParam(sender, e);
-		}
+        private void OnDoubleClickTemplateParameters(object sender, EventArgs e)
+        {
+            if (templateParameters.SelectedItems.Count == 1)
+                OnEditParam(sender, e);
+        }
+        private void OnDoubleClickReplacementParameters(object sender, EventArgs e)
+        {
+            if (replacementParameters.SelectedItems.Count == 1)
+                OnEditReplacementParam(sender, e);
+        }
 
 		public string TemplateName { get; private set; }
-		public Dictionary<string, string> Parameters { get; private set; }
+        public Dictionary<string, string> Parameters { get; private set; }
+        public Dictionary<string, string> Replacements { get; private set; }
 		public string Replacement { get; private set; }
-		public bool SkipIfNone { get; private set; }
+        public bool SkipIfNone { get; private set; }
+        public bool RemoveExcessPipes { get; private set; }
 
 		private void OK_Click(object sender, EventArgs e)
 		{
-			if (templateName.Text == "")
-			{
-				MessageBox.Show(string.Format("Template name must be specified"), PluginName);
-				DialogResult = DialogResult.None;
-				templateName.Focus();
-				return;
-			}
-			TemplateName = templateName.Text;
-			Replacement = replacementText.Text;
-			Parameters.Clear();
-			List<string> names = new List<string>();
-			foreach (ListViewItem lvi in templateParameters.Items)
-			{
-				// some sanity checking on the parameter names
-				string param = lvi.SubItems[0].Text;
-				string name = new Regex("[^a-z0-9_]").Replace(param, "");
-				if (name == "" || new Regex("[0-9]").IsMatch(name.Substring(0, 1)))
-				{
-					MessageBox.Show(string.Format("{0} is not a valid parameter name", param), PluginName);
-					templateParameters.Focus();
-					templateParameters.SelectedItems.Clear();
-					lvi.Selected = true;
-					DialogResult = DialogResult.None;
-					return;
-				}
-				if (names.Contains(name))
-				{
-					MessageBox.Show(string.Format("{0}: duplicated parameter", name), PluginName);
-					templateParameters.Focus();
-					templateParameters.SelectedItems.Clear();
-					lvi.Selected = true;
-					DialogResult = DialogResult.None;
-					return;
-				}
-				names.Add(name);
-				Parameters.Add(lvi.SubItems[0].Text, lvi.SubItems[1].Text);
-			}
-			SkipIfNone = skipIfNone.Checked;
+            if (templateName.Text == "")
+            {
+                MessageBox.Show(string.Format("Template name must be specified"), PluginName);
+                DialogResult = DialogResult.None;
+                templateName.Focus();
+                return;
+            }
+            if (templateParameters.Items.Count == 0)
+            {
+                MessageBox.Show(string.Format("At least one template parameter must be specified"), PluginName);
+                DialogResult = DialogResult.None;
+                return;
+            }
+            TemplateName = templateName.Text;
+            SaveParameters(templateParameters, Parameters);
+            SaveParameters(replacementParameters, Replacements);
+            SkipIfNone = skipIfNone.Checked;
+            RemoveExcessPipes = removeExcessPipes.Checked;
 		}
+
+        private bool SaveParameters(ListView parameterListView, Dictionary<string, string> ParameterList)
+        {
+            ParameterList.Clear();
+            List<string> names = new List<string>();
+            foreach (ListViewItem lvi in parameterListView.Items)
+            {
+                // some sanity checking on the parameter names
+                string param = lvi.SubItems[0].Text;
+                string name = new Regex("[^a-z0-9_]").Replace(param, "");
+                if (name == "" || new Regex("[0-9]").IsMatch(name.Substring(0, 1)))
+                {
+                    MessageBox.Show(string.Format("{0} is not a valid parameter name", param), TemplateName);
+                    parameterListView.Focus();
+                    parameterListView.SelectedItems.Clear();
+                    lvi.Selected = true;
+                    DialogResult = DialogResult.None;
+                    return false;
+                }
+                if (names.Contains(name))
+                {
+                    MessageBox.Show(string.Format("{0}: duplicated parameter", name), TemplateName);
+                    parameterListView.Focus();
+                    parameterListView.SelectedItems.Clear();
+                    lvi.Selected = true;
+                    DialogResult = DialogResult.None;
+                    return false;
+                }
+                names.Add(name);
+                ParameterList.Add(lvi.SubItems[0].Text, lvi.SubItems[1].Text);
+            }
+            return true;
+        }
+
 		static internal string PluginName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
 
 		private void regexHelp_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -126,5 +176,25 @@ namespace AutoWikiBrowser.Plugins.TheTemplator
 			regexHelp.LinkVisited = true;
 			WikiFunctions.Tools.OpenURLInBrowser("http://msdn.microsoft.com/en-us/library/hs600312.aspx");
 		}
-	}
+
+        bool InTemplateParameters_ColumnWidthChanging = false;
+        private void templateParameters_ColumnWidthChanging(object sender, ColumnWidthChangingEventArgs e)
+        {
+            if (InTemplateParameters_ColumnWidthChanging)
+                return;
+            InTemplateParameters_ColumnWidthChanging = true;
+            replacementParameters.Columns[e.ColumnIndex].Width = e.NewWidth;
+            InTemplateParameters_ColumnWidthChanging = false;
+        }
+
+        bool InReplacementParameters_ColumnWidthChanging = false;
+        private void replacementParameters_ColumnWidthChanging(object sender, ColumnWidthChangingEventArgs e)
+        {
+            if (InReplacementParameters_ColumnWidthChanging)
+                return;
+            InReplacementParameters_ColumnWidthChanging = true;
+            templateParameters.Columns[e.ColumnIndex].Width = e.NewWidth;
+            InReplacementParameters_ColumnWidthChanging = false;
+        }
+    }
 }
