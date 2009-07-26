33c9915646bd989c3076f3ce86b4372fae72c779
Sam Reed <sam@reedyboy.net>
2009-07-26 18:42:12 +0000

    Port 5023 to branch
    Tweak to ConvertFromTalk, use in branch aswell (using new code)
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@5024 5227b50d-9349-4574-b055-4b0a7525e11c

--- branches/WebBrowser/WikiFunctions/Tools.cs
+++ branches/WebBrowser/WikiFunctions/Tools.cs
@@ -1727,7 +1727,7 @@ Message: {2}
         /// <returns>Article Title</returns>
         public static string ConvertToTalk(Article a)
         {
-			if (a.NameSpaceKey < 0)
+            if (Namespace.IsSpecial(a.NameSpaceKey))
                 return a.Name;
 		
             if (Namespace.IsTalk(a.NameSpaceKey))
@@ -1768,7 +1768,7 @@ Message: {2}
         /// <returns>Article Title</returns>
         public static string ConvertFromTalk(Article a)
         {
-			if (a.NameSpaceKey < 0)
+            if (Namespace.IsSpecial(a.NameSpaceKey))
                 return a.Name;
 		
             if (Namespace.IsTalk(a.NameSpaceKey))
