e72748bd65c356cf955613ddd5f1824e697731c8
Sam Reed <sam@reedyboy.net>
2009-07-29 18:55:54 +0000

    Start of updates to TypoScan database.. Probably not finished, but in the right direction
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@5044 5227b50d-9349-4574-b055-4b0a7525e11c

--- TypoScan/database.sql
+++ TypoScan/database.sql
@@ -7,6 +7,7 @@ CREATE TABLE  `articles` (
   `skipid` int(10) NOT NULL default '0',
   `userid` int(10) unsigned NOT NULL default '0',
   `checkedin` datetime NOT NULL default '0000-00-00 00:00:00',
+  `siteid` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`articleid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -25,3 +26,12 @@ CREATE TABLE `users` (
   `username` varchar(50) default NULL,
   PRIMARY KEY (`userid`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `site`;
+CREATE TABLE `site` (
+  `siteid` int(10) unsigned NOT NULL auto_increment,
+  `address` varchar(50) unsigned NOT NULL,
+   PRIMARY KEY (`siteid`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `site`(`address`) VALUES (`http://en.wikipedia.org`);
