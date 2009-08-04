034f16f1a482313b1252af9ab853c63dbdd1040a
Sam Reed <sam@reedyboy.net>
2009-08-04 19:45:51 +0000

    Do some moving about of List Providers
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@5120 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Lists/Providers/ListProviders.cs
+++ AWB/WikiFunctions/Lists/Providers/ListProviders.cs
@@ -18,12 +18,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 using System;
-using System.Text;
 using System.Web;
-using System.IO;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
-using System.Windows.Forms;
 using System.Xml;
 using WikiFunctions.Controls.Lists;
 
@@ -141,106 +138,6 @@ namespace WikiFunctions.Lists
         {
             get { return "Category (recurse user defined level)"; }
         }
-    }
-
-    /// <summary>
-    /// Gets a list of pages from a text file
-    /// </summary>
-    public class TextFileListProvider : IListProvider
-    {
-        private readonly static Regex RegexFromFile = new Regex("(^[a-z]{2,3}:)|(simple:)", RegexOptions.Compiled);
-        private readonly static Regex LoadWikiLink = new Regex(@"\[\[:?(.*?)(?:\]\]|\|)", RegexOptions.Compiled);
-        private readonly OpenFileDialog OpenListDialog = new OpenFileDialog();
-
-        public TextFileListProvider()
-        {
-            OpenListDialog.Filter = "Text files|*.txt|Text files (no validation)|*.txt|All files|*.*";
-            OpenListDialog.Multiselect = true;
-        }
-
-        public List<Article> MakeList(string searchCriteria)
-        {
-            return MakeList(searchCriteria.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries));
-        }
-
-        public List<Article> MakeList()
-        {
-            return MakeList(new string[0]);
-        }
-
-        public List<Article> MakeList(params string[] searchCriteria)
-        {
-            List<Article> list = new List<Article>();
-            try
-            {
-                if (searchCriteria.Length == 0 && OpenListDialog.ShowDialog() == DialogResult.OK)
-                    searchCriteria = OpenListDialog.FileNames;
-
-                foreach (string fileName in searchCriteria)
-                {
-                    string pageText, title;
-
-                    using (StreamReader sr = new StreamReader(fileName, Encoding.UTF8))
-                    {
-                        pageText = sr.ReadToEnd();
-                        sr.Close();
-                    }
-
-                    switch (OpenListDialog.FilterIndex)
-                    {
-                        case 2:
-                            foreach (string s in pageText.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries))
-                            {
-                                list.Add(new Article(Tools.RemoveSyntax(Tools.TurnFirstToUpper(s.Trim()))));
-                            }
-                            break;
-                        default:
-                            if (LoadWikiLink.IsMatch(pageText))
-                            {
-                                foreach (Match m in LoadWikiLink.Matches(pageText))
-                                {
-                                    title = m.Groups[1].Value;
-                                    if (!RegexFromFile.IsMatch(title) && (!(title.StartsWith("#"))))
-                                    {
-                                        list.Add(new Article(Tools.RemoveSyntax(Tools.TurnFirstToUpper(title))));
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                foreach (string s in pageText.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries))
-                                {
-                                    if (s.Trim().Length == 0 || !Tools.IsValidTitle(s)) continue;
-                                    list.Add(new Article(Tools.RemoveSyntax(Tools.TurnFirstToUpper(s.Trim()))));
-                                }
-                            }
-                            break;
-                    }
-                }
-                return list;
-            }
-            catch (Exception ex)
-            {
-                ErrorHandler.Handle(ex);
-                return list;
-            }
-        }
-
-        #region ListMaker properties
-        public string DisplayText
-        { get { return "Text file"; } }
-
-        public string UserInputTextBoxText
-        { get { return ""; } }
-
-        public bool UserInputTextBoxEnabled
-        { get { return false; } }
-
-        public void Selected() { }
-
-        public bool RunOnSeparateThread
-        { get { return false; } }
-        #endregion
     }
 
     /// <summary>
@@ -811,67 +708,6 @@ namespace WikiFunctions.Lists
         { get { return true; } }
 
         public override void Selected() { }
-        #endregion
-    }
-
-    /// <summary>
-    /// Gets a list of google results based on the named pages
-    /// </summary>
-    public class GoogleSearchListProvider : IListProvider
-    {
-        private static readonly Regex RegexGoogle = new Regex("href\\s*=\\s*(?:\"(?<1>[^\"]*)\"|(?<1>\\S+) class=l)",
-                                                              RegexOptions.IgnoreCase | RegexOptions.Compiled);
-
-        public List<Article> MakeList(params string[] searchCriteria)
-        {
-            List<Article> list = new List<Article>();
-
-            foreach (string g in searchCriteria)
-            {
-                int intStart = 0;
-                string google = HttpUtility.UrlEncode(g);
-                string url = "http://www.google.com/search?q=" + google + "+site:" + Variables.URL +
-                             "&num=100&hl=en&lr=&start=0&sa=N&filter=0";
-
-                do
-                {
-                    string googleText = Tools.GetHTML(url, Encoding.Default);
-
-                    //Find each match to the pattern
-                    foreach (Match m in RegexGoogle.Matches(googleText))
-                    {
-                        string title = Tools.GetTitleFromURL(m.Groups[1].Value);
-
-                        if (!string.IsNullOrEmpty(title))
-                            list.Add(new Article(title));
-                    }
-
-                    if (!googleText.Contains("img src=\"nav_next.gif\""))
-                        break;
-
-                    intStart += 100;
-                    url = "http://www.google.com/search?q=" + google + "+site:" + Variables.URL +
-                          "&num=100&hl=en&lr=&start=" + intStart + "&sa=N&filter=0";
-
-                } while (true);
-            }
-            return Tools.FilterSomeArticles(list);
-        }
-
-        #region ListMaker properties
-        public string DisplayText
-        { get { return "Google search"; } }
-
-        public string UserInputTextBoxText
-        { get { return "Google search:"; } }
-
-        public bool UserInputTextBoxEnabled
-        { get { return true; } }
-
-        public void Selected() { }
-
-        public bool RunOnSeparateThread
-        { get { return true; } }
         #endregion
     }
 
@@ -1670,79 +1506,5 @@ namespace WikiFunctions.Lists
 
         public bool NamespacesEnabled
         { get { return false; } }
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public class HTMLPageScraperListProvider : IListProvider
-    {
-        public List<Article> MakeList(params string[] searchCriteria)
-        {
-            List<Article> list = new List<Article>();
-
-            foreach (string url in searchCriteria)
-            {
-                foreach (
-                    string entry in
-                        Tools.StringBetween(Tools.GetHTML(url), "<body>", "</body>").Split(new[] {"\r\n", "\n"},
-                                                                                           StringSplitOptions.RemoveEmptyEntries))
-                {
-                    if (entry.Length > 0 && CheckExtra(entry))
-                    {
-                        list.Add(new Article(ModifyArticleName(entry)));
-                    }
-                }
-            }
-
-            return list;
-        }
-
-        protected virtual bool CheckExtra(string entry)
-        {
-            if (entry.StartsWith(@"<h1>"))
-                return false;
-            return true;
-        }
-
-        protected virtual string ModifyArticleName(string title)
-        {
-            title = title.Replace(@"&amp;", "&");
-            return title.Replace("<br />", "");
-        }
-
-        public virtual string DisplayText
-        { get { return "HTML Scraper"; } }
-
-        public string UserInputTextBoxText
-        { get { return "URL:"; } }
-
-        public bool UserInputTextBoxEnabled
-        { get { return true; } }
-
-        public void Selected()
-        { }
-
-        public bool RunOnSeparateThread
-        { get { return true; } }
-    }
-
-    /// <summary>
-    /// Gets a list of pages from an online CheckWiki-output type page
-    /// </summary>
-    public class CheckWikiListProvider : HTMLPageScraperListProvider
-    {
-        protected override bool CheckExtra(string entry)
-        {
-            return !entry.Contains("Error number");
-        }
-
-        protected override string ModifyArticleName(string title)
-        {
-            return title.Replace("<br />", "");
-        }
-
-        public override string DisplayText
-        { get { return "CheckWiki error"; } }
     }
 }
