fc919fccede4c30fc6f3ea08404efeb1b2d4d112
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2009-08-14 21:54:15 +0000

    some more unit tests in Tools
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@5192 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Tools.cs
+++ AWB/WikiFunctions/Tools.cs
@@ -589,7 +589,7 @@ namespace WikiFunctions
             return words;
         }
 
-        // Not Covered
+        // Covered by ToolsTests.InterwikiCount
         /// <summary>
         /// Returns number of interwiki links in the text
         /// </summary>
@@ -603,7 +603,7 @@ namespace WikiFunctions
             return count;
         }
 
-        // Not Covered
+        // Covered by ToolsTests.LinkCountTests
         /// <summary>
         /// Returns the number of [[links]] in the string
         /// </summary>
