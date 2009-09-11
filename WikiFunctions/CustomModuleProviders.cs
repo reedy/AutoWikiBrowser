6d53290a0a5f2c5a476e046ebf3728244d1ac681
Max Semenik <maxsem.wiki@gmail.com>
2009-09-11 19:28:50 +0000

    Comitted work in process - modularisation of CustomModule. Deadbeef so far, no changes to working code.
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@5352 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ WikiFunctions/CustomModuleProviders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.CodeDom.Compiler;
+
+namespace WikiFunctions
+{
+    public interface ICustomModule
+    {
+        string Name
+        { get; }
+
+        string Language
+        { get; }
+
+        CompilerResults Compile(string sourceCode);
+    }
+}
