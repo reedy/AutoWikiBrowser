f3505f84dcf7110074c158a91a162d466a4386fd
Max Semenik <maxsem.wiki@gmail.com>
2009-09-21 17:21:27 +0000

    Tweaked error handling strategy in ExternalProgram
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@5395 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/ExternalProgram.cs
+++ AWB/AWB/ExternalProgram.cs
@@ -116,7 +116,11 @@ namespace AutoWikiBrowser
             }
             catch (Exception ex)
             {
-                WikiFunctions.ErrorHandler.Handle(ex);
+                // Most, if not all exceptions here are related to user wrong user input
+                // or environment specifics, so ErrorHandler is not needed.
+                MessageBox.Show(Application.OpenForms[0], ex.Message, "External processing error", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
                 return origText;
             }
         }
