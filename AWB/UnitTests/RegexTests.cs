b7856c3ae0a739f441b505f78dde81be65766204
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2009-09-24 16:09:03 +0000

    part fix bug http://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Bugs#Inserts_Reflist_when_article_contains_.3Creferences.3E_tags –do not insert {[reflist}}  in this situation
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@5444 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/UnitTests/RegexTests.cs
+++ AWB/UnitTests/RegexTests.cs
@@ -462,6 +462,8 @@ fast„ "));
             Assert.IsTrue(WikiRegexes.ReferencesTemplate.IsMatch(@"Hello<ref>Fred</ref> {{References}}"));
             Assert.IsTrue(WikiRegexes.ReferencesTemplate.IsMatch(@"Hello<ref>Fred</ref> {{references}}"));
             Assert.IsTrue(WikiRegexes.ReferencesTemplate.IsMatch(@"Hello<ref>Fred</ref> <references/>"));
+            Assert.IsTrue(WikiRegexes.ReferencesTemplate.IsMatch(@"<references>
+<ref name =Fred>Fred</ref> </references>"));
 
             Assert.IsFalse(WikiRegexes.ReferencesTemplate.IsMatch(@"Hello<ref>Fred</ref>"));
             Assert.IsFalse(WikiRegexes.ReferencesTemplate.IsMatch(@"Hello<ref name=""F"">Fred</ref>"));
