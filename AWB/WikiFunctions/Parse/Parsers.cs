b738d55cd480d2da2deab6338eea78f9fadc795a
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2009-11-07 17:12:46 +0000

    Parsers.Mdashes to avoid dimensions in format 55-66-77
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@5616 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Parse/Parsers.cs
+++ AWB/WikiFunctions/Parse/Parsers.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
 
 Copyright (C) 2007 Martin Richards
 
@@ -1283,7 +1283,9 @@ namespace WikiFunctions.Parse
         }
 
         private static readonly Regex PageRangeIncorrectMdash = new Regex(@"(pages\s*=\s*|pp\.?\s*)(\d+\s*)(?:-|—|&mdash;|&#8212;)(\s*\d+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-        private static readonly Regex UnitTimeRangeIncorrectMdash = new Regex(@"([1-9]?\d+\s*)(?:-|—|&mdash;|&#8212;)(\s*[1-9]?\d+)(\s+|&nbsp;)((?:years|months|weeks|days|hours|minutes|seconds|[km]g|kb|[ckm]?m|[Gk]?Hz|miles|mi\.|%|feet|foot|ft)\b|in\))", RegexOptions.Compiled);
+        
+        // avoid dimensions in format 55-66-77
+        private static readonly Regex UnitTimeRangeIncorrectMdash = new Regex(@"(?<!-)(\b[1-9]?\d+\s*)(?:-|—|&mdash;|&#8212;)(\s*[1-9]?\d+)(\s+|&nbsp;)((?:years|months|weeks|days|hours|minutes|seconds|[km]g|kb|[ckm]?m|[Gk]?Hz|miles|mi\.|%|feet|foot|ft)\b|in\))", RegexOptions.Compiled);
         private static readonly Regex DollarAmountIncorrectMdash = new Regex(@"(\$[1-9]?\d{1,3}\s*)(?:-|—|&mdash;|&#8212;)(\s*\$?[1-9]?\d{1,3})", RegexOptions.Compiled);
         private static readonly Regex AMPMIncorrectMdash = new Regex(@"([01]?\d:[0-5]\d\s*([AP]M)\s*)(?:-|—|&mdash;|&#8212;)(\s*[01]?\d:[0-5]\d\s*([AP]M))", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         private static readonly Regex AgeIncorrectMdash = new Regex(@"([Aa]ge[sd])\s([1-9]?\d\s*)(?:-|—|&mdash;|&#8212;)(\s*[1-9]?\d)", RegexOptions.Compiled);
