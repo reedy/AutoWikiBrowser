6cf0fb02b3181cfb38d6fd6a6707a966aa24b584
Sam Reed <sam@reedyboy.net>
2009-11-25 01:16:10 +0000

    Move RegexReplacement to own file
    Code tweaks etc to Parse/Parsers.cs
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@5673 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ AWB/WikiFunctions/Parse/RegexReplacement.cs
@@ -0,0 +1,46 @@
+﻿/*
+
+Copyright (C) 
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+using System.Text.RegularExpressions;
+
+namespace WikiFunctions.Parse
+{
+    struct RegexReplacement
+    {
+        public RegexReplacement(Regex regex, string replacement)
+        {
+            Regex = regex;
+            Replacement = replacement;
+        }
+
+        public RegexReplacement(string pattern, RegexOptions options, string replacement)
+            : this(new Regex(pattern, options), replacement)
+        {
+        }
+
+        public RegexReplacement(string pattern, string replacement)
+            : this(pattern, RegexOptions.Compiled, replacement)
+        {
+        }
+
+        public readonly Regex Regex;
+        public readonly string Replacement;
+        // This could get extended to be a class with some of the Regex methods, such as IsMatch, Replace, etc, but there's little point
+    }
+}
