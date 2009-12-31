0216dfc40cdf93adc8770978d093e72f1c5517dc
Sam Reed <sam@reedyboy.net>
2009-12-31 14:23:07 +0000

    Rem php?> (lolwut)
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@5859 5227b50d-9349-4574-b055-4b0a7525e11c

--- UsageStats/includes/Stats.php
+++ UsageStats/includes/Stats.php
@@ -37,7 +37,7 @@ Some queries we might want on a stats page:
 function htmlstats(){
 	global $db;
 	
-	php?>
+	?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
 <head>
