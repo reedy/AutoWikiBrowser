0a81077cfe4128d9229bd46dffd3bf4e58638067
Sam Reed <sam@reedyboy.net>
2010-01-11 20:59:49 +0000

    Bump 5.0.0.0
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@5996 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Properties/AssemblyInfo.cs
+++ AWB/AWB/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.9.0.3")]
-[assembly: AssemblyFileVersion("4.9.0.3")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
