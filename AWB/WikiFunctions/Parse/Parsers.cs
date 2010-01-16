fae10cb2453a65523849509e5450d0f6068084fe
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2010-01-16 13:58:04 +0000

    new Regex CiteUrlDomain for future use
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@6052 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Parse/Parsers.cs
+++ AWB/WikiFunctions/Parse/Parsers.cs
@@ -2038,7 +2038,8 @@ namespace WikiFunctions.Parse
         private static readonly Regex UppercaseCiteFields = new Regex(@"(\{\{(?:[Cc]ite\s*(?:web|book|news|journal|paper|press release|hansard|encyclopedia)|[Cc]itation)\b\s*[^{}]*\|\s*)(\w*?[A-Z]+\w*)(?<!(?:IS[BS]N|DOI|PMID))(\s*=\s*[^{}\|]{3,})", RegexOptions.Compiled);
 
         private static readonly Regex CiteUrl = new Regex(@"\|\s*url\s*=\s*([^\[\]<>""\s]+)", RegexOptions.Compiled);
-
+        private static readonly Regex CiteUrlDomain = new Regex(@"\|\s*url\s*=\s*(?:ht|f)tps?://(?:www\.)?([^\[\]<>""\s/:]+)", RegexOptions.Compiled);
+        
         private static readonly Regex CiteFormatFieldTypo = new Regex(@"(\{\{\s*[Cc]it[^{}]*?\|\s*)(?i)(?:fprmat)(\s*=\s*)", RegexOptions.Compiled);
         private static readonly Regex WorkInItalics = new Regex(@"(\|\s*work\s*=\s*)''([^'{}\|]+)''(?=\s*(?:\||}}))", RegexOptions.Compiled);
 
