aaf005d63c755a22e6ee460af8e5287ec9d9e606
Sam Reed <sam@reedyboy.net>
2010-01-27 11:45:16 +0000

    Reverting r6113
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@6133 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Lists/Providers/ListProviders.cs
+++ AWB/WikiFunctions/Lists/Providers/ListProviders.cs
@@ -382,7 +382,7 @@ namespace WikiFunctions.Lists.Providers
 
         public WhatLinksHereAndPageRedirectsExcludingTheRedirectsListProvider()
         {
-            //Blfilterredir = "nonredirects"; //Decomment this line when http://www.mediawiki.org/wiki/Special:Code/MediaWiki/61447 is live on WMF
+            Blfilterredir = "nonredirects";
             IncludeWhatLinksToRedirects = true;
         }
 
