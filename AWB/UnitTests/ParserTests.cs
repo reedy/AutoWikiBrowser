c9580667c9fd1eec80442a80c4c6bddafcdf42b9
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2010-02-01 20:27:09 +0000

    update FixPeopleCategories logic to also parse DateOfBirth and DateOfDeath fields in infoboxes for Magioladitis
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@6145 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/UnitTests/ParserTests.cs
+++ AWB/UnitTests/ParserTests.cs
@@ -1337,6 +1337,22 @@ died 2002
 |alma_mater      =
 |occupation      =brickmason, mercha
 
+}} {{persondata}}", infob1b = @"{{Infobox Officeholder
+|honorific-prefix   =
+|name            = John C. Zimmerman, Sr.
+|term_start       = 1895
+|term_end         = 1896
+|predecessor      = [[Arthur C. McCall]]
+|successor        = [[Samuel C. Randall]]
+|DateOfBirth     = May 12, 1835
+|birth_place     = [[Free City of Frankfurt]]
+|DateOfDeath= October 26, 1935
+|death_place=
+|restingplace = Glenwood Cemetery, Flint
+|restingplacecoordinates =
+|alma_mater      =
+|occupation      =brickmason, mercha
+
 }} {{persondata}}";
 
             // scraped from infobox
@@ -1347,6 +1363,10 @@ died 2002
             Assert.AreEqual(infob1a + @"
 [[Category:1835 births]]
 [[Category:1935 deaths]]", Parsers.FixPeopleCategories(infob1a, "foo"));
+            
+            Assert.AreEqual(infob1b + @"
+[[Category:1835 births]]
+[[Category:1935 deaths]]", Parsers.FixPeopleCategories(infob1b, "foo"));
 
             Assert.AreEqual(infob2 + @"
 [[Category:193 BC births]]
