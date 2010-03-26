496bd29ff1fd0baec8ea60bf056452ace740df6d
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2010-03-26 19:43:23 +0000

    bugfix bracket handling of WikiRegex BareRefExternalLink
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@6341 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/RegexTests.cs
+++ UnitTests/RegexTests.cs
@@ -1520,6 +1520,7 @@ words2"));
             TestMatch(WikiRegexes.BareRefExternalLink, @"<ref>[http://news.bbc.co.uk/hi/England/story4384.htm title here]</ref>", false);
         
             Assert.AreEqual(WikiRegexes.BareRefExternalLink.Match(@"<ref>[ http://news.bbc.co.uk/hi/England/story4384.htm ] </ref>").Groups[1].Value, @"http://news.bbc.co.uk/hi/England/story4384.htm");
+            Assert.AreEqual(WikiRegexes.BareRefExternalLink.Match(@"<ref>[ http://news.bbc.co.uk/hi/England/story4384.htm] </ref>").Groups[1].Value, @"http://news.bbc.co.uk/hi/England/story4384.htm");
         }
 
         [Test]
