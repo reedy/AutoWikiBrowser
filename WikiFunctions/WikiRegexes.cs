1e6673322c52ba784c37460aaaca0f9fb2576282
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2010-04-03 10:07:37 +0000

    fix for http://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Bugs#Not_detecting_.3Creferences_.3E_properly
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@6359 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/WikiRegexes.cs
+++ WikiFunctions/WikiRegexes.cs
@@ -535,7 +535,7 @@ namespace WikiFunctions
         /// </summary>
         public static readonly Regex BLPSources = new Regex(@"{{\s*([Bb](LP|lp) ?(sources|[Uu]n(sourced|ref(?:erenced)?))|[Uu]n(sourced|referenced) ?[Bb](LP|lp))\b", RegexOptions.Compiled);
 
-        public const string ReferencesTemplates = @"(\{\{\s*(?:ref(?:-?li(?:st|nk)|erence)|[Ll]istaref)(?>[^\{\}]+|\{(?<DEPTH>)|\}(?<-DEPTH>))*(?(DEPTH)(?!))\}\}|<references\s*/>|\{\{refs|<references>.*</references>)";
+        public const string ReferencesTemplates = @"(\{\{\s*(?:ref(?:-?li(?:st|nk)|erence)|[Ll]istaref)(?>[^\{\}]+|\{(?<DEPTH>)|\}(?<-DEPTH>))*(?(DEPTH)(?!))\}\}|<\s*references\s*/>|\{\{refs|<\s*references\s*>.*</\s*references\s*>)";
         public const string ReferenceEndGR = @"(?:</ref>|{{GR\|\d}})";
 
         /// <summary>
