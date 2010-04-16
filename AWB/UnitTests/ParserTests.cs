0d358fd1e9e13255f3299a7add9e89aec4bbf08a
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2010-04-16 15:30:51 +0000

    for FR http://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Feature_requests#Add_non-human-name_default_sort_for_musical_groups article is not about a person if contains "Infobox musical group" template (or redirect) with "Background" field value of "group_or_band", even if talk page has a WPBiography tag.
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@6428 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/UnitTests/ParserTests.cs
+++ AWB/UnitTests/ParserTests.cs
@@ -4854,6 +4854,10 @@ foo {{persondata}}
 			Assert.IsFalse(Parsers.IsArticleAboutAPerson(@"Foo [[fictional character]] {{persondata|name=smith}}", "foo"));
 			Assert.IsFalse(Parsers.IsArticleAboutAPerson(@"Foo [[fictional character|character]] {{persondata|name=smith}}", "foo"));
 			
+			Assert.IsFalse(Parsers.IsArticleAboutAPerson(@"Foo [[Category:2002 births]] {{infobox Musical artist|Background=group_or_band}}", "foo"));
+			Assert.IsFalse(Parsers.IsArticleAboutAPerson(@"Foo [[Category:2002 births]] {{infobox Band|Background=group_or_band}}", "foo"));
+			Assert.IsTrue(Parsers.IsArticleAboutAPerson(@"Foo [[Category:2002 births]] {{infobox Band|Background=other}}", "foo"));
+			
 
 			// multiple birth dates means not about one person
 			Assert.IsFalse(Parsers.IsArticleAboutAPerson(@"{{nat fs player|no=1|pos=GK|name=[[Meg]]|age={{Birth date|1956|01|01}} ({{Age at date|1956|01|01|1995|6|5}})|caps=|club=|clubnat=}}
