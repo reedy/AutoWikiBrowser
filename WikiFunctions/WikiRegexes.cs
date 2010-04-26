e8d9d08d829d704f0078fc4bb13392cadb1f3500
Marios Magioladitis <magioladitis@users.sourceforge.net>
2010-04-26 19:50:21 +0000

    Added Unreferenced stub a a variant of Unreferenced
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@6462 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/WikiRegexes.cs
+++ WikiFunctions/WikiRegexes.cs
@@ -753,7 +753,7 @@ namespace WikiFunctions
         /// <summary>
         /// Matches the {{Unreferenced}} template
         /// </summary>
-        public static readonly Regex Unreferenced = new Regex(@"{{\s*([Uu]nreferenced|[Uu]nsourced|[Uu]nverified|[Uu]nref|[Rr]eferences|[Uu]ncited-article|[Cc]itesources|[Nn][Rr]|[Nn]o references|[Uu]nrefarticle|[Nn]o ?refs?|[Nn]oreferences|[Cc]leanup-cite|[Rr]eferences needed)\s*(?:\|.*?)?}}", RegexOptions.Singleline);
+        public static readonly Regex Unreferenced = new Regex(@"{{\s*([Uu]nreferenced( stub)?|[Uu]nsourced|[Uu]nverified|[Uu]nref|[Rr]eferences|[Uu]ncited-article|[Cc]itesources|[Nn][Rr]|[Nn]o references|[Uu]nrefarticle|[Nn]o ?refs?|[Nn]oreferences|[Cc]leanup-cite|[Rr]eferences needed)\s*(?:\|.*?)?}}", RegexOptions.Singleline);
         /// <summary>
         /// Matches {{XX Portal}} templates
         /// </summary>
