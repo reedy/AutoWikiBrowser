527013d90f8a45bfd123cdae818a7f463be02270
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2010-06-03 11:12:51 +0000

    reinstate TurnFirstToUpperWiktionary test, new unit test & debug only Variables function
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@6625 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Variables.cs
+++ AWB/WikiFunctions/Variables.cs
@@ -394,9 +394,9 @@ namespace WikiFunctions
                 "traditio.",
                 "volgota.com",
                 "wikireality.ru"
-            };
+        };
 
-#if DEBUG
+        #if DEBUG
         /// <summary>
         /// Sets the language code of the current project
         /// </summary>
@@ -406,7 +406,19 @@ namespace WikiFunctions
         {
             LangCode = langCode;
         }
-#endif
+        
+        /// <summary>
+        /// Sets different language variables, such as namespaces. Default is english Wikipedia
+        /// </summary>
+        /// <param name="langCode">The language code, default is en</param>
+        /// <param name="projectName">The project name default is Wikipedia</param>
+        /// <remarks>Do not use this outside unit tests</remarks>
+        public static void SetProjectSimple(string langCode, ProjectEnum projectName)
+        {
+            Project = projectName;
+            LangCode = langCode;
+        }
+        #endif
 
         /// <summary>
         /// Sets different language variables, such as namespaces. Default is english Wikipedia
