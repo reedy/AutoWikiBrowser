3e0d098fbceb0af9265b9fa5a950e8a9626c021c
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2010-07-05 11:24:38 +0000

    fix failing test: edit summary length affected by revision number being in edit summary for debug builds
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@6792 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/SummaryTests.cs
+++ UnitTests/SummaryTests.cs
@@ -53,9 +53,12 @@ namespace UnitTests
             Assert.AreEqual(waffle + bug1, Summary.Trim(waffle + bug1));
             Assert.AreEqual(waffle + waffle + @"replaced category 'Actual event ballads' → 'Songs based on actual events' per...", Summary.Trim(waffle + waffle + bug1));
 
-            Assert.AreEqual("clean upclean upclean up, typos fixed: dissapointment → disappointment (2), attatched → attached (2), begining → beginning, Expiditionary → Expeditionary, manuever → maneuver (5), thier → their (7), independant ",
+            #if DEBUG
+            // debug only - length affected by revision number being in edit summary for debug builds
+            Assert.AreEqual("clean upclean upclean up, typos fixed: dissapointment → disappointment (2), attatched → attached (2), begining → beginning, Expiditionary → Expeditionary, manuever → maneuver (5), thier → their (7), indep",
                             Summary.Trim(
                                 "clean upclean upclean up, typos fixed: dissapointment → disappointment (2), attatched → attached (2), begining → beginning, Expiditionary → Expeditionary, manuever → maneuver (5), thier → their (7), independant → independent"));
+            #endif
         }
 
         [Test]
