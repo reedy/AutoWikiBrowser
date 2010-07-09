6089b53ea3d4b3865b945526b623662b300be2a1
Sam Reed <sam@reedyboy.net>
2010-07-09 14:00:20 +0000

    Simplify
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@6818 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/Kingbotk/AWB Plugin/AssessmentForm.vb
+++ Plugins/Kingbotk/AWB Plugin/AssessmentForm.vb
@@ -54,13 +54,11 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk.ManualAssessments
         ' Button event handlers:
         Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) _
         Handles OK_Button.Click
-            Me.DialogResult = System.Windows.Forms.DialogResult.OK
             Me.Close()
         End Sub
 
         Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) _
         Handles Cancel_Button.Click
-            Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
             Me.Close()
         End Sub
     End Class
