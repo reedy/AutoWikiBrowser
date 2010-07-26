9ad80b9266a273e9eeba4f7b62ac99cde668a81d
Sam Reed <sam@reedyboy.net>
2010-07-26 15:03:56 +0000

    Press button on enter
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@6884 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/TalkMessage.Designer.cs
+++ AWB/TalkMessage.Designer.cs
@@ -55,6 +55,7 @@
             // 
             // TalkMessage
             // 
+            this.AcceptButton = this.btnDefault;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(170, 69);
