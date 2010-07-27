a1d62e54d83999d96a85a8f88b74b29757b495cf
Sam Reed <sam@reedyboy.net>
2010-07-27 19:29:10 +0000

    Drop old crap as per Magioladitis
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@6887 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/Kingbotk/AWB Plugin/BannerShells.vb
+++ Plugins/Kingbotk/AWB Plugin/BannerShells.vb
@@ -15,7 +15,6 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk
             NotChecked
             NoneFound
             FoundWikiProjectBannerShell
-            FoundWikiProjectBanners ' if both are present, well, tough titty! :)
         End Enum
 
         'State:
@@ -27,20 +26,13 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk
         Private Shared ReadOnly WikiProjectBannerShellRegex As New Regex(conRegexpLeft & WikiProjectBannerShell & _
            ")\b\s*(?<start>\|[^1]*=.*?)*\s*\|\s*1\s*=\s*(?<body>.*}}[^{]*?)\s*(?<end>\|[^{]*)?\s*}}", _
            RegexOptions.Compiled Or RegexOptions.IgnoreCase Or RegexOptions.Singleline Or RegexOptions.ExplicitCapture)
-        ' last known reasonably good version (no catching blp= etc at the start of regex): '")\b\s*\|\s*1\s*=\s*(?<body>.*}}[^{]*?)(?<end>\|[^{]*)?}}"
-        Private Shared ReadOnly WikiProjectBannersRegex As New Regex(conRegexpLeft & WikiProjectBanners & _
-           ")\b(\s*\|\s*[0-9]+\s*=\s*(?<body>(\{\{\s*[^{]*}}[^{]*?)|\s*))*\s*}}", RegexOptions.Compiled _
-           Or RegexOptions.IgnoreCase Or RegexOptions.Singleline Or RegexOptions.ExplicitCapture)
-        ' last known reasonably good version: no catching of empty numbered params: ")\b(\s*\|\s*[0-9]+\s*=\s*(?<body>\{\{\s*[^{]*}}[^{]*?))*}}"
-        Private Shared ReadOnly ShellRegex As New Regex(conRegexpLeft & WikiProjectBannerShell & "|" & _
-           WikiProjectBanners & ")\b\s*\|", RegexOptions.Singleline Or RegexOptions.Compiled Or RegexOptions.IgnoreCase _
-           Or RegexOptions.ExplicitCapture)
+
+
         Friend Shared ReadOnly LineBreakRegex As New Regex("[\n\r]*")
         Private Shared ReadOnly DoubleLineBreakRegex As New Regex("[\n\r]{2,}")
 
         ' Regex constant strings:
-        Private Const WikiProjectBannerShell As String = "WikiProject ?Banner ?Shell|WP?BS|WPBannerShell" ' IGNORE CASE
-        Private Const WikiProjectBanners As String = "WikiProject ?Banners|WPB" ' IGNORE CASE
+        Private Const WikiProjectBannerShell As String = "WikiProject ?Banner ?Shell|WP?BS|WPBannerShell|WikiProject ?Banners|WPB" ' IGNORE CASE
 
         ' Match evaluators:
         Private Function WPBSRegexMatchEvaluator(ByVal match As Match) As String
@@ -54,23 +46,7 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk
             Return DoubleLineBreakRegex.Replace("{{" & templatename & "|1=" & Microsoft.VisualBasic.vbCrLf & LineBreakRegex.Replace(MatchEvaluatorString, "") & _
                Microsoft.VisualBasic.vbCrLf & match.Groups("body").Value & Ending & "}}", Microsoft.VisualBasic.vbCrLf)
         End Function
-        Private Function WikiProjectBannersRegexMatchEvaluator(ByVal match As Match) As String
-            Const templatename As String = "WikiProjectBanners"
-            Dim i As Integer = 1
 
-            ShellTemplateMatchEvaluatorsCommonTasks(templatename, match)
-
-            WikiProjectBannersRegexMatchEvaluator = "{{" & templatename
-
-            For Each c As Capture In match.Groups("body").Captures
-                If Not c.Value.Trim = "" Then
-                    WikiProjectBannersRegexMatchEvaluator += Microsoft.VisualBasic.vbCrLf + "|" + i.ToString + "=" + c.Value
-                    i += 1
-                End If
-            Next
-            WikiProjectBannersRegexMatchEvaluator += Microsoft.VisualBasic.vbCrLf + "|" + i.ToString + "=" + _
-               LineBreakRegex.Replace(MatchEvaluatorString, "") + Microsoft.VisualBasic.vbCrLf + "}}"
-        End Function
         Private Sub ShellTemplateMatchEvaluatorsCommonTasks(ByVal templatename As String, ByVal match As Match)
             ' Does the shell contain template: ?
             PluginCheckTemplateCall(match.Groups("tl").Value, templatename)
@@ -85,8 +61,6 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk
             If WeFoundBannerShells = BannerShellsEnum.NotChecked Then
                 If WikiProjectBannerShellRegex.IsMatch(AlteredArticleText) Then
                     WeFoundBannerShells = BannerShellsEnum.FoundWikiProjectBannerShell
-                ElseIf WikiProjectBannersRegex.IsMatch(AlteredArticleText) Then
-                    WeFoundBannerShells = BannerShellsEnum.FoundWikiProjectBanners
                 Else
                     WeFoundBannerShells = BannerShellsEnum.NoneFound
                 End If
@@ -95,11 +69,6 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk
             Text += Template.ParametersToString(ParameterBreak)
 
             Select Case WeFoundBannerShells
-                Case BannerShellsEnum.FoundWikiProjectBanners
-                    MatchEvaluatorString = Text
-                    AlteredArticleText = WikiProjectBannersRegex.Replace(AlteredArticleText, _
-                       AddressOf Me.WikiProjectBannersRegexMatchEvaluator, 1)
-                    MatchEvaluatorString = Nothing
                 Case BannerShellsEnum.FoundWikiProjectBannerShell
                     MatchEvaluatorString = Text
 
@@ -116,7 +85,7 @@ Namespace AutoWikiBrowser.Plugins.Kingbotk
             ' Currently only WPBio can possibly call this, so it's ok to just run the regex and not cache the results
             ' Later on we want to have dynamic redirects and management of these templates (or it maybe should be in
             ' WikiFunctions.TalkPages
-            Return ShellRegex.IsMatch(AlteredArticleText)
+            Return WikiProjectBannerShellRegex.IsMatch(AlteredArticleText)
         End Function
     End Class
 End Namespace
