d5b74cc03f8ba0439d4798152e9f0b4182dba1c1
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2010-08-07 10:19:55 +0000

    restrict otheruses4 rename to Wikipedia only
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@6969 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Parse/Parsers.cs
+++ AWB/WikiFunctions/Parse/Parsers.cs
@@ -467,8 +467,9 @@ namespace WikiFunctions.Parse
 
             // conversions
 
-            // otheruses4 rename
-            articleText = Tools.RenameTemplate(articleText, "otheruses4", "about");
+            // otheruses4 rename - Wikipedia only
+            if(Variables.IsWikipediaEN)
+                articleText = Tools.RenameTemplate(articleText, "otheruses4", "about");
 
             // "{{about|about x..." --> "{{about|x..."
             foreach (Match m in Tools.NestedTemplateRegex("about").Matches(articleText))
