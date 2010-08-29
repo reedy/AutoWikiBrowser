20f46e26c6d6c7613b43669eda56b0b6207284ac
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2010-08-29 20:42:14 +0000

    increase max text length for UntemplatedQuotes
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@7041 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/WikiRegexes.cs
+++ WikiFunctions/WikiRegexes.cs
@@ -557,7 +557,7 @@ namespace WikiFunctions
         /// </summary>
         /// see http://en.wikipedia.org/wiki/Quotation_mark_glyphs
         /// http://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Feature_requests#Ignoring_spelling_errors_within_quotation_marks.3F
-        public static readonly Regex UntemplatedQuotes = new Regex(@"(?<=[^\w])[""«»‘’“”‛‟‹›“”„‘’`’“‘”].{1,500}?[""«»‘’“”‛‟‹›“”„‘’`’“‘”](?=[^\w])", RegexOptions.Compiled | RegexOptions.Singleline);
+        public static readonly Regex UntemplatedQuotes = new Regex(@"(?<=[^\w])[""«»‘’“”‛‟‹›“”„‘’`’“‘”].{1,2000}?[""«»‘’“”‛‟‹›“”„‘’`’“‘”](?=[^\w])", RegexOptions.Compiled | RegexOptions.Singleline);
 
         // covered by TestFixNonBreakingSpaces
         /// <summary>
