ae413e64cab39c9f5b054331db02b3b28324bf67
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2010-09-03 20:25:39 +0000

    WikiRegexes.Refs must be single line, update tests
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@7088 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/WikiRegexes.cs
+++ AWB/WikiFunctions/WikiRegexes.cs
@@ -895,7 +895,7 @@ namespace WikiFunctions
         /// <summary>
         /// matches &lt;ref&gt; tags, including named references
         /// </summary>
-        public static readonly Regex Refs = new Regex(@"(<\s*ref\s+(?:name|group)\s*=\s*[^<>]*?/\s*>|<\s*ref\b[^<>]*?>(?>(?!<\s*ref\b[^>/]*?>|<\s*/\s*ref\s*>).|<\s*ref\b[^>/]*?>(?<DEPTH>)|<\s*/\s*ref\s*>(?<-DEPTH>))*(?(DEPTH)(?!))<\s*/\s*ref\s*>)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        public static readonly Regex Refs = new Regex(@"(<\s*ref\s+(?:name|group)\s*=\s*[^<>]*?/\s*>|<\s*ref\b[^<>]*?>(?>(?!<\s*ref\b[^>/]*?>|<\s*/\s*ref\s*>).|<\s*ref\b[^>/]*?>(?<DEPTH>)|<\s*/\s*ref\s*>(?<-DEPTH>))*(?(DEPTH)(?!))<\s*/\s*ref\s*>)", RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.Compiled);
 
         /// <summary>
         /// matches &lt;cite&gt; tags
