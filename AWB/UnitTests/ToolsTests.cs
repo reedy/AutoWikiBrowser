925180f7ab053fb8ec29f98f3c020001e6840228
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2010-10-05 17:57:37 +0000

    more ConvertDate tests
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@7219 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/UnitTests/ToolsTests.cs
+++ AWB/UnitTests/ToolsTests.cs
@@ -858,6 +858,9 @@ John", "*"));
             Assert.AreEqual(@"11 June 2009", Tools.ConvertDate("June 11, 2009", Parsers.DateLocale.International));
             Assert.AreEqual(@"11 June 2009", Tools.ConvertDate("June 11, 2009", Parsers.DateLocale.International, true));
             
+            Assert.AreEqual(@"15 January 2008", Tools.ConvertDate("Jan 15 2008", Parsers.DateLocale.International));
+            Assert.AreEqual(@"15 January 2008", Tools.ConvertDate("Jan 15 2008", Parsers.DateLocale.International, false));
+            
             const string AmericanDate = @"06/11/2009", UKDate = @"11/06/2009";
             
             Assert.AreEqual(iso, Tools.ConvertDate(AmericanDate, Parsers.DateLocale.ISO, true), "Converts MM/DD/YYYY format dates when flagged");
