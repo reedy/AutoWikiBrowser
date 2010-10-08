9f65c888f24fc5b30f44c0b69993c590c72730b2
Sam Reed <sam@reedyboy.net>
2010-10-08 12:30:40 +0000

    Per Charwito, make wikias now only use their local (not some global) checkpage
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@7234 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Variables.cs
+++ AWB/WikiFunctions/Variables.cs
@@ -18,7 +18,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 using System;
 using System.Collections.Generic;
-using System.Windows.Forms;
 using System.Text.RegularExpressions;
 using WikiFunctions.Lists.Providers;
 using WikiFunctions.Plugin;
