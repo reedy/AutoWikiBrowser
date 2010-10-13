12a5a3d6c052cbbcff174b42cfb14df5ffa1276c
Sam Reed <sam@reedyboy.net>
2010-10-13 22:02:16 +0000

    5.1.0.0 for release
    Bump copyright year also
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@7266 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Properties/AssemblyInfo.cs
+++ WikiFunctions/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WikiFunctions")]
-[assembly: AssemblyCopyright("Copyright © 2009 AutoWikiBrowser Team")]
+[assembly: AssemblyCopyright("Copyright © 2010 AutoWikiBrowser Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.0.3.1")]
-[assembly: AssemblyFileVersion("5.0.3.1")]
+[assembly: AssemblyVersion("5.1.0.0")]
+[assembly: AssemblyFileVersion("5.1.0.0")]
