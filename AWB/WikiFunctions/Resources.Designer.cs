b3f86441406d6b1be9d64a9ff9ba591f7216c31e
Steven Kennedy <kingboyk@users.sourceforge.net>
2010-11-08 11:40:56 +0000

    Upped year to 2010
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@7377 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Resources.Designer.cs
+++ AWB/WikiFunctions/Resources.Designer.cs
@@ -88,7 +88,7 @@ namespace WikiFunctions.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Copyright (c) 2009 Martin Richards, Max Semenik, Sam Reed and Stephen Kennedy, et al. Please refer to source code headers for author details and copyright claims.
+        ///   Looks up a localized string similar to Copyright (c) 2010 Martin Richards, Max Semenik, Sam Reed and Stephen Kennedy, et al. Please refer to source code headers for author details and copyright claims.
         ///
         ///This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the license, or (at your option) any later version.
         ///
