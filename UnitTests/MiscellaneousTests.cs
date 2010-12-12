0d7f02f2555ad9b6a58437d50a28b14cdb087c49
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2010-12-12 21:25:47 +0000

    another test
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@7458 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/MiscellaneousTests.cs
+++ UnitTests/MiscellaneousTests.cs
@@ -1080,6 +1080,7 @@ __TOC__", articleTextIn);
         public void NormalizeTitle()
         {
             Assert.AreEqual("Foo", LMaker.NormalizeTitle(@"http://en.wikipedia.org/w/index.php?title=Foo&diff=3&oldid=4"));
+            Assert.AreEqual("Health effects of chocolate", LMaker.NormalizeTitle(@"http://en.wikipedia.org/w/index.php?title=Health_effects_of_chocolate&diff=4018&oldid=40182"));
             Assert.AreEqual("Foo", LMaker.NormalizeTitle(@"http://en.wikipedia.org/w/index.php?title=Foo&action=history"));
             Assert.AreEqual("Foo", LMaker.NormalizeTitle(@"http://en.wikipedia.org/w/index.php?title=Foo&oldid=5"));
             Assert.AreEqual("Science (journal)", LMaker.NormalizeTitle(@"http://en.wikipedia.org/w/index.php?title=Science%20%28journal%29&action=history"));
