7a9f64bfb5478d1b7f93aa66421fe900b8904a47
Marios Magioladitis <magioladitis@users.sourceforge.net>
2011-01-01 18:55:19 +0000

    Change settings to implied array size (Reedy)
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@7522 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Plugins/CFD/CfdCore.cs
+++ AWB/Plugins/CFD/CfdCore.cs
@@ -167,14 +167,11 @@ namespace AutoWikiBrowser.Plugins.CFD
         public object[] SaveSettings()
         {
             Settings.Enabled = PluginEnabled;
-
-            PrefsKeyPair[] prefs = new PrefsKeyPair[2];
-            prefs[0] = new PrefsKeyPair("Enabled", Settings.Enabled);
-            prefs[1] = new PrefsKeyPair("Skip", Settings.Skip);
-            prefs[3] = new PrefsKeyPair("AppendToEditSummary", Settings.AppendToEditSummary);
-            //prefs[3] = new PrefsKeyPair("categories", Settings.Images);
-
-            return prefs;
+            
+            return new PrefsKeyPair[] {
+            new PrefsKeyPair("Enabled", Settings.Enabled),
+            new PrefsKeyPair("Skip", Settings.Skip),
+            new PrefsKeyPair("AppendToEditSummary", Settings.AppendToEditSummary) };
         }
 
         public void Reset()
