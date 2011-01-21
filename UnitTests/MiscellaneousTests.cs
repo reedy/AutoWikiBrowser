df200e9176148c67af7db9a26e5608713e8f5fa3
Marios Magioladitis <magioladitis@users.sourceforge.net>
2011-01-21 10:48:28 +0000

    Expanded logic for {{activepol}}
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@7561 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/MiscellaneousTests.cs
+++ UnitTests/MiscellaneousTests.cs
@@ -1055,11 +1055,11 @@ __TOC__", articleTextIn);
             Assert.AreEqual(a.Replace(@"}}"," | living=yes}}"), TalkPageFixes.WPBiography(a + @"{{blp}}"), "Add blp to WPBiography");
             Assert.AreEqual(a.Replace(@"}}"," |living=yes}}"), TalkPageFixes.WPBiography(a.Replace(@"}}"," |living=}}") + @"{{blp}}"), "Add value to empty parameter");
             Assert.AreEqual(a.Replace(@"}}","|living=no}}" + @"{{blp}}"), TalkPageFixes.WPBiography(a.Replace(@"}}","|living=no}}") + @"{{blp}}"), "No change if blp=yes and living=no");
-            Assert.AreEqual(a.Replace(@"}}"," | activepol=yes}}"), TalkPageFixes.WPBiography(a + @"{{activepol}}"), "Add activepol to WPBiography");
-            Assert.AreEqual(a.Replace(@"}}"," | activepol=yes}}"), TalkPageFixes.WPBiography(a + @"{{active politician}}"), "Add activepol via redirect to WPBiography");
-            Assert.AreEqual(a.Replace(@"}}"," |activepol=yes}}"), TalkPageFixes.WPBiography(a.Replace(@"}}"," |activepol=}}") + @"{{activepol}}"), "Add value to empty parameter");
-            Assert.AreEqual(a.Replace(@"}}"," |activepol=yes}}"), TalkPageFixes.WPBiography(a.Replace(@"}}"," |activepol=no}}") + @"{{activepol}}"), "Change value to activepol no nonsense parameter");
-            Assert.AreEqual(a.Replace(@"}}"," | living=yes | activepol=yes}}"), TalkPageFixes.WPBiography(a + @"{{blp}}{{activepol}}"), "Add activepol and blp to WPBiography");
+            Assert.AreEqual(a.Replace(@"}}"," | living=yes | activepol=yes | politician-work-group=yes}}"), TalkPageFixes.WPBiography(a + @"{{activepol}}"), "Add activepol to WPBiography");
+            Assert.AreEqual(a.Replace(@"}}"," | living=yes | activepol=yes | politician-work-group=yes}}"), TalkPageFixes.WPBiography(a + @"{{active politician}}"), "Add activepol via redirect to WPBiography");
+            Assert.AreEqual(a.Replace(@"}}"," |activepol= yes| living=yes | politician-work-group=yes}}"), TalkPageFixes.WPBiography(a.Replace(@"}}"," |activepol=}}") + @"{{activepol}}"), "Add value to empty parameter");
+            Assert.AreEqual(a.Replace(@"}}"," |activepol=yes | living=yes | politician-work-group=yes}}"), TalkPageFixes.WPBiography(a.Replace(@"}}"," |activepol=no}}") + @"{{activepol}}"), "Change value to activepol no nonsense parameter");
+            Assert.AreEqual(a.Replace(@"}}"," | living=yes | activepol=yes | politician-work-group=yes}}"), TalkPageFixes.WPBiography(a + @"{{blp}}{{activepol}}"), "Add activepol and blp to WPBiography");
         }
 
     }
