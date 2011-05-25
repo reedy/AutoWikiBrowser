9f6a54a5f17356e9451e00c12aa5de65c6b7fe56
Sam Reed <sam@reedyboy.net>
2011-05-25 15:11:37 +0000

    Tagging 5.3
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@7729 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ tags/REL_5_3/AWB/ShutdownNotification.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AutoWikiBrowser
+{
+    public partial class ShutdownNotification : Form
+    {
+        int Counter = 120;  // 2 minutes
+        string SType;
+
+        public ShutdownNotification()
+        {
+            InitializeComponent();
+        }
+
+        public string ShutdownType
+        {
+            set {
+                SType = value;
+                txtPrompt.Text = string.Format(txtPrompt.Text, value);
+                SetShutdownLabel(Counter);
+            }
+        }
+
+        private void SetShutdownLabel(int time)
+        {
+            lblTimer.Text = "Time until " + SType + ": " + time;
+        }
+
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            Counter--;
+            if (Counter != 0)
+            {
+                SetShutdownLabel(Counter);
+                Application.DoEvents();
+            }
+            else
+                Close();
+        }
+    }
+}
---
+++ tags/REL_5_3/WikiFunctions/Controls/Lists/ListSplitter.cs
@@ -0,0 +1,161 @@
+﻿/*
+ListComparer
+Copyright (C) 2007 Sam Reed
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.IO;
+
+using WikiFunctions.AWBSettings;
+
+namespace WikiFunctions.Controls.Lists
+{
+    public partial class ListSplitter : Form
+    {
+        private readonly UserPrefs _p;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="prefs"></param>
+        public ListSplitter(UserPrefs prefs)
+        {
+            InitializeComponent();
+            _p = prefs;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="prefs"></param>
+        /// <param name="list"></param>
+        public ListSplitter(UserPrefs prefs, List<Article> list)
+            : this(prefs)
+        {
+            listMaker1.Add(list);
+        }
+
+        private void ListSplitter_Load(object sender, EventArgs e)
+        {
+            listMaker1.MakeListEnabled = true;
+        }
+
+        private readonly Regex _characterBlacklist = new Regex(@"[""/:*?<>|.]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveDialog(saveTXT, false);
+        }
+
+        private void btnXMLSave_Click(object sender, EventArgs e)
+        {
+            SaveDialog(saveXML, true);
+        }
+
+        private void SaveDialog(SaveFileDialog sfd, bool xml)
+        {
+            if (listMaker1.Count == 0)
+            {
+                MessageBox.Show("Nothing to save", "No items in List Maker");
+                return;
+            }
+
+            sfd.FileName = RemoveBadChars();
+
+            if (sfd.ShowDialog() == DialogResult.OK && !string.IsNullOrEmpty(sfd.FileName))
+                Save(sfd.FileName, xml);
+        }
+
+        private string RemoveBadChars()
+        {
+            string text = listMaker1.SourceText;
+            foreach (Match m in _characterBlacklist.Matches(listMaker1.SourceText))
+            {
+                text = text.Replace(m.Value, "");
+            }
+            return text;
+        }
+
+        private void Save(string path, bool xml)
+        {
+            listMaker1.AlphaSortList();
+            listMaker1.BeginUpdate();
+            try
+            {
+                int noA = listMaker1.Count;
+
+                int roundlimit = Convert.ToInt32(numSplitAmount.Value / 2);
+
+                if ((noA % numSplitAmount.Value) <= roundlimit)
+                    noA += roundlimit;
+
+                int noGroups =
+                    Convert.ToInt32((Math.Round(noA / numSplitAmount.Value) * numSplitAmount.Value) / numSplitAmount.Value);
+
+                if (xml)
+                {
+                    string pathPrefix = path.Replace(".xml", " {0}.xml");
+                    for (int i = 0; i < noGroups; i++)
+                    {
+                        List<Article> listart = new List<Article>();
+                        for (int j = 0; j < numSplitAmount.Value && listMaker1.Count != 0; j++)
+                        {
+                            listart.Add(listMaker1[j]);
+                        }
+
+                        _p.List.ArticleList = listart;
+
+                        UserPrefs.SavePrefs(_p, string.Format(pathPrefix, i + 1));
+                    }
+                    MessageBox.Show("Lists Saved to AWB Settings Files");
+                }
+                else
+                {
+                    string pathPrefix = path.Replace(".txt", " {0}.txt");
+                    for (int i = 0; i < noGroups; i++)
+                    {
+                        StringBuilder strList = new StringBuilder();
+
+                        for (int j = 0; j < numSplitAmount.Value && listMaker1.Count != 0; j++)
+                        {
+                            strList.AppendLine(listMaker1[j].ToString());
+                        }
+                        Tools.WriteTextFileAbsolutePath(strList.ToString(), string.Format(pathPrefix, i + 1),
+                                                        false);
+                    }
+                    MessageBox.Show("Lists saved to text files");
+                }
+
+                listMaker1.Clear();
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message, "Save error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.Handle(ex);
+            }
+
+
+            listMaker1.EndUpdate();
+        }
+    }
+}
