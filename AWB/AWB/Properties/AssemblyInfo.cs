1afd425a15009f060c1a140c49ceb1026f6ee452
Sam Reed <sam@reedyboy.net>
2011-05-25 15:09:23 +0000

    5300
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@7728 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWB/Properties/AssemblyInfo.cs
+++ AWB/AWB/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("5.2.0.1")]
-[assembly: AssemblyFileVersion("5.2.0.1")]
+[assembly: AssemblyVersion("5.3.0.0")]
+[assembly: AssemblyFileVersion("5.3.0.0")]
