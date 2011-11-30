2c7637fd4fc8d15503c1abbdd7969a33ec3ec4ba
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2011-11-30 19:20:54 +0000

    expand Tools.GetMetaContentValue to handle name or property tags
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@7865 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/ToolsTests.cs
+++ UnitTests/ToolsTests.cs
@@ -1810,6 +1810,7 @@ foo<!--comm-->|title=abc
             Assert.AreEqual(@"2009-03-02", Tools.GetMetaContentValue(@"< META NAME = ""PubDate""  content = ""2009-03-02""  />", "PubDate"));
             Assert.AreEqual(@"2009-03-02", Tools.GetMetaContentValue(@"<meta name  =""PubDate"" CONTENT="" 2009-03-02 "">", "PUBDATE"));
             Assert.AreEqual(@"2009-03-02", Tools.GetMetaContentValue(@"<meta name  =""PubDate"" scheme=""URI"" CONTENT="" 2009-03-02 "">", "PUBDATE"));
+            Assert.AreEqual(@"2009-03-02", Tools.GetMetaContentValue(@"<meta property  =""PubDate"" scheme=""URI"" CONTENT="" 2009-03-02 "">", "PUBDATE"));
             
             Assert.AreEqual(@"", Tools.GetMetaContentValue(@"<meta name  =""PubDate"" CONTENT="" 2009-03-02 "">", "PUBDATEXX"));
             Assert.AreEqual(@"", Tools.GetMetaContentValue(@"<meta name  =""PubDateX"" CONTENT="" 2009-03-02 "">", "PUBDATE"));
