fe30037c3cdf31875d0ffd9f1d2560da221f0155
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2012-01-27 08:13:42 +0000

    bugfix GetMetaContentValue
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@7940 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/ToolsTests.cs
+++ UnitTests/ToolsTests.cs
@@ -1811,6 +1811,7 @@ foo<!--comm-->|title=abc
         public void GetMetaContentValue()
         {
             Assert.AreEqual(@"2009-03-02", Tools.GetMetaContentValue(@"<meta name=""PubDate""  content=""2009-03-02"">", "PubDate"));
+            Assert.AreEqual(@"2009-03-02", Tools.GetMetaContentValue(@"<meta name='PubDate'  content='2009-03-02'>", "PubDate"));
             Assert.AreEqual(@"2009-03-02", Tools.GetMetaContentValue(@"< META NAME = ""PubDate""  content = ""2009-03-02""  />", "PubDate"));
             Assert.AreEqual(@"2009-03-02", Tools.GetMetaContentValue(@"<meta name  =""PubDate"" CONTENT="" 2009-03-02 "">", "PUBDATE"));
             Assert.AreEqual(@"2009-03-02", Tools.GetMetaContentValue(@"<meta name  =""PubDate"" scheme=""URI"" CONTENT="" 2009-03-02 "">", "PUBDATE"));
