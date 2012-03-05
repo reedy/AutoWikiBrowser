bd8f549d68c884fb521bf50dd1a3a459f1b2abfa
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2012-03-05 21:20:23 +0000

    Rearrange PerformGeneralFixes logic to fix https://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Bugs#Removes_.22excess.22_pipes_before_adding_missing_curly_braces
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@7978 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/GenfixesTests.cs
+++ UnitTests/GenfixesTests.cs
@@ -191,6 +191,25 @@ a");
             AssertChange(@"{{cite web | url = http://www.census.gov/popest/geographic/boundary_changes/index.html | year=2010 |title = Boundary Changes | date = 2010-1-1 }}", 
                          @"{{cite web | url = http://www.census.gov/popest/geographic/boundary_changes/index.html |title = Boundary Changes | date = 2010-01-01 }}");
         }
+        
+        [Test]
+        public void CiteMissingClosingCurlyBracePairTable()
+        {
+        	const string MissingClosingCurlyBracePair = @"'''Foo''' is great.
+
+{|
+||
+||Foo<ref>{{cite web | title = foo2 | year=2010 | url=http://www.site.com </ref>
+||
+||x
+||y
+|}
+
+==Refs==
+{{reflist}}";
+        	
+        	AssertChange(MissingClosingCurlyBracePair, MissingClosingCurlyBracePair.Replace(@" </ref>", @"}}</ref>")); // mising braces fixed before cleaning double pipes within unclosed cite template
+        }
     }
     
     [TestFixture]
