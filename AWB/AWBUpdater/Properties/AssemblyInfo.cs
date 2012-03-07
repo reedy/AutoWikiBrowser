4b0f1f3036e6a088011f92fbb3b1f5af31e22459
Sam Reed <sam@reedyboy.net>
2012-03-07 21:37:33 +0000

    Add message saying update is doned and you can close the updater
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@7991 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWBUpdater/Properties/AssemblyInfo.cs
+++ AWB/AWBUpdater/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.0.8")]
-[assembly: AssemblyFileVersion("2.0.0.8")]
+[assembly: AssemblyVersion("2.0.0.9")]
+[assembly: AssemblyFileVersion("2.0.0.9")]
