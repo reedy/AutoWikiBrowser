7e2c72d59ae1a136f38db38ae6abef75c3db5d44
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2012-04-09 15:23:19 +0000

    Fix for https://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Bugs#Multiple_issues_section_parameter_not_checked_when_changing.2Fremoving_parameters
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@8060 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/ParserTests.cs
+++ UnitTests/ParserTests.cs
@@ -8362,6 +8362,10 @@ Proin in odio. Pellentesque habitant morbi tristique senectus et netus et malesu
             text = parser.Tagger("{{multiple issues|COI=May 2010 | POV = May 2010 |wikify=June 2010}}" + Regex.Replace(LongText, @"(\w+)", "[[$1]]"), "Test", false, out noChange, ref summary);
             //wikify tag removed
             Assert.IsFalse(WikiRegexes.Wikify.IsMatch(text));
+            
+            text = parser.Tagger("A ==x== {{multiple issues|COI=May 2010 | POV = May 2010 |wikify=June 2010|section=y}} ==B==" + Regex.Replace(LongText, @"(\w+)", "[[$1]]"), "Test", false, out noChange, ref summary);
+            //wikify tag removed
+            Assert.IsTrue(WikiRegexes.Wikify.IsMatch(text));
 
             Globals.UnitTestIntValue = 4;
             text = parser.Tagger("{{uncategorised}}", "Test", false, out noChange, ref summary);
