e15c5c71f19e2ede11406f3544636f73ae0503f3
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2012-11-14 12:40:50 +0000

    FixSyntax: tweak regexes for performance improvement
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@8562 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Parse/Parsers.cs
+++ WikiFunctions/Parse/Parsers.cs
@@ -2709,7 +2709,7 @@ namespace WikiFunctions.Parse
         private static readonly Regex WordingIntoBareExternalLinks = new Regex(@"(<ref(?:\s*name\s*=[^{}<>]+?\s*)?>\s*)([^<>{}\[\]\r\n]{3,70}?)[\.,::]?\s*\[\s*((?:[Hh]ttp|[Hh]ttps|[Ff]tp|[Mm]ailto)://[^\ \n\r<>]+)\s*\](?=\s*</ref>)", RegexOptions.Compiled);
 
         // space needed between word and external link
-        private static readonly Regex ExternalLinkWordSpacingBefore = new Regex(@"(\w)(?=\[(?:https?|ftp|mailto|irc|gopher|telnet|nntp|worldwind|news|svn)://.*?\])", RegexOptions.Compiled);
+        private static readonly Regex ExternalLinkWordSpacingBefore = new Regex(@"(?<=\w)(\[(?:https?|ftp|mailto|irc|gopher|telnet|nntp|worldwind|news|svn)://.*?\])", RegexOptions.Compiled);
         private static readonly Regex ExternalLinkWordSpacingAfter = new Regex(@"(\[(?:https?|ftp|mailto|irc|gopher|telnet|nntp|worldwind|news|svn)://[^\]\[<>]*?\])(\w)", RegexOptions.Compiled);
 
         private static readonly Regex WikilinkEndsBr = new Regex(@"<br[\s/]*>\]\]$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
@@ -2871,7 +2871,7 @@ namespace WikiFunctions.Parse
 
             articleText = WordingIntoBareExternalLinks.Replace(articleText, @"$1[$3 $2]");
 
-            articleText = ExternalLinkWordSpacingBefore.Replace(articleText, "$1 ");
+            articleText = ExternalLinkWordSpacingBefore.Replace(articleText, " $1");
             articleText = ExternalLinkWordSpacingAfter.Replace(articleText, "$1 $2");
 
             // CHECKWIKI error 65: Image description ends with break – http://toolserver.org/~sk/cgi-bin/checkwiki/checkwiki.cgi?project=enwiki&view=only&id=65
