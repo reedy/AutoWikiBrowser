1ecbf34576d31ada566f7bfa0628e6d5b10fc243
Marios Magioladitis <magioladitis@users.sourceforge.net>
2012-11-18 13:53:00 +0000

    Added a (failed, commented out) test at DoublePipeLinkTests
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@8627 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/RegexTests.cs
+++ UnitTests/RegexTests.cs
@@ -339,6 +339,7 @@ Shul, p. 726    </ref>").Groups[2].Value, "ref value doesn't include leading/tra
             TestMatch(WikiRegexes.DoublePipeLink, "[[text|text2|text3]]", "[[text|text2|text3]]");
             TestMatch(WikiRegexes.DoublePipeLink, "[[Text|Text2|Text3]]", "[[Text|Text2|Text3]]");
             TestMatch(WikiRegexes.DoublePipeLink, "[[text||text3]]", "[[text||text3]]");
+            //TestMatch(WikiRegexes.DoublePipeLink, "[[text|(text2)|text3]]", "[[text|(text2)|text3]]");
             TestMatch(WikiRegexes.DoublePipeLink, "[[text|foo bar]]", false);
             TestMatch(WikiRegexes.DoublePipeLink, "[[|linktext]]", false);
             TestMatch(WikiRegexes.DoublePipeLink, "[[foo|bar]]", false);
