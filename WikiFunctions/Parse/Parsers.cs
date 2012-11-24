3a41eab9d3c922e58305435ea9eaabc224a15567
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2012-11-24 19:17:52 +0000

    Remove unneeded RegexOptions.Compiled
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@8690 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Parse/Parsers.cs
+++ WikiFunctions/Parse/Parsers.cs
@@ -2632,11 +2632,11 @@ namespace WikiFunctions.Parse
             return IncorrectCommaInternationalDates.Replace(textPortion, @"$1 $2");
         }
 
-        private static readonly Regex BrTwoNewlines = new Regex("(?:<br */?>)+\r\n\r\n", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-        private static readonly Regex FourOrMoreNewlines = new Regex("(\r\n){4,}", RegexOptions.Compiled);
-        private static readonly Regex NewlinesBelowExternalLinks = new Regex(@"==External links==[\r\n\s]*\*", RegexOptions.Compiled);
-        private static readonly Regex NewlinesBeforeUrl = new Regex(@"\r\n\r\n(\* ?\[?http)", RegexOptions.Compiled);
-        private static readonly Regex HorizontalRule = new Regex("----+$", RegexOptions.Compiled);
+        private static readonly Regex BrTwoNewlines = new Regex("(?:<br */?>)+\r\n\r\n", RegexOptions.IgnoreCase);
+        private static readonly Regex FourOrMoreNewlines = new Regex("(\r\n){4,}");
+        private static readonly Regex NewlinesBelowExternalLinks = new Regex(@"==External links==[\r\n\s]*\*");
+        private static readonly Regex NewlinesBeforeUrl = new Regex(@"\r\n\r\n(\* ?\[?http)");
+        private static readonly Regex HorizontalRule = new Regex("----+$");
         private static readonly Regex MultipleTabs = new Regex("  +", RegexOptions.Compiled);
         private static readonly Regex SpaceThenNewline = new Regex(" \r\n", RegexOptions.Compiled);
         private static readonly Regex WikiListWithMultipleSpaces = new Regex(@"^([\*#]+) +", RegexOptions.Compiled | RegexOptions.Multiline);
