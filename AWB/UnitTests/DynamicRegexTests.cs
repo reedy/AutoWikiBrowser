0240cfee884299508f4acbd5313fa3e587c55d7f
Marios Magioladitis <magioladitis@users.sourceforge.net>
2013-03-26 11:47:43 +0000

    Uncategorized stub is a stub template
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@8993 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/UnitTests/DynamicRegexTests.cs
+++ AWB/UnitTests/DynamicRegexTests.cs
@@ -527,6 +527,8 @@ Image here");
             RegexAssert.IsMatch(WikiRegexes.Stub, @"{{-stub}}");
             RegexAssert.IsMatch(WikiRegexes.Stub, @"{{Japan-spacecraft-stub|nocat=yes}}");
             RegexAssert.IsMatch(WikiRegexes.Stub, @"{{Japan-spacecraft-stub|date=August 2012}}");
+            RegexAssert.IsMatch(WikiRegexes.Stub, @"{{Uncategorized stub}}");
+            RegexAssert.IsMatch(WikiRegexes.Stub, @"{{Uncategorized stub|date=March 2013}}");
             
             RegexAssert.NoMatch(WikiRegexes.Stub, @"{{now stubborn}}");
             RegexAssert.NoMatch(WikiRegexes.Stub, @"{{stubby}}");
