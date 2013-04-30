191b05ce7d2381e940988cbc9f2346bd4bdfa402
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2013-04-30 19:44:43 +0000

    Commit file missed in last
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@9117 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/RegexTests.cs
+++ UnitTests/RegexTests.cs
@@ -2000,8 +2000,7 @@ Bert").Groups[2].Value, "foo bar\r");
         {
             Assert.IsTrue(WikiRegexes.SurnameClarificationTemplates.IsMatch(@"{{Malay name}}"));
             Assert.IsTrue(WikiRegexes.SurnameClarificationTemplates.IsMatch(@"{{Malay_name}}"));
-            Assert.IsTrue(WikiRegexes.SurnameClarificationTemplates.IsMatch(@"{{Malay-name}}"));
-            Assert.IsTrue(WikiRegexes.SurnameClarificationTemplates.IsMatch(@"{{Chinese-name}}"));
+            Assert.IsTrue(WikiRegexes.SurnameClarificationTemplates.IsMatch(@"{{Chinese name}}"));
             Assert.IsFalse(WikiRegexes.SurnameClarificationTemplates.IsMatch(@"{{Eastern name order}}"));
         }
         
