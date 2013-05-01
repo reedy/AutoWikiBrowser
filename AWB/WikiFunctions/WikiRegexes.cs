0dcfc867fd30169ffdb78242b8fa7d12835db499
Marios Magioladitis <magioladitis@users.sourceforge.net>
2013-05-01 07:26:27 +0000

    Yet another infobox
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@9119 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/WikiRegexes.cs
+++ AWB/WikiFunctions/WikiRegexes.cs
@@ -1088,7 +1088,7 @@ namespace WikiFunctions
                                                                                             "Infobox US associate justice", "Infobox US cabinet official", "Infobox US chief justice", "Infobox US territorial governor", "Infobox vice president", "Infobox US ambassador", "Infobox Eritrea cabinet official",
                                                                                             "Infobox sportsperson", "Infobox NFL player", "Infobox football biography", "Infobox football official", "Infobox golfer", "Infobox gridiron football person", "Infobox gymnast", "Infobox handball biography,",
                                                                                             "Infobox ice hockey player", "Infobox motorcycle rider", "Infobox rugby biography", "Infobox rugby league biography", "Infobox Rugby Union biography", "Infobox rugby union biography", "Infobox cricketer", "Infobox nobility",
-                                                                                            "Infobox noble", "Infobox tennis biography", "Infobox handball biography", "Infobox pro football player", "Infobox badminton player", "Infobox basketball official", "Infobox bodybuilder", "Infobox boxer"}, false);
+                                                                                            "Infobox noble", "Infobox tennis biography", "Infobox handball biography", "Infobox pro football player", "Infobox badminton player", "Infobox basketball official", "Infobox bodybuilder", "Infobox boxer", "Infobox MLB player"}, false);
 
         /// <summary>
         /// Matches the {{circa}} template
