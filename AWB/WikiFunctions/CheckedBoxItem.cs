be6fd211b17b58b13e422f8dd1151f3509160ad5
Sam Reed <sam@reedyboy.net>
2013-05-26 20:36:03 +0000

    Damn you Microsoft for not making Dictionaries seriali[sz]able because you ran out of time
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@9210 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ AWB/WikiFunctions/CheckedBoxItem.cs
@@ -0,0 +1,12 @@
+﻿namespace WikiFunctions
+{
+    public class CheckedBoxItem
+    {
+        public int ID;
+        public string Description;
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+}
