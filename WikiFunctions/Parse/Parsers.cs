0739e09c34125cd6e88a41a19a93eb0054bfb13d
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2013-08-13 10:59:40 +0000

    Slight performance fix to FixSyntax
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@9419 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Parse/Parsers.cs
+++ WikiFunctions/Parse/Parsers.cs
@@ -3042,9 +3042,8 @@ namespace WikiFunctions.Parse
         private static readonly Regex SyntaxRegexSimpleWikilinkEndsWithSpaces = new Regex(@"\[\[([A-Za-z]*) \]\]", RegexOptions.Compiled);
 
         private static readonly Regex SyntaxRegexListRowBrTag = new Regex(@"^([#\*:;]+.*?) *(?:<[/\\]?br ?[/\\]? ?>)+ *\r\n", RegexOptions.Multiline | RegexOptions.IgnoreCase);
-        private static readonly Regex SyntaxRegexListRowBrTagQuick = new Regex(@"<[/\\]?[Bb][Rr] ?[/\\]? ?>\s*$", RegexOptions.Multiline);
         private static readonly Regex SyntaxRegexListRowBrTagMiddle = new Regex(@"^([#\*:;]+.*?)\s*(?:<[/\\]?br ?[/\\]? ?>)+ *\r\n([#\*:;]+)", RegexOptions.Multiline | RegexOptions.IgnoreCase);
-        private static readonly Regex SyntaxRegexBrNewlineList = new Regex(@"<[/\\]?[Bb][Rr] ?[/\\]? ?> *\r\n[#\*:;]");
+        private static readonly Regex SyntaxRegexBrNewline = new Regex(@"<[/\\]?[Bb][Rr] ?[/\\]? ?> *\r\n");
 
         private static readonly Regex SyntaxRegexListRowBrTagStart = new Regex(@"<[/\\]?br ?[/\\]? ?> *(\r\n[#\*:;]+)", RegexOptions.IgnoreCase);
 
@@ -3163,13 +3162,14 @@ namespace WikiFunctions.Parse
             // remove unnecessary namespace
             articleText = RemoveTemplateNamespace(articleText);
 
-            // remove <br> from lists (end of list line) - CHECKWIKI error 54
-            if(SyntaxRegexListRowBrTagQuick.IsMatch(articleText))
+            if(SyntaxRegexBrNewline.IsMatch(articleText))
+            {
+                // remove <br> from lists (end of list line) - CHECKWIKI error 54
                 articleText = SyntaxRegexListRowBrTag.Replace(articleText, "$1\r\n");
 
-            // remove <br> from the middle of lists
-            if(SyntaxRegexBrNewlineList.IsMatch(articleText))
+                // remove <br> from the middle of lists
                 articleText = SyntaxRegexListRowBrTagMiddle.Replace(articleText, "$1\r\n$2");
+            }
 
             articleText = MultipleHttpInLink.Replace(articleText, "$1");
             articleText = MultipleFtpInLink.Replace(articleText, "$1");
