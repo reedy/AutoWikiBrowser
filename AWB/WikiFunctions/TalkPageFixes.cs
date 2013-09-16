2c604023bc798155e5a2885be4773a29e773c1dc
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2013-09-16 16:19:46 +0000

    For https://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Bugs#Talk_page_genfixes_ignore_.3Cgallery.3E_tags
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@9488 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/TalkPageFixes.cs
+++ AWB/WikiFunctions/TalkPageFixes.cs
@@ -174,6 +174,7 @@ namespace WikiFunctions.TalkPages
             // don't match on lines within templates
             string articleTextTemplatesSpaced = Tools.ReplaceWithSpaces(articleText, WikiRegexes.NestedTemplates.Matches(articleText));
             articleTextTemplatesSpaced = Tools.ReplaceWithSpaces(articleTextTemplatesSpaced, WikiRegexes.UnformattedText.Matches(articleTextTemplatesSpaced));
+            articleTextTemplatesSpaced = Tools.ReplaceWithSpaces(articleTextTemplatesSpaced, WikiRegexes.GalleryTag.Matches(articleTextTemplatesSpaced));
 
             if (FirstComment.IsMatch(articleTextTemplatesSpaced))
             {
