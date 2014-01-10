fd003f5b231526e20333e4b6dda749a267b6f4e9
Marios Magioladitis <magioladitis@users.sourceforge.net>
2014-01-10 09:01:33 +0000

    Added a failing commented out test until we fix the bug
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@9847 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/UnitTests/ParserTests.cs
+++ AWB/UnitTests/ParserTests.cs
@@ -8346,10 +8346,12 @@ fish | name = Bert }} ''Bert'' is a good fish."));
             Assert.IsFalse(Parsers.CheckNoBots("{{nobots|deny=awb,test}}", "test"));
             Assert.IsFalse(Parsers.CheckNoBots("{{bots|deny=test}}", "test"));
             Assert.IsFalse(Parsers.CheckNoBots("{{bots|allow=none}}", ""));
+            //Assert.IsFalse(Parsers.CheckNoBots(@"{{bots|deny=AWB, MenoBot, MenoBot II}}", "AWB and other bots"));
 
             Assert.IsFalse(Parsers.CheckNoBots(@"{{bots|allow=MiszaBot III,SineBot}}", "otherBot"));
             Assert.IsTrue(Parsers.CheckNoBots(@"{{bots|allow=MiszaBot III,SineBot}}", "SineBot"));
-            Assert.IsTrue(Parsers.CheckNoBots(@"{{bots|allow=MiszaBot III,SineBot, AWB}}", "SomeotherBot"));
+            Assert.IsTrue(Parsers.CheckNoBots(@"{{bots|allow=MiszaBot III,SineBot, AWB}}", "SomeotherBot, AWB last"));
+            Assert.IsTrue(Parsers.CheckNoBots(@"{{bots|allow=AWB, MiszaBot III,SineBot}}", "SomeotherBot, AWB first"));
 
             /* prospective future changes to bots template
             Assert.IsTrue(Parsers.CheckNoBots(@"{{bots|deny=all|allow=MiszaBot III,SineBot}}", "SineBot"));
