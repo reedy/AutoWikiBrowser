8b63616d609e88028580ae9951a8d9b612802bf1
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2014-02-01 14:45:47 +0000

    Tweak to IsArticleAboutAPerson
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@9901 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/ParserTests.cs
+++ UnitTests/ParserTests.cs
@@ -7964,6 +7964,7 @@ foo {{persondata}}
             Assert.IsFalse(Parsers.IsArticleAboutAPerson(@"Foo [[Category:Some noble families]] {{foo-bio-stub}}", "foo"));
             Assert.IsFalse(Parsers.IsArticleAboutAPerson(@"Foo [[Category:Noble families]] {{foo-bio-stub}}", "foo"));
             Assert.IsFalse(Parsers.IsArticleAboutAPerson(@"Foo [[Category:X musical groups]] {{foo-bio-stub}}", "foo"));
+            Assert.IsFalse(Parsers.IsArticleAboutAPerson(@"Foo [[Category:Musical groups established in 2000]] {{foo-bio-stub}}", "foo"));
             Assert.IsFalse(Parsers.IsArticleAboutAPerson(@"Foo {{infobox television|bye=a}} {{refimproveBLP}}", "foo"));
             Assert.IsFalse(Parsers.IsArticleAboutAPerson(@"'''Foo''' {{England-bio-stub}} {{sia}}", "Foo"));
 
