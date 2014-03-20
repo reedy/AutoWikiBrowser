4cb321a3f4c5a883e3d40b11a7c5a7d1c8447904
Sam Reed <sam@reedyboy.net>
2014-03-20 00:07:37 +0000

    Trap error if unable to create temporary directory, then bail out
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@9992 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWBUpdater/Properties/AssemblyInfo.cs
+++ AWB/AWBUpdater/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AWBUpdater")]
-[assembly: AssemblyCopyright("Copyright © 2013 AutoWikiBrowser Team")]
+[assembly: AssemblyCopyright("Copyright © 2014 AutoWikiBrowser Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.2.0")]
