0b2017a1355aec622cf5e6b554d62fbedbe36577
Sam Reed <sam@reedyboy.net>
2014-03-27 10:52:51 +0000

    MFB 10012-30, 10037-10039
    Merged revision(s) 10037-10039 from branches/AWBPluginCS:
    Allow plugin loading
    
    Fix NRE due to menu item (seemingly been there a while maybe)
    ........
    Drop ToolsVersion to 4 for non VS IDEs
    ........
    Remove old BMP reference
    ........
    Merged revision(s) 10012-10030 from branches/AWBPluginCS:
    De-folder and reorder Kingbotk plugin
    ........
    Force x86 again
    ........
    Fixup AboutBox designer
    
    Remove dupe using
    ........
    Fixup designer related migration crap
    ........
    Connect event
    ........
    Fixup designer issues with BioWithWorkgroups
    ........
    Fix indenting
    ........
    Kill off VbCrLf
    ........
    Newline -> NewLine
    
    Damn autocomplete
    ........
    Remove more Visual Basic-isms and code usage
    ........
    And another
    ........
    Restore VB scaryness temporarily
    ........
    Fix orphaned AddGenericTemplateMenuItem_Click
    
    Remove more VBness
    ........
    Cleanup vbness
    ........
    Remove withEvents from designer
    ........
    Fixup rest of vbism withevents
    ........
    Reattached handlers
    ........
    Reattach orphaned event handlers
    ........
    More cleanup in one way or another
    ........
    
    
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@10042 5227b50d-9349-4574-b055-4b0a7525e11c

---
+++ Plugins/Kingbotk Plugin/TimerStats.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+//Copyright © 2008 Stephen Kennedy (Kingboyk) http://www.sdk-software.com/
+//Copyright © 2008 Sam Reed (Reedy) http://www.reedyboy.net/
+
+//This program is free software; you can redistribute it and/or modify it under the terms of Version 2 of the GNU General Public License as published by the Free Software Foundation.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License Version 2 along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+using WikiFunctions.API;
+
+namespace AutoWikiBrowser.Plugins.Kingbotk.Components
+{
+	internal sealed partial class TimerStats
+	{
+	    private PluginSettingsControl.Stats mStats;
+		private TimeSpan TimeSpan;
+		private DateTime Start;
+		private int mNumberOfEdits;
+		private int mSkipped;
+
+		private Label mETALabel;
+		private int NumberOfEdits {
+			get { return mNumberOfEdits; }
+			set {
+				mNumberOfEdits = value;
+				EditsLabel.Text = mNumberOfEdits.ToString();
+			}
+		}
+
+		internal void Init(AsyncApiEdit e, Label ETALabel, PluginSettingsControl.Stats Stats)
+		{
+			if (!TimerEnabled) {
+				ResetVars();
+				mETALabel = ETALabel;
+
+				TimerEnabled = true;
+
+				mStats = Stats;
+			    mStats.SkipMisc += mStats_SkipMisc;
+
+				Timer1_Tick(null, null);
+			}
+		}
+		internal void Reset()
+		{
+			ResetVars();
+			TimerLabel.Text = "0:00";
+			SpeedLabel.Text = "0";
+			EditsLabel.Text = "0";
+		}
+		private void ResetVars()
+		{
+			NumberOfEdits = 0;
+			Start = DateTime.Now;
+			mSkipped = 0;
+		}
+		internal void StopStats()
+		{
+			ResetVars();
+			TimerEnabled = false;
+		}
+		private string ETA {
+			get { return mETALabel.Text.Replace("ETC: ", ""); }
+			set { mETALabel.Text = "ETC: " + value; }
+		}
+		private void CalculateETA(double SecondsPerPage)
+		{
+			int Count = PluginManager.AWBForm.ListMaker.Count;
+
+			if (Count == 0) {
+				ETA = "Now";
+			} else {
+				DateTime ETADateTime = DateTime.Now.AddSeconds(SecondsPerPage * Count);
+
+				if (ETADateTime.Date == DateTime.Now.Date) {
+					ETA = ETADateTime.ToString("HH:mm") + " today";
+				} else if (DateTime.Now.AddDays(1).Date == ETADateTime.Date) {
+					ETA = ETADateTime.ToString("HH:mm") + " tomorrow";
+				} else {
+					ETA = ETADateTime.ToString("HH:mm, ddd d MMM");
+				}
+			}
+		}
+		private bool TimerEnabled {
+			get { return Timer1.Enabled; }
+			set {
+				if (mETALabel != null) {
+					mETALabel.Visible = value;
+				}
+				Timer1.Enabled = value;
+			}
+		}
+
+		// Event handlers
+		private readonly Regex timerregexp = new Regex("\\..*");
+		int UpdateETACount;
+		
+        private void Timer1_Tick(object sender, EventArgs e)
+		{
+		    UpdateETACount += 1;
+			TimeSpan = DateTime.Now - Start;
+			TimerLabel.Text = timerregexp.Replace(TimeSpan.ToString(), "");
+		    double secondsPerPage = NumberOfEdits == 0 ? TimeSpan.TotalSeconds : Math.Round(TimeSpan.TotalSeconds/NumberOfEdits, 2);
+
+			if (double.IsInfinity(secondsPerPage)) {
+				SpeedLabel.Text = "0";
+				ETA = "-";
+				if (UpdateETACount > 9)
+					UpdateETACount = 0;
+			} else {
+				SpeedLabel.Text = secondsPerPage + " s/p";
+				if (UpdateETACount > 9 || ETA == "-") {
+					UpdateETACount = 0;
+					if ((NumberOfEdits + mSkipped) == 0) {
+						CalculateETA(TimeSpan.TotalSeconds);
+					} else {
+						CalculateETA(TimeSpan.TotalSeconds / (NumberOfEdits + mSkipped));
+					}
+
+				}
+			}
+		}
+		internal void IncrementSavedEdits(AsyncApiEdit sender, SaveInfo save)
+		{
+			IncrementSavedEdits();
+		}
+		internal void IncrementSavedEdits()
+		{
+			NumberOfEdits += 1;
+		}
+		private void mStats_SkipMisc(int val)
+		{
+			mSkipped += 1;
+		}
+		public TimerStats()
+		{
+			InitializeComponent();
+		}
+	}
+}
