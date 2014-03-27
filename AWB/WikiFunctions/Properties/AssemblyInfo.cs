cfa7303fa6a071c3bc2e88e3457c2da5cedb7935
Sam Reed <sam@reedyboy.net>
2014-03-27 00:40:48 +0000

    Update copyright years
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@10032 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/WikiFunctions/Properties/AssemblyInfo.cs
+++ AWB/WikiFunctions/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -9,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WikiFunctions")]
-[assembly: AssemblyCopyright("Copyright © 2013 AutoWikiBrowser Team")]
+[assembly: AssemblyCopyright("Copyright © 2006-2014 AutoWikiBrowser Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
