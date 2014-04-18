7fcc09fc4803b5029d194701700e10d14671386d
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2014-04-18 18:39:50 +0000

    Support another variation in GetMetaContentValue
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@10078 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Tools.cs
+++ WikiFunctions/Tools.cs
@@ -782,7 +782,7 @@ namespace WikiFunctions
 			if (pagesource.Length == 0 || metaname.Length == 0)
 				return "";
 
-			Regex metaContent = new Regex(@"< *meta +(?:xmlns=""http://www\.w3\.org/1999/xhtml"" +)?(?:name|property) *= *(?:""|')" + Regex.Escape(metaname) + @"(?:""|')[^<>/]+content *= *(?:""|')([^""<>]+?)(?:""|') */? *>", RegexOptions.IgnoreCase);
+			Regex metaContent = new Regex(@"< *meta +(?:xmlns=""http://www\.w3\.org/1999/xhtml"" +)?(?:name|property|itemprop) *= *(?:""|')" + Regex.Escape(metaname) + @"(?:""|')[^<>/]+content *= *(?:""|')([^""<>]+?)(?:""|') */? *>", RegexOptions.IgnoreCase);
 			Regex metaContent2 = new Regex(@"< *meta +(?:xmlns=""http://www\.w3\.org/1999/xhtml"" +)?[^<>/]*content *= *(?:""|')([^""<>]+?)(?:""|') *(?:name|property) *= *(?:""|')" + Regex.Escape(metaname) + @"(?:""|') */? *>", RegexOptions.IgnoreCase);
 
 			if(metaContent.IsMatch(pagesource))
