2063cd7e5e0d211e02dd2405ade99ffff7e2ca2e
Marios Magioladitis <magioladitis@users.sourceforge.net>
2014-05-11 09:30:29 +0000

    Removed unused using
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code@10165 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Plugins/Kingbotk Plugin/Article.cs
+++ AWB/Plugins/Kingbotk Plugin/Article.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using WikiFunctions;
 
 //Copyright © 2008 Stephen Kennedy (Kingboyk) http://www.sdk-software.com/
