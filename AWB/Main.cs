c8c1a4d5218d3e21aa83325ce33b4f050fdfe237
Marios Magioladitis <magioladitis@users.sourceforge.net>
2014-07-09 15:50:10 +0000

    Removed some redundant conversion
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@10298 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Main.cs
+++ AWB/Main.cs
@@ -2077,7 +2077,7 @@ font-size: 150%;'>No changes</h2><p>Press the ""Skip"" button below to skip to t
                 GetDiff();
 
                 // scroll back to where user was
-                object[] ob = {"window.scrollTo(0, " + webBrowserYScroll.ToString() + @")"};
+                object[] ob = {"window.scrollTo(0, " + webBrowserYScroll + @")"};
                 webBrowser.Document.InvokeScript("eval", ob);
 
                 // now put caret back where it was
