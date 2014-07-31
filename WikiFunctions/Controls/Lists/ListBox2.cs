7add2f5fb722ac1417e59e5e59625f7e3b71e7d8
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2014-07-31 15:33:18 +0000

    Use List.Except when .NET 3.5 runtime available for big performance improvement in RemoveSelected
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@10335 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Controls/Lists/ListBox2.cs
+++ WikiFunctions/Controls/Lists/ListBox2.cs
@@ -23,6 +23,7 @@ using System.Collections.Generic;
 using System.Collections;
 using System.Text;
 using System.Windows.Forms;
+using System.Linq;
 
 namespace WikiFunctions.Controls.Lists
 {
@@ -122,54 +123,71 @@ namespace WikiFunctions.Controls.Lists
             EndUpdate();
         }
 
-        /// <summary>
-        /// Removes the currently selected articles from the list
-        /// Operates in two modes: a quick (even at 50k volumes) mode when the selected items are in a contiguous block
-        /// OR an individual mode for non-contiguous selections, which can be slow on large volumes
-        /// </summary>
+        // Removes the currently selected articles from the list
         public void RemoveSelected()
         {
             int i = SelectedIndex;
-
+            
             if(i >= 0) // check something selected
-            {                
-                // is this a contiguous block of selected items: e.g. last selected minus first selected+1 equals the number of selected items
-                if(SelectedIndices[SelectedItems.Count-1]-SelectedIndex+1 == SelectedItems.Count)
-                {
-                    /* Fast block mode: convert all articles in listbox into an article list, remove range from article list by index, then add articles back to 
-                       listbox. This means loop through listbox once, a single remove by index operation and a single loop through article list.
-                       Alternative of removing selected articles one by one from listbox means each removal requires scan of listbox from beginning up
-                       to point of item. e.g. scan 10,000x5 to remove five entries from position 10000 in a list. */
+            {
+                BeginUpdate();
 
-                    List<Article> articles = new List<Article>();
-                    
-                    for (int j = 0; j < Items.Count; j++)
-                        articles.Add((Article)Items[j]);
-
-                    articles.RemoveRange(SelectedIndex, SelectedItems.Count);
-                    
-                    BeginUpdate();
-                    Items.Clear();
-                    Items.AddRange(articles.ToArray());
-                }
-                else // non-contiguous, fall back to loop mode, could be slow on large data volumes.
-                {
-                    BeginUpdate();
-
-                    /* remove at index rather than removing article, else if list has article twice the first instance of it
-                     * will be removed, not necessarily the one the user selected */
-                    while (SelectedItems.Count > 0)
-                        Items.RemoveAt(SelectedIndex);
-                }
+                if(Globals.SystemCore3500Available)
+                    RemoveSelectedNew();
+                else
+                    RemoveSelectedOld();
 
                 // update selected index: if last deleted article was at end of list then set selected item at end of list
                 if (Items.Count > i)
                     SelectedIndex = i;
                 else
                     SelectedIndex = Math.Min(i, Items.Count) - 1;
-
+                
                 EndUpdate();
             }
+        }
+        /// <summary>
+        /// Removes the currently selected articles from the list
+        /// Operates in two modes: a relatively quick (even at 50k volumes) mode when the selected items are in a contiguous block
+        /// OR an individual mode for non-contiguous selections, which can be very slow on large volumes
+        /// </summary>
+        private void RemoveSelectedOld()
+        {
+            // is this a contiguous block of selected items: e.g. last selected minus first selected+1 equals the number of selected items
+            if(SelectedIndices[SelectedItems.Count-1]-SelectedIndex+1 == SelectedItems.Count)
+            {
+                /* Fast block mode: convert all articles in listbox into an article list, remove range from article list by index, then add articles back to 
+                   listbox. This means loop through listbox once, a single remove by index operation and a single loop through article list.
+                   Alternative of removing selected articles one by one from listbox means each removal requires scan of listbox from beginning up
+                   to point of item. e.g. scan 10,000x5 to remove five entries from position 10000 in a list. */
+
+                List<Article> articles = new List<Article>();
+                
+                for (int j = 0; j < Items.Count; j++)
+                    articles.Add((Article)Items[j]);
+
+                articles.RemoveRange(SelectedIndex, SelectedItems.Count);
+
+                Items.Clear();
+                Items.AddRange(articles.ToArray());
+            }
+            else // non-contiguous, fall back to loop mode, could be slow on large data volumes.
+            {
+                /* remove at index rather than removing article, else if list has article twice the first instance of it
+                 * will be removed, not necessarily the one the user selected */
+                while (SelectedItems.Count > 0)
+                    Items.RemoveAt(SelectedIndex);
+            }
+        }
+
+        // Fast implementation to remove the currently selected articles from the list, making use of List.Except method
+        private void RemoveSelectedNew()
+        {
+            List<Article> articles = Items.Cast<Article>().ToList();
+            List<Article> Sarticles = SelectedItems.Cast<Article>().ToList();
+
+            Items.Clear();
+            Items.AddRange(articles.Except(Sarticles).ToArray());
         }
 
         static string _list = "";
