167cfde9dc83696f4a560c4dac8e1a8c9797fbb9
Marios Magioladitis <magioladitis@users.sourceforge.net>
2014-08-20 17:47:29 +0000

    5.5.4.1
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@10395 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Properties/AssemblyInfo.cs
+++ AWB/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("5.5.4.0")]
-[assembly: AssemblyFileVersion("5.5.4.0")]
+[assembly: AssemblyVersion("5.5.4.1")]
+[assembly: AssemblyFileVersion("5.5.4.1")]
 
 [assembly: CLSCompliant(true)]
