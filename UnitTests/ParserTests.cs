47452511c69a95feac3e540e5174b808e341cdf3
Marios Magioladitis <magioladitis@users.sourceforge.net>
2014-12-10 08:31:08 +0000

    Updated citewebparameters once again
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@10512 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/ParserTests.cs
+++ UnitTests/ParserTests.cs
@@ -752,14 +752,14 @@ Jones 2005</ref>"));
             Assert.AreEqual(Found, Parsers.BadCiteParameters(@"now {{cite web|url=bar|date=2009|first1=Foo|last1=Great |first2=Bar|title=here}} was"));
             Assert.AreEqual(Found, Parsers.BadCiteParameters(@"now {{cite web|url=bar|date=2009|authorlink1=Smith}} was"));
             Assert.AreEqual(Found, Parsers.BadCiteParameters(@"now {{cite web|url=bar|date=2009|authorlink20=Bill}} was"));
-            Assert.AreEqual(Found, Parsers.BadCiteParameters(@"now {{cite web|url=bar|date=2009|contribution=Smith}} was"));
+            Assert.AreEqual(Found, Parsers.BadCiteParameters(@"now {{cite web|url=bar|date=2009|author=Smith}} was"));
             Assert.AreEqual(Found, Parsers.BadCiteParameters(@"{{cite web | periodical=, |journal=, |newspaper=, |magazine= |work= |website= |encyclopedia= |encyclopaedia= |dictionary=
-|contribution=, |entry=, |article=, |section=
+|arxiv=, |ARXIV=, |ASIN=, |ASIN-TLD=
 |publicationplace=, |publication-place=
 |date=, |year=, |publicationdate=, |publication-date=
 |series= |volume= |issue=, |number= |page=, |pages=, |at=
 |edition= |publisher=, |institution=
-|chapterurl=, |chapter-url=, |contributionurl=, |contribution-url=, |sectionurl= }}"));
+|journal=, |jstor=, |agency=, |archive-date=, |others= }}"));
 
             // multiple errors
             Found.Add(15, 6);
