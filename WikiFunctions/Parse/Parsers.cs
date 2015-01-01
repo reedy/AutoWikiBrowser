035becacc894b01dda4a51753d938273dce4003e
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2015-01-01 18:41:23 +0000

    Simplify EmptyReferences
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@10635 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Parse/Parsers.cs
+++ WikiFunctions/Parse/Parsers.cs
@@ -2876,7 +2876,7 @@ namespace WikiFunctions.Parse
             return ReferenceListTags.Replace(articleText, ReflistMatchEvaluator);
         }
 
-        private static readonly Regex EmptyReferences = new Regex(@"(<ref\s+name\s*=\s*(?:""|')?[^<>=\r\n]+?(?:""|')?)\s*(?:/\s*)?>\s*<\s*/\s*ref\s*>", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private static readonly Regex EmptyReferences = new Regex(@"(<ref\s+name\s*=\s*[^<>=\r\n]+?)\s*(?:/\s*)?>\s*<\s*/\s*ref\s*>", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
         // Covered by: FootnotesTests.TestSimplifyReferenceTags()
         /// <summary>
