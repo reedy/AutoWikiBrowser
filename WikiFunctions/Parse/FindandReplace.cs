400fbd03a03aa47d2684b8dd899ac91bd4f2392c
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2015-01-08 14:16:55 +0000

    Tidy up
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@10721 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Parse/FindandReplace.cs
+++ WikiFunctions/Parse/FindandReplace.cs
@@ -117,10 +117,7 @@ namespace WikiFunctions.Parse
 
             foreach (DataGridViewRow dataGridRow in dataGridView1.Rows)
             {
-                if (dataGridRow.IsNewRow)
-                    continue;
-
-                if (dataGridRow.Cells["find"].Value == null)
+                if (dataGridRow.IsNewRow || dataGridRow.Cells["find"].Value == null)
                     continue;
 
                 _replacementList.Add(RowToReplacement(dataGridRow));
