27ecb0c92d20a0f160b2882df8cbed91815fc2d6
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2015-01-22 10:23:37 +0000

    UnclosedTags to also cover center tags
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@10788 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/LinkTests.cs
+++ UnitTests/LinkTests.cs
@@ -3938,6 +3938,11 @@ x
             Assert.IsTrue(uct.ContainsKey(15));
             Assert.IsTrue(uct.ContainsValue(6));
 
+            uct = Parsers.UnclosedTags(@"<pre>bar</pre> <center> not ended");
+            Assert.AreEqual(uct.Count, 1);
+            Assert.IsTrue(uct.ContainsKey(15));
+            Assert.IsTrue(uct.ContainsValue(8));
+
             uct = Parsers.UnclosedTags(@"<pre>bar</pre> <ref name=<ref name=Foo/> not ended");
             Assert.AreEqual(uct.Count, 1);
             Assert.IsTrue(uct.ContainsKey(15));
