83de84431caca9487625a28ea6f41d3cfa039b18
Sam Reed <sam@reedyboy.net>
2015-05-16 18:34:52 +0000

    Bump AWBPackager versions and copyright
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@10940 5227b50d-9349-4574-b055-4b0a7525e11c

--- Extras/AWBPackager/Properties/AssemblyInfo.cs
+++ Extras/AWBPackager/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AWBPackager")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © AWB Team 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
