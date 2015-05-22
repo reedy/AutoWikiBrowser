4817b1a42b2ae77d6b639d8e406a6d59b8d9bcf6
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2015-05-22 18:11:25 +0000

    Follow up to last to avoid finding there are unclosed tags in new check due to commented out text containing valid tags
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@10967 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Parse/Parsers.cs
+++ WikiFunctions/Parse/Parsers.cs
@@ -2824,7 +2824,7 @@ namespace WikiFunctions.Parse
         private static readonly Regex CenterTag = new Regex(@"<\s*center\s*>((?>(?!<\s*/?\s*center\s*>).|<\s*center\s*>(?<DEPTH>)|<\s*/\s*center\s*>(?<-DEPTH>))*(?(DEPTH)(?!)))<\s*/\s*center\s*>", RegexOptions.Singleline | RegexOptions.IgnoreCase);
         private static readonly Regex SupTag = new Regex(@"<\s*sup\s*>((?>(?!<\s*/?\s*sup\s*>).|<\s*sup\s*>(?<DEPTH>)|<\s*/\s*sup\s*>(?<-DEPTH>))*(?(DEPTH)(?!)))<\s*/\s*sup\s*>", RegexOptions.Singleline | RegexOptions.IgnoreCase);
         private static readonly Regex SubTag = new Regex(@"<\s*sub\s*>((?>(?!<\s*/?\s*sub\s*>).|<\s*sub\s*>(?<DEPTH>)|<\s*/\s*sub\s*>(?<-DEPTH>))*(?(DEPTH)(?!)))<\s*/\s*sub\s*>", RegexOptions.Singleline | RegexOptions.IgnoreCase);
-        private static readonly Regex AnyTag = new Regex(@"<([^>]+)>");
+        private static readonly Regex AnyTag = new Regex(@"<([^<>]+)>");
         /// <summary>
         ///  Searches for any unclosed &lt;math&gt;, &lt;source&gt;, &lt;ref&gt;, &lt;code&gt;, &lt;nowiki&gt;, &lt;small&gt;, &lt;pre&gt; &lt;center&gt; &lt;sup&gt; &lt;sub&gt; or &lt;gallery&gt; tags and comments
         /// </summary>
@@ -2870,13 +2870,12 @@ namespace WikiFunctions.Parse
                 if(start != end)
                 {
                     unmatched = true;
-
                     break;
                 }
             }
 
             // check for unclosed part tag
-            if(!unmatched && !Regex.IsMatch(articleText, @"<[^>]+$"))
+            if(!unmatched && !Regex.IsMatch(AnyTag.Replace(articleText, ""), @"<[^>]+$"))
                 return back;
             
             // if here then have some unmatched tags, so do full clear down and search
