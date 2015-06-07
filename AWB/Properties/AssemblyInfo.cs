abe158e2a36435869d1decae102cb7c2d52d1c06
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2015-06-07 08:40:09 +0000

    Add "on the English Wikipedia" per https://phabricator.wikimedia.org/T100749
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@11077 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Properties/AssemblyInfo.cs
+++ AWB/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AutoWikiBrowser")]
-[assembly: AssemblyDescription("AutoWikiBrowser (AWB) comes with ABSOLUTELY NO WARRANTY.\r\n\r\nAWB is constantly in development, and there may be bugs in the software. YOU are responsible for what you do with this tool.  Use it at your own risk.\r\n\r\nAnyone is free to copy and distribute this software; only users registered on the Wikipedia:AutoWikiBrowser/CheckPage are allowed to use it.  All editing done with it must be allowed under the policies and guidelines of the wiki you are editing.")]
+[assembly: AssemblyDescription("AutoWikiBrowser (AWB) comes with ABSOLUTELY NO WARRANTY.\r\n\r\nAWB is constantly in development, and there may be bugs in the software. YOU are responsible for what you do with this tool.  Use it at your own risk.\r\n\r\nAnyone is free to copy and distribute this software; only users registered on the Wikipedia:AutoWikiBrowser/CheckPage are allowed to use it on the English Wikipedia.  All editing done with it must be allowed under the policies and guidelines of the wiki you are editing.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AutoWikiBrowser")]
