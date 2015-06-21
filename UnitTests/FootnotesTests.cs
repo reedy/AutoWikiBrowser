f2b0d4404271ac4f08e21f71c212ca00fa6d01f0
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2015-06-21 15:16:40 +0000

    FixReferenceTags: handle nested empty ref tags
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@11229 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/FootnotesTests.cs
+++ UnitTests/FootnotesTests.cs
@@ -221,6 +221,7 @@ namespace UnitTests
 
             // empty tags
             Assert.AreEqual(@"", Parsers.FixReferenceTags(@"<ref> </ref>"));
+            Assert.AreEqual(@"", Parsers.FixReferenceTags(@"<ref><ref> </ref></ref>"));
 
             // <ref name="Fred" Smith> --> <ref name="Fred Smith">
             Assert.AreEqual(@"<ref name=""Fred Smith"" />", Parsers.FixReferenceTags(@"<ref name=""Fred"" Smith />"));
