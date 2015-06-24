b0eefffead0edf49d9cf9bfd77a99a031f3fa1d3
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2015-06-24 17:02:19 +0000

    Avoid changing ISBN in URL
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@11271 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/Parse/FixSyntax.cs
+++ WikiFunctions/Parse/FixSyntax.cs
@@ -127,7 +127,7 @@ namespace WikiFunctions.Parse
         private static readonly Regex SyntaxRegexHorizontalRule = new Regex("^(<hr>|-{5,})", RegexOptions.Compiled | RegexOptions.Multiline);
         private static readonly Regex SyntaxRegexHeadingWithHorizontalRule = new Regex("(^==?[^=]*==?)\r\n(\r\n)?----+", RegexOptions.Compiled | RegexOptions.Multiline);
         private static readonly Regex SyntaxRegexHTTPNumber = new Regex(@"HTTP/\d\.", RegexOptions.Compiled);
-        private static readonly Regex SyntaxRegexISBN = new Regex(@"(?:ISBN(?:-1[03])?:|\[\[ISBN\]\]|ISBN\t)\s*(\d)", RegexOptions.Compiled);
+        private static readonly Regex SyntaxRegexISBN = new Regex(@"(?<!:)(?:ISBN(?:-1[03])?:|\[\[ISBN\]\]|ISBN\t)\s*(\d)", RegexOptions.Compiled);
         private static readonly Regex SyntaxRegexISBN2 = new Regex(@"ISBN-(?!1[03]\b)", RegexOptions.Compiled);
         private static readonly Regex SyntaxRegexISBN3 = new Regex(@"\[\[ISBN\]\]\s\[\[Special\:BookSources[^\|]*\|([^\]]*)\]\]", RegexOptions.Compiled);
         private static readonly Regex SyntaxRegexPMID = new Regex(@"(PMID): *(\d)", RegexOptions.Compiled);
