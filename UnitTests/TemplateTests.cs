c3674c4fe3284ced9cf6e5ac95e2cec940ea437b
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2015-09-14 08:44:37 +0000

    Tests for GetAllTemplates
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@11468 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/TemplateTests.cs
+++ UnitTests/TemplateTests.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using WikiFunctions;
+using WikiFunctions.Parse;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace UnitTests
 {
@@ -6,8 +10,24 @@ namespace UnitTests
     public class TemplateTests : RequiresInitialization
     {
         [Test]
-        public void SimpleExtraction()
+        public void GetAllTemplates()
         {
+            List<string> t = new List<string>();
+
+            Assert.AreEqual(t, Parsers.GetAllTemplates(""));
+            t.Add("Foo");
+            Assert.AreEqual(t, Parsers.GetAllTemplates("{{foo}}"), "case converted to first letter upper");
+            Assert.AreEqual(t, Parsers.GetAllTemplates("{{Foo}}"), "uppercase template first letter handled");
+            Assert.AreEqual(t, Parsers.GetAllTemplates("{{Foo}} {{foo}}"), "Templates list deduplicated");
+            Assert.AreEqual(t, Parsers.GetAllTemplates("{{Foo|bar=1}}"), "Support templates with arguments");
+            t.Add("Other");
+            Assert.AreEqual(t, Parsers.GetAllTemplates("{{Foo|bar=1 {{Other}} }}"), "Support nested templates");
+            t.Add("Other2");
+            Assert.AreEqual(t, Parsers.GetAllTemplates("{{Foo|bar=1 {{Other| a= {{Other2|}} }} }}"), "Support 2x nested templates");
+            Assert.AreEqual(t, Parsers.GetAllTemplates(@"{{_Foo  |bar=1 {{ Other
+| a= {{ Other2_|}} }} }}"), "Ignore whitespace");
+            Assert.AreEqual(t, Parsers.GetAllTemplates(@"{{Foo|bar=1 {{Other
+| a= {{Template:Other2_|}} }} }}"), "Ignore Template: prefix");
         }
     }
 }
