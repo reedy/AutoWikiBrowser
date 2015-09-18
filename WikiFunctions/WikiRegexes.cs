3688c4147812a4d5390297c172f02242ce4da077
Marios Magioladitis <magioladitis@users.sourceforge.net>
2015-09-18 12:38:18 +0000

    Ru.wiki does not have AI nor MI template
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@11549 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/WikiRegexes.cs
+++ WikiFunctions/WikiRegexes.cs
@@ -244,7 +244,7 @@ namespace WikiFunctions
                 case "ru":
                     Orphan = Tools.NestedTemplateRegex(@"изолированная статья");
                     DateYearMonthParameter = @"date={{subst:CURRENTMONTHNAME}} {{subst:CURRENTYEAR}}";
-					DeadEnd = new Regex(@"(?:{{\s*(?:[Tt]упиковая статья|[Dd]ead ?end)(?:\|(?:[^{}]+|" + DateYearMonthParameter + @"))?}}|({{\s*(?:[Aa]rticle|[Mm]ultiple)\s*issues\b[^{}]*?(?:{{subst:CURRENTMONTHNAME}} {{subst:CURRENTYEAR}})?[^{}]*?)*\|\s*dead ?end\s*=\s*(?:{{subst:CURRENTMONTHNAME}} {{subst:CURRENTYEAR}}|[^{}\|]+))");
+					DeadEnd = new Regex(@"(?:{{\s*(?:[Tt]упиковая статья|[Dd]ead ?end)(?:\|(?:[^{}]+|" + DateYearMonthParameter + @"))?}})");
                     Wikify = new Regex(@"({{\s*Wikify(?:\s*\|\s*(" +DateYearMonthParameter +@"|.*?))?}})", RegexOptions.IgnoreCase);
                     InUse = Tools.NestedTemplateRegex(new[] {"Редактирую", "Перерабатываю", "Inuse-by", "Пишу", "Inuse", "Правлю", "Перевожу", "In-use", "Processing", "Process", "Статья редактируется", "Викифицирую", "Under construction" });
                     break;
