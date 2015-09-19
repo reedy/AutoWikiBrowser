9a2f6ab22b4d3b887832bb2b64c5dd656e840bae
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2015-09-19 08:29:09 +0000

    Make use of Tools.DatesCount function, saves ~30 ms on long articles
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@11553 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Main.cs
+++ AWB/Main.cs
@@ -2895,8 +2895,9 @@ font-size: 150%;'>No changes</h2><p>Press the ""Skip"" button below to skip to t
 
                 // for date types count ignore images and URLs
                 string articleTextNoImagesURLs = WikiRegexes.ExternalLinksHTTPOnlyQuick.Replace(Tools.ReplaceWithSpaces(articleText, imagesMC), "");
-                lblDates.Text = Dates + WikiRegexes.ISODatesQuick.Matches(articleTextNoImagesURLs).Count + "/" + WikiRegexes.DayMonth.Matches(articleTextNoImagesURLs).Count
-                    + "/" + WikiRegexes.MonthDay.Matches(articleTextNoImagesURLs).Count;
+                Dictionary<Parsers.DateLocale, int> results = Tools.DatesCount(articleTextNoImagesURLs);
+
+                lblDates.Text = Dates + results[Parsers.DateLocale.ISO] + "/" + results[Parsers.DateLocale.International] + "/" + results[Parsers.DateLocale.American];
 
                 // Find multiple wikilinks
                 //get all the links, ignore commented out text etc.
