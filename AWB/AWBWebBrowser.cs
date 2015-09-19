e682b7e884b895ecd2100949d1255003338565f0
Sam Reed <sam@reedyboy.net>
2015-09-19 18:27:39 +0000

    Move class into namespace
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@11561 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/AWBWebBrowser.cs
+++ AWB/AWBWebBrowser.cs
@@ -1,99 +1,100 @@
 ﻿using System.Windows.Forms;
 using mshtml;
 using WikiFunctions;
-class AWBWebBrowser : WebBrowser
+
+namespace AutoWikiBrowser
 {
-    public override bool PreProcessMessage(ref Message msg) 
-    {
-        // look for and intercept a Ctrl+C key up event to copy selected text to keyboard
-        if (msg.Msg == 0x101 && msg.WParam.ToInt32() == (int)Keys.C
-            && ModifierKeys == Keys.Control)
-        {
-            CopySelectedText();
-            return true;
-        }
+	class AWBWebBrowser : WebBrowser
+	{
+		public override bool PreProcessMessage(ref Message msg) 
+		{
+			// look for and intercept a Ctrl+C key up event to copy selected text to keyboard
+			if (msg.Msg == 0x101 && msg.WParam.ToInt32() == (int)Keys.C
+			    && ModifierKeys == Keys.Control)
+			{
+				CopySelectedText();
+				return true;
+			}
 
-        // Ctrl+J to find selected text in edit text box
-        else if (msg.Msg == 0x101 && msg.WParam.ToInt32() == (int)Keys.J
-            && ModifierKeys == Keys.Control && TextSelected())
-        {
-            Variables.MainForm.EditBox.Find(SelectedText(), false, false, Variables.MainForm.TheSession.Page.Title);
-            return true;
-        }
-        return base.PreProcessMessage(ref msg);
-    }
+			// Ctrl+J to find selected text in edit text box
+			if (msg.Msg == 0x101 && msg.WParam.ToInt32() == (int)Keys.J
+			    && ModifierKeys == Keys.Control && TextSelected())
+			{
+				Variables.MainForm.EditBox.Find(SelectedText(), false, false, Variables.MainForm.TheSession.Page.Title);
+				return true;
+			}
+			return base.PreProcessMessage(ref msg);
+		}
     
-    /// <summary>
-    /// Copies the selected text (if any) to the clipboard
-    /// </summary>
-    public void CopySelectedText()
-    {
-        Document.ExecCommand("Copy", false, null);
-    }
+		/// <summary>
+		/// Copies the selected text (if any) to the clipboard
+		/// </summary>
+		public void CopySelectedText()
+		{
+			Document.ExecCommand("Copy", false, null);
+		}
     
-    /// <summary>
-    /// Returns whether there is currently any text selected
-    /// Only works if Microsoft.mshtml.dll is available
-    /// Check of Globals property must be in separate method to the IHTMLDocument2 code
-    /// Further, on some systems even when Assembly can be loaded, it still doesn't work
-    /// so require additional try/catch
-    /// </summary>
-    /// <returns></returns>
-    public bool TextSelected()
-    {
-        if (Globals.MSHTMLAvailable)
-        {
-            try
-            {
-                return TextSelectedChecked();
-            }
-            catch
-            {
-                // So system reported that Assembly does load, but it still doesn't work
-                // See https://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Bugs/Archive_23#Single_click_to_focus_the_edit_box_to_a_line_-_no_longer_works_with_SVN9282
-                return false;
-            }
-        }
-        
-        return false;
-    }
+		/// <summary>
+		/// Returns whether there is currently any text selected
+		/// Only works if Microsoft.mshtml.dll is available
+		/// Check of Globals property must be in separate method to the IHTMLDocument2 code
+		/// Further, on some systems even when Assembly can be loaded, it still doesn't work
+		/// so require additional try/catch
+		/// </summary>
+		/// <returns></returns>
+		public bool TextSelected()
+		{
+			if (!Globals.MSHTMLAvailable) return false;
 
-    /// <summary>
-    /// Returns whether there is currently any text selected
-    /// Only works if Microsoft.mshtml.dll is available
-    /// </summary>
-    /// <returns></returns>
-    private bool TextSelectedChecked()
-    {
-        IHTMLDocument2 htmlDocument = Document.DomDocument as IHTMLDocument2;
+			try
+			{
+				return TextSelectedChecked();
+			}
+			catch
+			{
+				// So system reported that Assembly does load, but it still doesn't work
+				// See https://en.wikipedia.org/wiki/Wikipedia_talk:AutoWikiBrowser/Bugs/Archive_23#Single_click_to_focus_the_edit_box_to_a_line_-_no_longer_works_with_SVN9282
+				return false;
+			}
+		}
 
-        IHTMLSelectionObject currentSelection = htmlDocument.selection;
+		/// <summary>
+		/// Returns whether there is currently any text selected
+		/// Only works if Microsoft.mshtml.dll is available
+		/// </summary>
+		/// <returns></returns>
+		private bool TextSelectedChecked()
+		{
+			IHTMLDocument2 htmlDocument = Document.DomDocument as IHTMLDocument2;
 
-        if (currentSelection != null)
-        {
-            IHTMLTxtRange range = currentSelection.createRange() as IHTMLTxtRange;
+			IHTMLSelectionObject currentSelection = htmlDocument.selection;
 
-            if (range != null && !string.IsNullOrEmpty(range.text))
-                return true;
-        }
-        
-        return false;
-    }
+			if (currentSelection != null)
+			{
+				IHTMLTxtRange range = currentSelection.createRange() as IHTMLTxtRange;
 
-    private string SelectedText()
-    {
-        IHTMLDocument2 htmlDocument = Document.DomDocument as IHTMLDocument2;
+				if (range != null && !string.IsNullOrEmpty(range.text))
+					return true;
+			}
 
-        IHTMLSelectionObject currentSelection = htmlDocument.selection;
+			return false;
+		}
 
-        if (currentSelection != null)
-        {
-            IHTMLTxtRange range = currentSelection.createRange() as IHTMLTxtRange;
+		private string SelectedText()
+		{
+			IHTMLDocument2 htmlDocument = Document.DomDocument as IHTMLDocument2;
 
-            if (range != null && !string.IsNullOrEmpty(range.text))
-                return range.text;
-        }
+			IHTMLSelectionObject currentSelection = htmlDocument.selection;
 
-        return "";
-    }
+			if (currentSelection != null)
+			{
+				IHTMLTxtRange range = currentSelection.createRange() as IHTMLTxtRange;
+
+				if (range != null && !string.IsNullOrEmpty(range.text))
+					return range.text;
+			}
+
+			return "";
+		}
+	}
 }
