e41a7123ca31f56bb5313ef66dba687188407942
Marios Magioladitis <magioladitis@users.sourceforge.net>
2016-01-20 12:28:08 +0000

    Fix copyright symbols to be the same everywhere
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@11804 5227b50d-9349-4574-b055-4b0a7525e11c

--- Plugins/Kingbotk Plugin/WikiProjects/WPAlbums.cs
+++ Plugins/Kingbotk Plugin/WikiProjects/WPAlbums.cs
@@ -1,6 +1,6 @@
 /*
-Copyright © 2008 Stephen Kennedy (Kingboyk) http://www.sdk-software.com/
-Copyright © 2008 Sam Reed (Reedy) http://www.reedyboy.net/
+Copyright (C) 2008 Stephen Kennedy (Kingboyk) http://www.sdk-software.com/
+Copyright (C) 2008 Sam Reed (Reedy) http://www.reedyboy.net/
 
 This program is free software; you can redistribute it and/or modify it under the terms of Version 2 of the GNU General Public License as published by the Free Software Foundation.
 
