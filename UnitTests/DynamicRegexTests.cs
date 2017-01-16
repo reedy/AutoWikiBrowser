9793af231e2421ee0408fd6961bab96da93c6c56
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2017-01-16 12:45:22 +0000

    For T155382 Add storm index to SIAs list
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@12143 5227b50d-9349-4574-b055-4b0a7525e11c

--- UnitTests/DynamicRegexTests.cs
+++ UnitTests/DynamicRegexTests.cs
@@ -1219,6 +1219,7 @@ ABC");
             RegexAssert.IsMatch(WikiRegexes.SIAs, @"{{Shipindex}}");
             RegexAssert.IsMatch(WikiRegexes.SIAs, @"{{sportindex}}");
             RegexAssert.IsMatch(WikiRegexes.SIAs, @"{{sport index}}");
+            RegexAssert.IsMatch(WikiRegexes.SIAs, @"{{storm index}}");
             RegexAssert.IsMatch(WikiRegexes.SIAs, @"{{Animal common name}}");
             RegexAssert.IsMatch(WikiRegexes.SIAs, @"{{Fungus common name}}");
             RegexAssert.IsMatch(WikiRegexes.SIAs, @"{{Plant common name}}");
