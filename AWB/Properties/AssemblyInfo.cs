94789f86690f79f0fe571c31d9335391526502b3
Sam Reed <sam@reedyboy.net>
2018-08-28 14:32:44 +0000

    Bump .NET 4.5 versions to being 5.11.0.0 numbering
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@12249 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Properties/AssemblyInfo.cs
+++ AWB/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 
-[assembly: AssemblyVersion("5.10.0.1")]
-[assembly: AssemblyFileVersion("5.10.0.1")]
+[assembly: AssemblyVersion("5.11.0.0")]
+[assembly: AssemblyFileVersion("5.11.0.0")]
 
 [assembly: CLSCompliant(true)]
