953038233bf855d5f843fcf83750e996c12ca407
Kumioko <40904121+Kumioko@users.noreply.github.com>
2018-09-06 19:48:08 -0400

    Fix a comment typo

--- WikiFunctions/Article/Article.cs
+++ WikiFunctions/Article/Article.cs
@@ -377,7 +377,7 @@ namespace WikiFunctions
         { get { return mAWBLogListener.Skipped; } }
 
         /// <summary>
-        /// Returns true of article general fixes can be applied to the page: article, draft or category namespace, sandbox, template documnetation page or Anexo namespace on es-wiki and pt-wiki
+        /// Returns true of article general fixes can be applied to the page: article, draft or category namespace, sandbox, template documentation page or Anexo namespace on es-wiki and pt-wiki
         /// </summary>
         [XmlIgnore]
         public bool CanDoGeneralFixes
