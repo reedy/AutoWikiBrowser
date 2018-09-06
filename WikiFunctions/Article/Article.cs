493f0d35e5156b8f9781d6d553cfacaa40960c2a
Kumioko <40904121+Kumioko@users.noreply.github.com>
2018-09-06 19:41:45 -0400

    Allow GeneralFixes to work on Drafts per T111660
    Per T111660, allow GeneralFixes to work on article drafts. Especially since all new articles have to be done as drafts this will help reviewers a lot.

--- WikiFunctions/Article/Article.cs
+++ WikiFunctions/Article/Article.cs
@@ -377,7 +377,7 @@ namespace WikiFunctions
         { get { return mAWBLogListener.Skipped; } }
 
         /// <summary>
-        /// Returns true of article general fixes can be applied to the page: article or category namespace, sandbox, template documnetation page or Anexo namespace on es-wiki and pt-wiki
+        /// Returns true of article general fixes can be applied to the page: article, draft or category namespace, sandbox, template documnetation page or Anexo namespace on es-wiki and pt-wiki
         /// </summary>
         [XmlIgnore]
         public bool CanDoGeneralFixes
@@ -386,6 +386,7 @@ namespace WikiFunctions
             {
                 return (NameSpaceKey == Namespace.Article
                         || NameSpaceKey == Namespace.Category
+                        || NameSpaceKey == Namespace.Draft
                         || Name.Contains("Sandbox")
                         || Name.Contains("sandbox")
                         || Name.Contains("/doc")
