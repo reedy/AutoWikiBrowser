baf6d776501561c6cef4883cab409ff69cdb6b7c
Sam Reed <sam@reedyboy.net>
2018-09-17 02:31:25 +0000

    First pass on converter
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@12293 5227b50d-9349-4574-b055-4b0a7525e11c

--- Extras/CheckPage Converter/Program.cs
+++ Extras/CheckPage Converter/Program.cs
@@ -1,4 +1,8 @@
-﻿using WikiFunctions;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using WikiFunctions;
+using WikiFunctions.API;
 
 namespace CheckPage_Converter
 {
@@ -6,11 +10,41 @@ namespace CheckPage_Converter
     {
         static void Main(string[] args)
         {
-            var checkpage =
+            var checkPageText =
                 Tools.GetHTML(
                     "https://en.wikipedia.org/w/index.php?title=Wikipedia:AutoWikiBrowser/CheckPage&action=raw");
 
+            checkPageText = Tools.StringBetween(checkPageText, "<!--enabledusersbegins-->",
+                                                    "<!--enabledusersends-->");
 
+            string botUsers = Tools.StringBetween(checkPageText, "<!--enabledbots-->", "<!--enabledbotsends-->");
+
+            checkPageText = checkPageText.Replace("<!--enabledbots-->\r\n" + checkPageText + "\r\n<!--enabledbotsends-->", "");
+
+            Regex username = new Regex(@"^\*\s*(.*?)\s*$", RegexOptions.Multiline | RegexOptions.Compiled);
+
+            List<string> users = new List<string>();
+            foreach (Match m in username.Matches(checkPageText)) {
+                users.Add(m.Groups[0].Value);
+            }
+
+            List<string> bots = new List<string>();
+            foreach (Match m in username.Matches(botUsers))
+            {
+                bots.Add(m.Groups[0].Value);
+            }
+
+            Dictionary<string, List<string>> output = new Dictionary<string, List<string>> {
+                { "enabledusers", users },
+                { "enabledbots", bots }
+            };
+
+            string json = JsonConvert.SerializeObject(output, Formatting.Indented);
+
+            ApiEdit edit = new ApiEdit("https://en.wikipedia.org/w/");
+            edit.Login("", "");
+            edit.Open("Project:AutoWikiBrowser/CheckPageJSON");
+            edit.Save(json, "Converting from non json page", false, WatchOptions.NoChange);
         }
     }
 }
