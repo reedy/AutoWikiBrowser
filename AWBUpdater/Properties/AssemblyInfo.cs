f2d6f3c50a11c2207f1ad266044d641e8fa57b81
Sam Reed <sam@reedyboy.net>
2019-09-17 17:58:51 +0000

    Bump AWBUpdater Copyright to include 2019
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@12348 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWBUpdater/Properties/AssemblyInfo.cs
+++ AWBUpdater/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AWBUpdater")]
-[assembly: AssemblyCopyright("Copyright © 2007-2018 AutoWikiBrowser Team")]
+[assembly: AssemblyCopyright("Copyright © 2007-2019 AutoWikiBrowser Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
