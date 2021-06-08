3409112026aaac6734123c083e6c905669aed605
Sam Reed <sam@reedyboy.net>
2021-06-08 15:23:13 +0000

    Bump 6.2.0.0
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@12469 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Properties/AssemblyInfo.cs
+++ AWB/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AutoWikiBrowser")]
-[assembly: AssemblyCopyright("Copyright © 2006-2019 AutoWikiBrowser Team")]
+[assembly: AssemblyCopyright("Copyright © 2006-2021 AutoWikiBrowser Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 
-[assembly: AssemblyVersion("6.1.0.2")]
-[assembly: AssemblyFileVersion("6.1.0.2")]
+[assembly: AssemblyVersion("6.2.0.0")]
+[assembly: AssemblyFileVersion("6.2.0.0")]
 
 [assembly: CLSCompliant(true)]
--- WikiFunctions/Properties/AssemblyInfo.cs
+++ WikiFunctions/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WikiFunctions")]
-[assembly: AssemblyCopyright("Copyright © 2006-2019 AutoWikiBrowser Team")]
+[assembly: AssemblyCopyright("Copyright © 2006-2021 AutoWikiBrowser Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("6.1.0.2")]
-[assembly: AssemblyFileVersion("6.1.0.2")]
+[assembly: AssemblyVersion("6.2.0.0")]
+[assembly: AssemblyFileVersion("6.2.0.0")]
 
 [assembly: CLSCompliant(true)]
