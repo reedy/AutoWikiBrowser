3dee3eb79175e8651f893a144ae3f9f670164d1e
Sam Reed <sam@reedyboy.net>
2021-06-08 15:36:47 +0000

    Bump 6.2.0.1
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@12470 5227b50d-9349-4574-b055-4b0a7525e11c

--- AWB/Properties/AssemblyInfo.cs
+++ AWB/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 
-[assembly: AssemblyVersion("6.2.0.0")]
-[assembly: AssemblyFileVersion("6.2.0.0")]
+[assembly: AssemblyVersion("6.2.0.1")]
+[assembly: AssemblyFileVersion("6.2.0.1")]
 
 [assembly: CLSCompliant(true)]
