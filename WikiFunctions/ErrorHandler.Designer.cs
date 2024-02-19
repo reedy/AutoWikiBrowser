c84ed499a961a0f039fe76e741c9a90c5ce8cde6
Sam Reed <sam@reedyboy.net>
2024-02-19 18:19:47 +0000

    ErrorHandler: Add globe icon
    git-svn-id: https://svn.code.sf.net/p/autowikibrowser/code/AWB@12571 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/ErrorHandler.Designer.cs
+++ WikiFunctions/ErrorHandler.Designer.cs
@@ -158,6 +158,7 @@
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(401, 489);
             this.Name = "ErrorHandler";
             this.Text = "Program error";
